8ee19062aa63cdac2e2ffb415ae81012ccbeae11
Quang Trung <gnurt10@gmail.com>
2022-02-15 22:20:18 +0700

    Create 118-pascals-triangle.js

---
+++ 118-pascals-triangle.js
@@ -0,0 +1,16 @@
+// 1. ....
+
+function generate(numRows) {
+    let ret = [];
+    
+    for(let i = 0; i < numRows; i++)
+    {
+        ret[i] = [];
+        ret[i][0] = ret[i][i] = 1;
+        for(let j = 1; j < i; j++)
+        {
+               ret[i][j] = ret[i-1][j] + ret[i-1][j-1]; 
+        }
+    }
+    return ret;
+};
