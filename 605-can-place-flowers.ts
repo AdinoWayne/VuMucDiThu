a6fd1ac65d6b261a40e7ab5466ec87ba9387a08d
Quang Trung <gnurt10@gmail.com>
2022-03-02 15:16:38 +0700

    Create 605-can-place-flowers.ts

---
+++ 605-can-place-flowers.ts
@@ -0,0 +1,15 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    var r = 0;
+    const l = flowerbed.length;
+    for(var i = 0;i< l; i++) {
+        if(flowerbed[i]==0 && flowerbed[i-1]!==1 && flowerbed[i+1]!==1) {
+            ++r;
+            flowerbed.splice(i,1,1);
+        }
+    }
+    if (n<=r) { 
+        return true
+    } else {
+        return false 
+    };
+};
