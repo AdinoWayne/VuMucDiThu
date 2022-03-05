0fd8336efcc3541dcf5708ba45ccf4a9cab58035
Quang Trung <gnurt10@gmail.com>
2022-03-05 21:44:40 +0700

    Create 733-flood-fill.ts

---
+++ code/733-flood-fill.ts
@@ -0,0 +1,9 @@
+function floodFill(image: number[][], sr: number, sc: number, newColor: number, firstColor = image[sr][sc]): number[][] {
+  if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[sr].length || image[sr][sc] !== firstColor || image[sr][sc] === newColor) return image;
+  image[sr][sc] = newColor;
+  floodFill(image, sr + 1, sc, newColor, firstColor);
+  floodFill(image, sr - 1, sc, newColor, firstColor);
+  floodFill(image, sr, sc + 1, newColor, firstColor);
+  floodFill(image, sr, sc - 1, newColor, firstColor);
+  return image;
+};
