3777f9186e3922231cd42352627f5a5a59b11b88
Quang Trung <gnurt10@gmail.com>
2022-03-22 13:08:15 +0700

    Create 1089-duplicate-zeros.ts

---
+++ code/1089-duplicate-zeros.ts
@@ -0,0 +1,17 @@
+/**
+ Do not return anything, modify arr in-place instead.
+ */
+function duplicateZeros(arr: number[]): void {
+  let temp = [];
+  for (let elem of arr) {
+    if (elem === 0) {
+      temp.push(0);
+      temp.push(0);
+    } else {
+      temp.push(elem);
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = temp[i];
+  }
+};
