b87f8a187bee94b8c13c1f029f6ebd198a6043a4
Quang Trung <gnurt10@gmail.com>
2022-04-03 01:03:50 +0700

    Create 1337-the-k-weakest-rows-in-a-matrix.js

---
+++ code/1337-the-k-weakest-rows-in-a-matrix.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} mat
+ * @param {number} k
+ * @return {number[]}
+ */
+var kWeakestRows = function(mat, k) {
+      return mat
+    .map((r, i) => [r, i])
+    .sort((a, b) => a[0].filter(Boolean).length - b[0].filter(Boolean).length)
+    .slice(0, k)
+    .map(([_, i]) => i)
+};
