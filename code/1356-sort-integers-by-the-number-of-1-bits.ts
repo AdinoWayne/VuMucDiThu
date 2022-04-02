a9e4029745a102109f99d137b3a6fdf75112ff12
Quang Trung <gnurt10@gmail.com>
2022-04-03 01:07:07 +0700

    Create 1356-sort-integers-by-the-number-of-1-bits.ts

---
+++ code/1356-sort-integers-by-the-number-of-1-bits.ts
@@ -0,0 +1,13 @@
+function sortByBits(arr: number[]): number[] {
+    let bin = function(x) {
+        let res = 0;
+        while (x) {
+            x = x & (x - 1);
+            res++;
+        }
+        return res;
+    }
+    arr.sort((a, b) => (bin(a) * 10000 + a) - (bin(b) * 10000 + b));
+    return arr;
+
+};
