fbbff5c4b2513f40df7a2ec85e63f7baaa9e6676
Quang Trung <gnurt10@gmail.com>
2022-04-06 11:36:01 +0700

    Create 1380-lucky-numbers-in-a-matrix.ts

---
+++ code/1380-lucky-numbers-in-a-matrix.ts
@@ -0,0 +1,24 @@
+function luckyNumbers (A: number[][]): number[] {
+    let r = A.length, c = A[0].length, res = [];
+
+    for (let i = 0; i < r; i++) {
+        let minv = 100000, minp = -1;
+        for (let j = 0; j < c; j++) {
+            if (A[i][j] < minv) {
+                minv = A[i][j];
+                minp = j;
+            }
+        }
+
+        let flag = false;
+        for (let j = 0; j < r; j++) {
+            if (A[j][minp] > minv) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) res.push(minv);
+    }
+    return res;
+
+};
