593dbf5b8dfe8f48197cb22b42c5bf8196383604
Quang Trung <gnurt10@gmail.com>
2022-05-20 14:45:40 +0700

    Create 2176-count-equal-and-divisible-pairs-in-an-array.ts

---
+++ code/2176-count-equal-and-divisible-pairs-in-an-array.ts
@@ -0,0 +1,8 @@
+function countPairs(nums: number[], k: number): number {
+    const n = nums.length;
+    let ans = 0;
+    for (let i = 0; i < n; ++i)
+      for (let j = i + 1; j < n; ++j)
+        ans += ((nums[i] == nums[j]) && (i * j % k == 0) ? 1 : 0);
+    return ans;
+};
