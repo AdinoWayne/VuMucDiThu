ab95e31944b3caa4c577a7c7a1750083b053d6eb
Quang Trung <gnurt10@gmail.com>
2022-05-30 13:56:06 +0700

    Create 242-valid-anagram.ts

---
+++ code/string/242-valid-anagram.ts
@@ -0,0 +1,23 @@
+function isAnagram(s: string, t: string): boolean {
+  var lenA = s.length;
+  var lenB = t.length;
+  var map = Array(26);
+  var index = 0;
+  var base = 'a'.charCodeAt(0);
+
+  if (lenA !== lenB) return false;
+
+  for (var i = 0; i < lenA; i++) {
+    index = s[i].charCodeAt(0) - base;
+    if (!map[index]) map[index] = 0;
+    map[index]++;
+  }
+
+  for (var j = 0; j < lenB; j++) {
+    index = t[j].charCodeAt(0) - base;
+    if (!map[index]) return false;
+    map[index]--;
+  }
+
+  return true;
+};
