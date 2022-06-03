f736e7fcbc570d05e173e9d13222019f72ed26a4
Quang Trung <gnurt10@gmail.com>
2022-06-03 10:53:34 +0700

    Create 409-longest-palindrome.ts

---
+++ code/string/409-longest-palindrome.ts
@@ -0,0 +1,22 @@
+function longestPalindrome(s: string): number {
+  const counts = {};
+  let sum = 0;
+  let oddPresent = false;
+
+  for (let i = 0; i < s.length; i++) {
+    counts[s[i]] = (counts[s[i]] || 0) + 1;
+  }
+
+  for (let letter in counts) {
+    if (counts[letter] % 2 === 0) {
+      sum += counts[letter];
+    } else {
+      oddPresent = true;
+      sum += counts[letter] - 1;
+    }
+  }
+
+  if (oddPresent) sum += 1;
+
+  return sum;
+};
