39a61c533517366653d8173fd754a56a90e35696
Quang Trung <gnurt10@gmail.com>
2022-06-18 12:56:58 +0700

    Update 925-long-pressed-name.ts

--- code/string/925-long-pressed-name.ts
+++ code/string/925-long-pressed-name.ts
@@ -1,9 +1,10 @@
 function isLongPressedName(name: string, typed: string): boolean {
     let i = 0;
+    const length = name.length;
     for (let j = 0; j < typed.length; ++j)
-      if (i < name.length && name[i] == typed[j])
+      if (i < length && name[i] == typed[j])
         ++i;
       else if (j == 0 || typed[j] != typed[j - 1])
         return false;
-    return i === name.length;
+    return i === length;
 };
