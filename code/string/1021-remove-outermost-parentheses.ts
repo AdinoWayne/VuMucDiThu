26b75f64130ab48464bde5df16d4dc263e98ad68
Quang Trung <gnurt10@gmail.com>
2022-06-18 12:57:37 +0700

    Update 1021-remove-outermost-parentheses.ts

--- code/string/1021-remove-outermost-parentheses.ts
+++ code/string/1021-remove-outermost-parentheses.ts
@@ -1,18 +1,17 @@
 function removeOuterParentheses(S: string): string {
-  let count = {}
-  let num = 0
-  const pArr = []
-
-  for(let i = 0; i < S.length; i++){
-    count[S[i]] = count[S[i]] ? ++count[S[i]] : 1
-    if(count['('] === count[')']){
-      pArr.push(S.slice(num, i + 1))
-      count = {}
-      num = i + 1
+  let count = {};
+  let num = 0;
+  const pArr = [];
+  for (let i = 0; i < S.length; i++) {
+    count[S[i]] = count[S[i]] ? ++count[S[i]] : 1;
+    if (count['('] === count[')']) {
+      pArr.push(S.slice(num, i + 1));
+      count = {};
+      num = i + 1;
     }
   }
-  for(let item in pArr){
+  for (let item in pArr) {
     pArr[item] = pArr[item].slice(1, pArr[item].length - 1)
   }
-  return pArr.join('')
+  return pArr.join('');
 };
