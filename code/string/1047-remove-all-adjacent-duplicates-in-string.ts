5de724f43df55a82d491c670ff1a9139ed221360
PeanutTran <85547159+PeanutTran@users.noreply.github.com>
2022-06-19 00:01:28 +0700

    Create 1047-remove-all-adjacent-duplicates-in-string.ts

---
+++ code/string/1047-remove-all-adjacent-duplicates-in-string.ts
@@ -0,0 +1,15 @@
+function removeDuplicates(S: string): string {
+    if(S.length < 2){
+        return S
+    }
+
+    let stack = [S.charAt(0)];
+    for(let i = 1; i< S.length; i++){
+        if(stack[stack.length - 1] == S.charAt(i)){
+            stack.pop();
+        }else{
+            stack.push(S.charAt(i));
+        }
+    }
+    return stack.join('');
+};
