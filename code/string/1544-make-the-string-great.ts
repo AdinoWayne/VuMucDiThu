e6883ce1ebd57faca5294ff5a667286e224fff94
Quang Trung <gnurt10@gmail.com>
2022-07-02 09:50:05 +0700

    Create 1544-make-the-string-great.ts

---
+++ code/string/1544-make-the-string-great.ts
@@ -0,0 +1,16 @@
+function makeGood(s: string): string {
+  let str = '';
+  for (let i = 0; i < s.length; i += 1) {
+    if (!str) {str = s[i]; continue;}
+    let char = str[str.length - 1];
+    if (char === char.toUpperCase()) {
+      if (s[i] === char.toLowerCase()) str = str.slice(0, str.length - 1);
+      else str += s[i];
+    }
+     else {
+      if (s[i] === char.toUpperCase()) str = str.slice(0, str.length - 1);
+      else str += s[i];
+    }
+  }
+  return str;
+};
