a29deac9a2a4956a9cca4fcfe92a050a533c1ba8
Quang Trung <gnurt10@gmail.com>
2022-07-06 00:05:47 +0700

    Create 1736-latest-time-by-replacing-hidden-digits.ts

---
+++ code/string/1736-latest-time-by-replacing-hidden-digits.ts
@@ -0,0 +1,8 @@
+function maximumTime(a: string): string {
+    let time = a.split('')
+    if (time[0] === "?") time[0] = +time[1] > 3 ? "1" : "2"
+    if (time[1] === "?") time[1] = +time[0] > 1 ? "3" : "9"
+    if (time[3] === "?") time[3] = "5"
+    if (time[4] === "?") time[4] = "9"
+    return time.join('')
+};
