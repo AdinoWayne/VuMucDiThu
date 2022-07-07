f27790c5bb7ccf9d2787bd115d0f0fcbc1c5383a
Quang Trung <gnurt10@gmail.com>
2022-07-07 23:05:01 +0700

    Create 1796-second-largest-digit-in-a-string.ts

---
+++ code/string/1796-second-largest-digit-in-a-string.ts
@@ -0,0 +1,12 @@
+function secondHighest(s: string): number {
+    let nums = s.split(/[A-Za-z]/).filter(a=> a!== '').join('')
+ 
+    let arr=[]
+    for(let i = 0; i < nums.length; i++) {
+        if(!arr.includes(nums[i])) {
+            arr.push(nums[i])
+        }
+    }
+    
+    return arr.sort((a,b) => a-b)[arr.length-2] ? arr.sort((a,b) => a-b)[arr.length-2] : -1
+};
