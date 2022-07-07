a17088a7ec94f8d8d80e0217ab50d432b9102761
Quang Trung <gnurt10@gmail.com>
2022-07-07 23:06:13 +0700

    Create 1768-merge-strings-alternately.ts

---
+++ code/string/1768-merge-strings-alternately.ts
@@ -0,0 +1,8 @@
+function mergeAlternately(word1: string, word2: string): string {
+  let res = '', i = 0
+  while (i < word1.length && i < word2.length) {
+    res += word1[i] + word2[i]
+    i += 1 
+  }
+  return res + (word1.length > word2.length ? word1.slice(i) : word2.slice(i))
+};
