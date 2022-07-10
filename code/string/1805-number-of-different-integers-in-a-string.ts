c43a099f54c7db5d91d8ff3b379970851b725eb1
Quang Trung <gnurt10@gmail.com>
2022-07-11 00:35:46 +0700

    Create 1805-number-of-different-integers-in-a-string.ts

---
+++ code/string/1805-number-of-different-integers-in-a-string.ts
@@ -0,0 +1,10 @@
+function numDifferentIntegers(word: string): number {
+    let chars = /[a-z]/g, remZeros = /^[0]*/g, set = new Set();
+    let arr = word.replace(chars, ' ').split(' ').map((val) => {
+        if (val != '') {
+            let newval = val.replace(remZeros, '');
+            set.add(newval);
+        }
+    });
+    return set.size;
+};
