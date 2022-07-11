e88a93d795846aa8bc5bcfff6ff489d4a1300157
Quang Trung <gnurt10@gmail.com>
2022-07-11 23:27:17 +0700

    Update 989-add-to-array-form-of-integer.ts

--- code/989-add-to-array-form-of-integer.ts
+++ code/989-add-to-array-form-of-integer.ts
@@ -1,46 +1,25 @@
 function addToArrayForm(A: number[], K: number): number[] {
-    let len = A.length
-    if(len <= 5) {
-        let res = 0
-        for(let i = 0; i < len; i ++) {
-            res += A[i] * Math.pow(10, len - 1 - i)
-        }
-        res += K
-        let ans = [0]
-        let i = 0
-        while(res) {
-            ans[i ++] = res % 10
-            res = Math.floor(res / 10)
-        }
-        return ans.reverse()
-    }else {
-        let res = 0
-        for(let i = len - 5, j = 0; i < len; i ++, j ++) {
-            res += A[i] * Math.pow(10, 4 - j)
-        }
-        res += K
-        let i = 0
-        let ans = [0]
-        while(res) {
-            ans[i ++] = res % 10
-            res = Math.floor(res / 10)
-        }
-        if(ans.length <= 5) {
-            for(let i = 0; i < 5 - ans.length; i ++) {
-                ans.push(0)
-            }
-            for(let i = len - 6, j = 0; i >= 0; i --, j ++) {
-                ans[5 + j] = A[i]
-            }
-        }else {
-            let yu = ans[5]
-            let j = 0
-            for(let i = len - 6; i >= 0; i --, j ++) {
-                ans[5 + j] = (A[i] + yu) % 10
-                yu = Math.floor((A[i] + yu) / 10)
-            }
-            if(yu) ans.push(yu)
-        }
-        return ans.reverse()
+    // i = current index of array A
+    // c = carry
+    // k = current least significant digit of K
+    // a = current least significant digit of A
+    // d = current digit to push
+    const n = A.length
+    const temp = []
+    let i = n - 1
+    let c = 0
+    while (i >= 0 || K > 0) {
+        let k = K % 10
+        let a = i < 0 ? 0 : A[i]
+        let s = k + a + c
+        let d = s % 10
+        temp.push(d)
+        c = s > 9 ? 1 : 0
+        K = Math.floor(K / 10)
+        i--
     }
+
+    if (c == 1) temp.push(c)
+    
+    return temp.reverse()
 };
