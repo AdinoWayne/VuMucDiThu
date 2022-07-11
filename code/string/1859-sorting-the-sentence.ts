209acbf89a9f6b922d4473e14dac9e65e1f8f544
Quang Trung <gnurt10@gmail.com>
2022-07-12 00:01:08 +0700

    Update 1859-sorting-the-sentence.ts

--- code/string/1859-sorting-the-sentence.ts
+++ code/string/1859-sorting-the-sentence.ts
@@ -1,11 +1,9 @@
 function sortSentence(s: string): string {
-    const result = []
-    const sArr = s.split(' ')
-    
+    const result = [];
+    const sArr = s.split(' ');
     for(const item of sArr) {
-        const index = Number(item[item.length-1]) - 1
-        result[index] = item.slice(0, item.length-1)
+        const index = Number(item[item.length-1]) - 1;
+        result[index] = item.slice(0, item.length-1);
     }
-    
-    return result.join(' ')
+    return result.join(' ');
 };
