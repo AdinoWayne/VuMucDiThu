6720c1ce69ccd8eb1864c857c8e53b9777124c42
Quang Trung <gnurt10@gmail.com>
2022-07-17 10:36:30 +0700

    Create 1945-sum-of-digits-of-string-after-convert.ts

---
+++ code/string/1945-sum-of-digits-of-string-after-convert.ts
@@ -0,0 +1,16 @@
+function getLucky(s: string, k: number): string {
+    let alpha = '-abcdefghijklmnopqrstuvwxyz';
+    let res = ''
+    for(let c of s) {
+        res += alpha.indexOf(c)
+    }
+    
+    while(k--) {
+        let sum = 0;
+        for(let c of res) {
+            sum += +c;
+        }
+        res = sum.toString();
+    }
+    return res; 
+};
