258783bc24fc9442f029289f16dcb65cc1547b68
Quang Trung <gnurt10@gmail.com>
2022-07-18 16:25:10 +0700

    Create 2047-number-of-valid-words-in-a-sentence.ts

---
+++ code/string/2047-number-of-valid-words-in-a-sentence.ts
@@ -0,0 +1,12 @@
+function countValidWords(sentence: string): number {
+    let list=sentence.split(' ')
+    let filtered=list.filter(s=>{
+        if(/\d/.test(s) || s=='') return false     //removes anything with numbers or is blank
+        if(/^[!,.]$/.test(s)) return true          //punctuation only
+        if(/^\w+[!,.]?$/.test(s)) return true      //word + optional punctuation
+        if(/^\w+[-]?\w+[!,.]?$/.test(s)) return true //word + optional hypen + word + optional punctuation
+        return false
+    })
+    
+    return filtered.length
+};
