677ac992d364e8e2a4b5957ab39eaa451046fba2
Quang Trung <gnurt10@gmail.com>
2022-07-24 10:21:56 +0700

    Create 2315-count-asterisks.ts

---
+++ code/string/2315-count-asterisks.ts
@@ -0,0 +1,8 @@
+function countAsterisks(s: string): number {
+    let green=true, count=0;
+    for(let i=0; i<s.length; i++){
+        if(green && s[i]=="*"){count++};
+        if(s[i]=="|"){green=!green};
+    }
+    return count;
+};
