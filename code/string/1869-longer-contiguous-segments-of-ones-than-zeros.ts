a09a2b329f810c9c6cc1fe69dd5aa5d155ac2d6e
Quang Trung <gnurt10@gmail.com>
2022-08-01 22:21:00 +0700

    Update 1869-longer-contiguous-segments-of-ones-than-zeros.ts

--- code/string/1869-longer-contiguous-segments-of-ones-than-zeros.ts
+++ code/string/1869-longer-contiguous-segments-of-ones-than-zeros.ts
@@ -1,22 +1,13 @@
 function checkZeroOnes(s: string): boolean {
-    let longest1 = 0;
-    let longest0 = 0;
-    
-    let count1 = 0;
-    let count0 = 0;
-    
-    for (let i = 0; i < s.length; i++) {
-        const bit = s.charAt(i);
-        
-        if (bit === "1") count1++;
-        else count1 = 0;
-        
-        if (bit === "0") count0++;
-        else count0 = 0;
-        
-        longest1 = Math.max(longest1, count1);
-        longest0 = Math.max(longest0, count0);
-    }
-    
-    return longest1 > longest0;
-};
+  let count1 = 0, count0 = 0, longest0 = 0, longest1  = 0;
+  for (let i = 0; i < s.length; i++) {
+    const bit = s.charAt(i);
+    if (bit === '1') { count1 += 1;}
+    else { count1 = 0; }
+    if (bit === '0') { count0 += 1;}
+    else { count0 = 0; }
+    longest0 = Math.max(longest0, count0);
+    longest1 = Math.max(longest1, count1);
+  }
+  return longest1 > longest0;
+}
