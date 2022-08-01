5318379d5722122cfe16c9e3adaed641df31f152
Quang Trung <gnurt10@gmail.com>
2022-08-01 22:03:57 +0700

    Create 1399-count-largest-group.ts

---
+++ code/linked-list/1399-count-largest-group.ts
@@ -0,0 +1,25 @@
+function countLargestGroup(n: number): number {
+    let count = {};
+    let largestSize = -Infinity;
+    let output = 0;
+    
+    for (let i = 1; i <= n; i++) {
+        let sum = 0;
+        let num = i;
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+        count[sum] ? count[sum] += 1 : count[sum] = 1;
+        if (count[sum] > largestSize) {
+            largestSize = count[sum];
+        }
+    }
+    
+    for (let key in count) {
+        if (count[key] === largestSize) {
+            output++;
+        }
+    }
+    return output++;
+};
