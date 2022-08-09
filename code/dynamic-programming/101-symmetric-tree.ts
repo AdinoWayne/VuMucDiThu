82370890535e039f7896a5d13bb64fb45c7fa948
Quang Trung <gnurt10@gmail.com>
2022-08-09 22:16:06 +0700

    Create 101-symmetric-tree.ts

---
+++ code/dynamic-programming/101-symmetric-tree.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+  const q = [root, root];
+  while (q.length) {
+    const [l, r] = [q.shift(), q.shift()];
+    if (!l && !r) continue;
+    if (!!l !== !!r || l.val !== r.val) return false;
+    q.push(l.left, r.right, l.right, r.left);
+  }
+  
+  return true;
+};
