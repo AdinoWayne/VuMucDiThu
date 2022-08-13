c757f3c4aab9fefac0a98ab5348d3a68f1fe75cb
Quang Trung <gnurt10@gmail.com>
2022-08-13 08:25:52 +0700

    Create 144-binary-tree-preorder-traversal.ts

---
+++ code/dynamic-programming/144-binary-tree-preorder-traversal.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    let output = [];
+    function foo (node) {
+        if(!node) return;
+        output.push(node.val);
+        foo(node.left);
+        foo(node.right);
+    }
+    foo(root);
+    return output;
+};
