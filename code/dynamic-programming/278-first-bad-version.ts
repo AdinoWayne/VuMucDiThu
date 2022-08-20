7f6c3cd17411c60282e63df45c4e1f6b7918ba66
Quang Trung <gnurt10@gmail.com>
2022-08-20 12:04:22 +0700

    Create 278-first-bad-version.ts

---
+++ code/dynamic-programming/278-first-bad-version.ts
@@ -0,0 +1,27 @@
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function(isBadVersion: any) {
+
+    return function(n: number): number {
+        let min = null;
+        let start = 1;
+        let end = n;
+        
+        while(start <= end) {
+            const mid = Math.floor((start + end) / 2);
+            
+            if(isBadVersion(mid)) {
+                min = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return min;
+    };
+};
