d12e1e938f78f39c1781d64fa0ae8b8a46719d3c
Quang Trung <gnurt10@gmail.com>
2022-08-24 22:02:24 +0700

    Create 404-sum-of-left-leaves.ts

---
+++ code/404-sum-of-left-leaves.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function sumOfLeftLeaves(root: TreeNode | null): number {
+    if(root == null) return 0;
+    let ans = 0;
+    if(root.left != null) {
+        if(root.left.left == null && root.left.right == null) ans += root.left.val;
+        else ans += sumOfLeftLeaves(root.left);
+    }
+    ans += sumOfLeftLeaves(root.right);
+    
+    return ans;
+};
