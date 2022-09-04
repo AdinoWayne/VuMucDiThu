75f091d2a7e92d2417cbe2760bb749f0ce1a47b3
Quang Trung <gnurt10@gmail.com>
2022-09-04 10:08:27 +0700

    Create 762-prime-number-of-set-bits-in-binary-representation.ts

---
+++ code/dynamic-programming/762-prime-number-of-set-bits-in-binary-representation.ts
@@ -0,0 +1,17 @@
+function countPrimeSetBits(left: number, right: number): number {
+    let result = 0;
+    let primes = [2, 3, 5, 7, 11, 13, 17, 19, 23];
+
+    for(let i = left; i <= right; i++){
+      let num2 = i.toString(2);
+      let count = 0;
+
+      num2.split('').forEach(sym => {
+        if(sym === '1') count++;
+      });
+
+      if(primes.indexOf(count) > -1) result++;
+    }
+
+    return result;
+};
