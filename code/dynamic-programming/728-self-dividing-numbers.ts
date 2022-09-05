932640d057213e7ffaee4b17c206b7caeb128981
Quang Trung <gnurt10@gmail.com>
2022-09-05 20:31:09 +0700

    Create 728-self-dividing-numbers.ts

---
+++ code/dynamic-programming/728-self-dividing-numbers.ts
@@ -0,0 +1,23 @@
+function selfDividingNumbers(left: number, right: number): number[] {
+    const result = [];
+    // Loop throuh all numbers
+    for (let number = left; number <= right; number++) {
+        let digits = number;
+        let valid = true;
+        // Loop through all number digits
+        while(digits > 1) {
+            let lastDigit = digits % 10;
+            // Check if zero or not self dividing
+            if (!lastDigit || number % lastDigit !== 0) {
+                valid = false;
+                break;
+            }
+            // Remove last digit from the number
+            digits = Math.floor(digits/10);
+        }
+        if (valid) {
+            result.push(number);
+        }
+    }
+    return result;
+};
