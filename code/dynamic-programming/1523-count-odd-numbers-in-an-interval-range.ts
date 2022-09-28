4d8c692d561f6d3fb82f20fb92fc78085985e1d9
Quang Trung <gnurt10@gmail.com>
2022-09-28 22:39:42 +0700

    Update 1523-count-odd-numbers-in-an-interval-range.ts

--- code/dynamic-programming/1523-count-odd-numbers-in-an-interval-range.ts
+++ code/dynamic-programming/1523-count-odd-numbers-in-an-interval-range.ts
@@ -1,10 +1,5 @@
 function countOdds(low: number, high: number): number {
-    if (low % 2 === 0) {
-        low++  
-    } 
-    if (high % 2 === 0) {
-        high-- 
-    } 
-    
-    return (high - low) / 2 + 1
+    if(low%2==1){low--};
+    if(high%2==1){high++};
+    return (high-low)/2;
 };
