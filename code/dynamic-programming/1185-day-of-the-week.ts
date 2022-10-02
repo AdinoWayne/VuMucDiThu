1df271eb35093c0d624ec3831bbcfde9e87f1a54
Quang Trung <gnurt10@gmail.com>
2022-10-02 13:52:13 +0700

    Update 1185-day-of-the-week.ts

--- code/dynamic-programming/1185-day-of-the-week.ts
+++ code/dynamic-programming/1185-day-of-the-week.ts
@@ -7,3 +7,16 @@ function dayOfTheWeek(d: number, m: number, y: number): string {
 };
 
 const LIST = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+// OR Zeller’s Congruence
+
+function dayOfTheWeek2(d: number, m: number, y: number): string {
+  const LIST = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  if (m < 3) { --y; m += 12; }
+  const c = Math.floor(y / 100);
+  const k = y % 100;
+  const w = d + 13 * (m + 1) / 5 + k + Math.floor(k / 4) +
+                              Math.floor(c / 4) + 5 * c;
+  return LIST[Math.floor(w) % 7];
+};
