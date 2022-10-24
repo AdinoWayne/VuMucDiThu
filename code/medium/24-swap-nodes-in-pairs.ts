11cdc8fc09a2e84121a1a4b315a5a3e8ddc3c97e
Quang Trung <gnurt10@gmail.com>
2022-10-24 21:51:35 +0700

    Create 24-swap-nodes-in-pairs.ts

---
+++ code/medium/24-swap-nodes-in-pairs.ts
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function swapPairs(head: ListNode | null): ListNode | null {
+    if(!head || !head.next) return head;
+    var v1 = head, v2 = head.next, v3 = v2.next;
+    v2.next = v1;
+    v1.next = swapPairs(v3);
+    return v2;
+};
