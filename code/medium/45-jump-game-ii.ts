c11d27a15759844f1fbf80b8a8870893d1a6867b
Quang Trung <gnurt10@gmail.com>
2022-10-29 08:31:20 +0700

    Create 45-jump-game-ii.ts

---
+++ code/medium/45-jump-game-ii.ts
@@ -0,0 +1,15 @@
+function jump(nums: number[]): number {
+	if(nums.length === 1) return 0;
+	let jumps = 0;
+	let maxReach = nums[0];
+	let steps = nums[0];
+	for(let i = 1; i < nums.length - 1; i++){
+		maxReach = Math.max(maxReach, i + nums[i])
+		steps--;
+		if(steps === 0){
+			jumps++;
+			steps = maxReach - i;
+		}
+	}
+	return jumps + 1; 
+};
