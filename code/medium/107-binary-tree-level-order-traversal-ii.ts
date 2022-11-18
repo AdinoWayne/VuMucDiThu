21fcd79cee9114834682ebd8e2bac23830e667c8
Quang Trung <gnurt10@gmail.com>
2022-11-18 21:17:04 +0700

    Create 107-binary-tree-level-order-traversal-ii.ts

---
+++ code/medium/107-binary-tree-level-order-traversal-ii.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrderBottom(root: TreeNode | null): number[][] {
+    if (root === null) { return []; }
+    let result = [];
+    let queue = [root];
+    while(queue.length > 0) {
+        let size = queue.length;
+        let current = [];
+        for (let i = 0; i < size; i++) {
+            let head = queue.shift();
+            current.push(head.val);
+            if(head.left !== null) { queue.push(head.left); }
+            if(head.right !== null) { queue.push(head.right); }
+        }
+        result.unshift(current);
+    }
+    return result;
+};
