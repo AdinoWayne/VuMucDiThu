09d51aa9b06ba0211a81f1bd1e570bf10838c0a1
Quang Trung <gnurt10@gmail.com>
2022-11-22 09:20:05 +0700

    Create 122-best-time-to-buy-and-sell-stock-ii.ts

---
+++ code/medium/122-best-time-to-buy-and-sell-stock-ii.ts
@@ -0,0 +1,14 @@
+function maxProfit(prices: number[]): number {
+    let profit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        let prev = prices[i - 1];
+        let current = prices[i];
+
+        if (prev < current) {
+            profit += current - prev;
+        }
+    }
+    
+    return profit;
+};
