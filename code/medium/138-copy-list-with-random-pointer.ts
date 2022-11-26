e27d9ea7689b35e0af5168e938ada877094c442f
Quang Trung <gnurt10@gmail.com>
2022-11-26 12:32:15 +0700

    Create 138-copy-list-with-random-pointer.ts

---
+++ code/medium/138-copy-list-with-random-pointer.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for Node.
+ * class Node {
+ *     val: number
+ *     next: Node | null
+ *     random: Node | null
+ *     constructor(val?: number, next?: Node, random?: Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *         this.random = (random===undefined ? null : random)
+ *     }
+ * }
+ */
+
+function copyRandomList(head: Node | null): Node | null {
+        if(!head) {
+      return null;
+    }
+    const clones = new Map();
+    let n = head;
+    while(n) {
+      clones.set(n, new Node(n.val));
+      n = n.next
+    }
+    n = head;
+    while(n) {
+      clones.get(n).next = clones.get(n.next) || null;
+      clones.get(n).random = clones.get(n.random) || null;
+      n = n.next
+    }
+    return clones.get(head);
+};
