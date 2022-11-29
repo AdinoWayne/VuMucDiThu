167893217caf64cc2edc979199e0f43c378080dd
Quang Trung <gnurt10@gmail.com>
2022-11-29 10:26:08 +0700

    Update 89-gray-code.ts

--- code/medium/89-gray-code.ts
+++ code/medium/89-gray-code.ts
@@ -7,3 +7,22 @@ function grayCode(n: number): number[] {
     }
     return res;
 };
+
+// another solution 
+// Time complexity : O(2^n).
+// Space complexity : O(2^n).
+
+function grayCode(n) {
+    var res = [0];
+    helper(n, res, 0);
+    return res;
+};
+
+var helper = function (n, res, num) {
+    if (num >= n) return;
+    var len = res.length;
+    for (var i = len - 1; i >= 0; i--) {
+      res.push(res[i] + len);
+    }
+    helper(n, res, num + 1);
+};
