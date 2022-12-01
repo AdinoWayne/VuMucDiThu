d737974ace77196718c23e592a984923181238fe
Quang Trung <gnurt10@gmail.com>
2022-12-01 18:28:38 +0700

    Create 153-find-minimum-in-rotated-sorted-array.ts

---
+++ code/medium/153-find-minimum-in-rotated-sorted-array.ts
@@ -0,0 +1,10 @@
+function findMin(nums: number[]): number {
+  let l = 0;
+  let r = nums.length - 1;
+  while (l < r) {
+    const m = ~~((l + r) / 2);
+    if (nums[m] > nums[r]) l = m + 1;
+    else r = m;
+  }
+  return nums[l];
+};
