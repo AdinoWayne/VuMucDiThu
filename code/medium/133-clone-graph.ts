a9125d79b539a6840e701a7b9dbfeaab276ca086
Quang Trung <gnurt10@gmail.com>
2022-12-16 22:58:04 +0700

    Update 133-clone-graph.ts

--- code/medium/133-clone-graph.ts
+++ code/medium/133-clone-graph.ts
@@ -1,3 +1,16 @@
+/**
+ * Definition for Node.
+ * class Node {
+ *     val: number
+ *     neighbors: Node[]
+ *     constructor(val?: number, neighbors?: Node[]) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.neighbors = (neighbors===undefined ? [] : neighbors)
+ *     }
+ * }
+ */
+
+function cloneGraph(node: Node | null): Node | null {
     // If start node is null than we can't do any cloning
     let start = node; 
     if (start === null) return null;
@@ -37,3 +50,4 @@
         }
     }
    return vertexMap.get(start); 
+};
