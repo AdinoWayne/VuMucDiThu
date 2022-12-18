1375fccda4e6cb14338ab94d3c17340047fa3fc7
Quang Trung <gnurt10@gmail.com>
2022-12-18 13:45:02 +0700

    Create 229-majority-element-ii.ts

---
+++ code/medium/229-majority-element-ii.ts
@@ -0,0 +1,12 @@
+function majorityElement(nums: number[]): number[] {
+    const memo = {}
+    for (let i = 0; i < nums.length; i++) {
+      if (!memo[nums[i]]) memo[nums[i]] = 0
+      memo[nums[i]]++
+    }
+    const ret = []
+    for (const m in memo) {
+      if (memo[m] > nums.length / 3) ret.push(parseInt(m))
+    }
+    return ret
+};
