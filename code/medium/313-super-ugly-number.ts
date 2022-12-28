862b5593af571e031f43a42120fbfce157118788
Quang Trung <gnurt10@gmail.com>
2022-12-28 10:43:38 +0700

    Create 313-super-ugly-number.ts

---
+++ code/medium/313-super-ugly-number.ts
@@ -0,0 +1,32 @@
+function nthSuperUglyNumber(n: number, primes: number[]): number {
+    let indeces = [];
+    let currents = [];
+    let i, l;
+    // fill up our indeces array with zeroes to start
+    for (i = 0, l = primes.length; i < l; i++) {
+        indeces[i] = 0;
+    }
+    let out = [1]; // we will record or prime multiples here
+    
+    while (!out[n-1]) { 
+        // simply calculate the next multiple of all the primes
+        for (i = 0, l = primes.length; i < l; i++) {
+            currents[i] = out[indeces[i]] * primes[i]; 
+            // so in the case of 2,3,5 , currents would come out as [2,3,5] first round.
+            // Then we use 2 and increase the index of that prime. Next round it would calculate to [4,3,5], then use 3, then calculate to [4,6,5] etc
+        }
+        
+        // select the lowest one next so we stay in order
+        let next = Math.min(...currents);
+        out.push(next);
+        
+        // and for that prime we increase the index so next round we find the next multiple of that prime
+        for (i = 0, l = primes.length; i < l; i++) {
+            if (next === out[indeces[i]] * primes[i]) {
+                indeces[i] = indeces[i] + 1;
+            }
+        }
+    }
+    
+    return out[n-1];
+};
