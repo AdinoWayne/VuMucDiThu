8f6a18c5dc4b9f26c006a23c130665d3710afb3a
Quang Trung <gnurt10@gmail.com>
2022-12-31 12:39:22 +0700

    Create 319-bulb-switcher.ts

---
+++ code/medium/319-bulb-switcher.ts
@@ -0,0 +1,14 @@
+function bulbSwitch(n: number): number {
+    let lo = 0;
+    let hi = n;
+    while(lo <= hi) {
+         let mid = Math.floor((lo + hi) / 2);
+         if (mid * mid > n) {
+          hi = mid - 1;   
+         }
+         else {
+           lo = mid + 1;  
+         } 
+    }
+    return Math.floor(hi);
+};
