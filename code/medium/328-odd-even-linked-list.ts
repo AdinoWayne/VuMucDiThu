69b6df1d1b57d84e547c28968a51e087bc6f9ec7
Quang Trung <gnurt10@gmail.com>
2023-01-01 13:39:58 +0700

    Create 328-odd-even-linked-list.ts

---
+++ code/medium/328-odd-even-linked-list.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function oddEvenList(head: ListNode | null): ListNode | null {
+    if(head != null){
+        
+        let odd = head, even = head.next, evenHead = even;
+        
+        while(even && even.next != null){
+            odd.next = even.next;
+		    odd = odd.next;
+		    even.next = odd.next;
+		    even = even.next;
+        }
+        odd.next = evenHead;
+    }
+    return head;
+};
