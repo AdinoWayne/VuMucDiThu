aa4d4a36f77132e8d6af19326f6991cbf31c1860
Quang Trung <gnurt10@gmail.com>
2023-01-02 09:50:16 +0700

    Create 334-increasing-triplet-subsequence.ts

---
+++ code/medium/334-increasing-triplet-subsequence.ts
@@ -0,0 +1,16 @@
+function increasingTriplet(nums: number[]): boolean {
+  let firstNumber = Infinity;
+  let secondNumber = Infinity;
+  
+  for (let currentNumber of nums) {
+    if (currentNumber > secondNumber && currentNumber > firstNumber) {
+      return true;
+    }
+    if (currentNumber > firstNumber) {
+      secondNumber = currentNumber;
+    } else {
+      firstNumber = currentNumber;
+    }
+  }
+  return false;
+};
