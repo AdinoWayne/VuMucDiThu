4f63c824fb8ca23cc8f138dae27bf3f84e74c8f3
Quang Trung <gnurt10@gmail.com>
2023-01-02 09:49:40 +0700

    Create 331-verify-preorder-serialization-of-a-binary-tree.ts

---
+++ code/medium/331-verify-preorder-serialization-of-a-binary-tree.ts
@@ -0,0 +1,11 @@
+function isValidSerialization(preorder: string): boolean {
+    const arr = preorder.split(',');
+    let i = 0;
+    
+    function callDFS() {
+        if(i >= arr.length) return false;        
+        if(arr[i++] === '#') return true;
+        return callDFS() && callDFS();
+    }
+    return callDFS() && i == arr.length;
+};
