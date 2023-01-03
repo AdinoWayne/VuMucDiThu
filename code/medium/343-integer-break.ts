9b5d106478af310f34ac0d92066e63eb0f1760eb
Quang Trung <gnurt10@gmail.com>
2023-01-03 09:57:47 +0700

    Create 343-integer-break.ts

---
+++ code/medium/343-integer-break.ts
@@ -0,0 +1,14 @@
+function integerBreak(n: number): number {
+  if (n < 4) return n - 1;
+    
+  let times3 = Math.floor(n / 3);
+  let diff = n - (times3 * 3);
+
+  if (diff == 0) {
+    return Math.pow(3, times3);
+  } else if (diff == 1) {
+    return Math.pow(3, times3 - diff) * (3 + diff)
+  } else { //diff == 2
+    return Math.pow(3, times3) / (3 + diff) * 10
+  }
+};
