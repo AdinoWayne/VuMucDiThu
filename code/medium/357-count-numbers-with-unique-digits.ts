a9db2f6b055ad57d22aa8f02a863d02aff37391c
Quang Trung <gnurt10@gmail.com>
2023-01-05 10:13:43 +0700

    Create 357-count-numbers-with-unique-digits.ts

---
+++ code/medium/357-count-numbers-with-unique-digits.ts
@@ -0,0 +1,9 @@
+function countNumbersWithUniqueDigits(n: number): number {
+    var dp = [1, 10];
+    var sum = [1, 11];
+    for (var i = 2; i <= n; i++) {
+        dp[i] = sum[i - 1] + (10 - i) * (dp[i - 1]);
+        sum[i] = sum[i - 1] + dp[i];
+    }
+    return dp[n];
+};
