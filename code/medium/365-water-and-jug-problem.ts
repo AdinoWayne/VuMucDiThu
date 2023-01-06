e14e1a3f772a2b86c23cbfa18847251a1a7ed66f
Quang Trung <gnurt10@gmail.com>
2023-01-06 10:01:42 +0700

    Create 365-water-and-jug-problem.ts

---
+++ code/medium/365-water-and-jug-problem.ts
@@ -0,0 +1,10 @@
+function canMeasureWater(jug1Capacity: number, jug2Capacity: number, targetCapacity: number): boolean {
+    if (targetCapacity >= jug1Capacity + jug2Capacity)
+        return targetCapacity === jug1Capacity + jug2Capacity;
+
+    var fetchGCD = (x, y) => {
+        return x === 0 ? y : fetchGCD(y % x, x);
+    };
+
+    return targetCapacity % fetchGCD(jug1Capacity, jug2Capacity) == 0;
+};
