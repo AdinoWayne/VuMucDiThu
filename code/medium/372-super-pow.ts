1f7554021d91593dec961fa90ca1f51f01e10419
Quang Trung <gnurt10@gmail.com>
2023-01-07 13:14:48 +0700

    Create 372-super-pow.ts

---
+++ code/medium/372-super-pow.ts
@@ -0,0 +1,18 @@
+function superPow(a: number, b: number[]): number {
+	const MOD = 1337;
+	const pow = (num, n) => {
+		let result = 1;
+		for (let index = 0; index < n; index++) {
+			result = result * num % MOD;
+		}
+		return result;
+	};
+
+	return b.reduceRight((result, n) => {
+		a %= MOD;
+		const powNum = result * pow(a, n) % MOD;
+
+		a = pow(a, 10);
+		return powNum;
+	}, 1);
+};
