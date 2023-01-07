3680291c8f0eb3a3bf4d200eae1f81a7bce68711
Quang Trung <gnurt10@gmail.com>
2023-01-07 13:17:34 +0700

    Create 371-sum-of-two-integers.ts

---
+++ code/medium/371-sum-of-two-integers.ts
@@ -0,0 +1,9 @@
+function getSum(a: number, b: number): number {
+      let carry;
+      while((a & b) !== 0){
+          carry = (a & b) << 1;
+          a = a ^ b;
+          b = carry;
+      }
+      return a ^ b;
+};
