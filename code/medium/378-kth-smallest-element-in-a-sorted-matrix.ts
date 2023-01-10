16304b339b26ab3feb82f18814e70a737455ec06
Quang Trung <gnurt10@gmail.com>
2023-01-10 17:55:55 +0700

    Create 378-kth-smallest-element-in-a-sorted-matrix.ts

---
+++ code/medium/378-kth-smallest-element-in-a-sorted-matrix.ts
@@ -0,0 +1,11 @@
+function kthSmallest(matrix: number[][], k: number): number {
+    const pq = new MaxPriorityQueue();
+    for(let i = 0 ; i < matrix.length; i++) {
+        for(let j  = 0 ; j < matrix.length; j++) {
+            pq.enqueue(matrix[i][j]);
+            if(pq.size() > k) pq.dequeue();
+        }
+    }
+    
+    return pq.dequeue().element;
+};
