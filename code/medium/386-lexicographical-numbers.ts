e669000bf7dcda79b4281b11d08478da53b74108
Quang Trung <gnurt10@gmail.com>
2023-01-12 18:15:03 +0700

    Create 386-lexicographical-numbers.ts

---
+++ code/medium/386-lexicographical-numbers.ts
@@ -0,0 +1,23 @@
+function lexicalOrder(n: number): number[] {
+    const arr = [];
+    
+    function dfs(baseIndex) {
+        if (baseIndex * 10 > n) {
+            return;
+        }
+        
+        for(let i = baseIndex * 10; i < baseIndex * 10 + 10 && i <= n; i++)  {
+           arr.push(i);
+           dfs(i);
+        }
+    }
+    
+    let stack = [];
+    
+    for(let i = 1; i <= 9 && i <= n; i++) {
+        arr.push(i);        
+        dfs(i);
+    }
+    
+    return arr;
+};
