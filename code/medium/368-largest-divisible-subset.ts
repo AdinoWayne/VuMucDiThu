5523d293fa1fb1308a52bc13c4aabba77cb4eb9a
Quang Trung <gnurt10@gmail.com>
2023-01-30 18:32:51 +0700

    Update 368-largest-divisible-subset.ts

--- code/medium/368-largest-divisible-subset.ts
+++ code/medium/368-largest-divisible-subset.ts
@@ -1,6 +1,11 @@
 function largestDivisibleSubset(nums: number[]): number[] {
+    // Sort all array elements in increasing order. The purpose of sorting is to make sure that all divisors of an element appear before it.
     nums.sort((a,b)=>a-b)
+    // Create an array dp[] of same size as input array.
+    // dp[i] stores size of divisible subset ending with nums[i] (In sorted array). The minimum value of dp[i] would be 1.
     var dp = Array.from({length: nums.length}, (v,k) => [nums[k]]);
+    // Traverse all array elements.
+    // For every element, find a divisor nums[j] with largest value of dp[j] and store the value of dp[i] as dp[j] + 1.
     for (let i=nums.length-1; i>=0; i--) {
         for (let j = i+1; j<nums.length; j++) {
             if (dp[i].length<=dp[j].length && dp[j][0]%nums[i]===0) {
