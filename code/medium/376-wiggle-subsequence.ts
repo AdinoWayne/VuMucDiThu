a88a7279aa2c3c04cf70a34eb61cd1b0e561fd1b
Quang Trung <gnurt10@gmail.com>
2023-02-01 15:54:30 +0700

    Update 376-wiggle-subsequence.ts

--- code/medium/376-wiggle-subsequence.ts
+++ code/medium/376-wiggle-subsequence.ts
@@ -10,3 +10,15 @@ function wiggleMaxLength(nums: number[]): number {
         }
         return Math.max(down, up);
 };
+
+// When nums[i]>nums[i-1]:it means it wiggles up. So the element before it must be in down position.
+// Therefore, up[i] = down[i-1]+1 and down[i] will be same as down[i-1].
+// When nums[i]<nums[i-1]: it means the wiggles down, the element before it must be in up position.
+// Therefore down[i] = 1+up[i-1] and up[i] will be same as up[i-1].
+
+// input : [1, 7, 4, 9, 2, 5]
+// UP: [1, 2, 2, 4, 4, 6]
+// DOWN: [1, 1, 3, 3, 5, 5]
+
+// Time complexity: O(n), n is the length of the array
+// Space complexity: O(n), n is the length of the array
