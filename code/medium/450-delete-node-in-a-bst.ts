83bd8a8a144a9555f8173d382dbe12f33645ab3d
Quang Trung <gnurt10@gmail.com>
2023-02-03 10:29:46 +0700

    Create 450-delete-node-in-a-bst.ts

---
+++ code/medium/450-delete-node-in-a-bst.ts
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
+    function callDFS(node) {
+        if(!node) return null;
+        if(node.val === key) {
+            if(!node.left) return node.right;
+            if(!node.right) return node.left;
+            let curr = node.right;
+            while(curr.left) curr = curr.left;
+            curr.left = node.left;
+            return node.right;
+        }
+        if(key > node.val) node.right = callDFS(node.right);
+        else node.left = callDFS(node.left);
+        return node;
+    }
+    return callDFS(root)
+};
