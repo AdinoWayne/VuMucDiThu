4eb3ab2bf5fee227daa06295514ef69c3308727b
Quang Trung <gnurt10@gmail.com>
2023-02-04 16:46:35 +0700

    Create 453-minimum-moves-to-equal-array-elements.ts

---
+++ code/medium/453-minimum-moves-to-equal-array-elements.ts
@@ -0,0 +1,13 @@
+function minMoves(nums: number[]): number {
+  nums.sort((a, b) => b - a)
+
+  let moves = 0
+
+  let index = 1
+  while (index < nums.length) {
+    moves += (nums[index - 1] - nums[index]) * index
+    ++index
+  }
+
+  return moves
+};
