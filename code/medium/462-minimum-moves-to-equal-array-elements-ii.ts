8d1bbd588e8bdd6186a5b0482d578149d30c4b1f
Quang Trung <gnurt10@gmail.com>
2023-02-06 18:56:16 +0700

    Create 462-minimum-moves-to-equal-array-elements-ii.ts

---
+++ code/medium/462-minimum-moves-to-equal-array-elements-ii.ts
@@ -0,0 +1,10 @@
+function minMoves2(nums: number[]): number {
+  nums.sort((a, b) => a - b); //! sort the array
+  let mid = nums[Math.floor(nums.length / 2)]; //! get the middle element
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const need_to_get = mid - nums[i]; //! get the difference between the middle element and the current element
+    count += Math.abs(need_to_get);
+  }
+  return count;
+};
