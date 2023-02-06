daaaed687437f7b5e62732227ba1ce91b19b9bb1
Quang Trung <gnurt10@gmail.com>
2023-02-06 19:34:32 +0700

    Update 395-longest-substring-with-at-least-k-repeating-characters.ts

--- code/medium/395-longest-substring-with-at-least-k-repeating-characters.ts
+++ code/medium/395-longest-substring-with-at-least-k-repeating-characters.ts
@@ -19,3 +19,7 @@ function longestSubstring(s: string, k: number): number {
     }
     return s.length;
 };
+
+// The time complexity of this function is O(n^2), where n is the length of the input string s.
+// This is because the function has a nested loop, in which the inner loop splits the input string into substrings using the split() method, and the outer loop calls the longestSubstring() function recursively on each of these substrings.
+// The split() method has a time complexity of O(n), and the number of recursive calls to the longestSubstring() function is proportional to the length of the input string, so the overall time complexity of the function is O(n^2).
