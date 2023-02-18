6802b04d30ddb00b25bfd4d0c389c20e6a9140ba
Quang Trung <gnurt10@gmail.com>
2023-02-18 15:24:09 +0700

    Update 436-find-right-interval.ts

--- code/medium/436-find-right-interval.ts
+++ code/medium/436-find-right-interval.ts
@@ -36,3 +36,9 @@ function search(arr, start, end, val) {
 
     return ans;
 }
+// Create an array of starting points where a[i] = starting point of interval i.
+// Use a Map to keep track of index of each starting point (Each starting point is unique).
+// Sort the array of starting points
+// Then for each end point 'intervals[i][1]':
+// perform binary search on array of starting points to get the minimum start point 'a' such that a >= intervals[i][1]
+// if such a start point is found, add its index (use the Map to get its index) to result array, else add -1 to result array
