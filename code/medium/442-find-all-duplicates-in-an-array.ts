6fdd459f86611f13d02d76268c4e436513b7a10a
Quang Trung <gnurt10@gmail.com>
2023-02-20 22:35:57 +0700

    Update 442-find-all-duplicates-in-an-array.ts

--- code/medium/442-find-all-duplicates-in-an-array.ts
+++ code/medium/442-find-all-duplicates-in-an-array.ts
@@ -1,23 +1,19 @@
 function findDuplicates(nums: number[]): number[] {
     const duplicates = []; 
-
     for (const currentNum of nums) {
-        // Get the index that currentNum points to:
-        // Note: we have to use Math.abs because the currentNum 
-        // could have been flipped:
         const otherIndex = Math.abs(currentNum) - 1;
 
-        // If the number at otherIndex > 0 (hasn't been flipped):
         if (nums[otherIndex] > 0) {
-            // Flip it to negative to mark that we've visited it:
             nums[otherIndex] = nums[otherIndex] * (-1);
         }
-        // If the number at otherIndex < 0 (has been flipped):
         else {
-            // We know that we've visited this currentNum before because
-            // it leads us to the same number.
             duplicates.push(Math.abs(currentNum));
         }
     }
     return duplicates;
 };
+
+// when find a number i, flip the number at position i-1 to negative. 
+// if the number at position i-1 is already negative, i is the number that occurs twice.
+// O(n) Time
+// O(1) Space
