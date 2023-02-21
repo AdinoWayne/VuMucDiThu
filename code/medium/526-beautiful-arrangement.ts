f2f4282eea2da011cff022943e1b331081760230
Quang Trung <gnurt10@gmail.com>
2023-02-21 20:41:09 +0700

    Create 526-beautiful-arrangement.ts

---
+++ code/medium/526-beautiful-arrangement.ts
@@ -0,0 +1,31 @@
+function countArrangement(n: number): number {
+    let count = 0;
+    const nums = [];
+    for(let i = 1; i <= n; i++) {
+        nums.push(i);
+    }
+    
+    function findArrangements(index) {        
+        if (index == nums.length) {
+            count++;
+        }
+        
+        for(let i = index; i < nums.length; i++) {     
+            swap(nums, i, index);
+            if ((index + 1) % nums[index] === 0 ||
+                nums[index] % (index + 1) === 0) { 
+                findArrangements(index + 1);
+            }
+            swap(nums, i, index);
+        }
+    }
+    findArrangements(0);
+    
+    return count;
+};
+
+function swap(nums, one, two) {
+    let temp = nums[one];
+    nums[one] = nums[two];
+    nums[two] = temp;
+}
