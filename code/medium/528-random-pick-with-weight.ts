657b6ce8dcb717433428942be91232a6fcaf3d51
Quang Trung <gnurt10@gmail.com>
2023-02-22 21:15:27 +0700

    Create 528-random-pick-with-weight.ts

---
+++ code/medium/528-random-pick-with-weight.ts
@@ -0,0 +1,32 @@
+class Solution {
+    arr: number[]
+    constructor(w: number[]) {
+        this.arr = [];
+        let sum = 0;
+        for(let i=0; i<w.length; i++) {
+            sum += w[i];
+            this.arr.push(sum);
+        }
+    }
+
+    pickIndex(): number {
+        let target = Math.random() * this.arr[this.arr.length-1];
+        
+        let low = 0;
+        let high = this.arr.length - 1;
+        while (low < high) {
+            let mid = Math.floor(low + ((high-low)/2));
+            if(this.arr[mid] <= target)
+                low = mid + 1;
+            else 
+                high = mid;
+        }
+        return high;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(w)
+ * var param_1 = obj.pickIndex()
+ */
