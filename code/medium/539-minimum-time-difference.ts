ea3df7be5f72b2f5a737d917f8b869d96460c9cb
Quang Trung <gnurt10@gmail.com>
2023-02-25 10:15:23 +0700

    Create 539-minimum-time-difference.ts

---
+++ code/medium/539-minimum-time-difference.ts
@@ -0,0 +1,15 @@
+function findMinDifference(timePoints: string[]): number {
+  timePoints.sort();
+  const arrPoint = timePoints.map(el => {
+    let [hours, minutes] = el.split(':');
+    return Number(hours) * 60 + Number(minutes);
+  })
+  
+  timePoints.push(timePoints[0] + 1440);
+  let minDiff = Infinity;
+  for (let i = 1; i < timePoints.length; i ++) {
+    minDiff = Math.min(minDiff, arrPoint[i] - arrPoint[i - 1]);
+  }
+  
+  return minDiff;
+};
