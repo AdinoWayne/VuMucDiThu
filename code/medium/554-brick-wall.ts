4defe460a044a79b33330ee7f4c787c2660714e2
Quang Trung <gnurt10@gmail.com>
2023-02-27 21:00:13 +0700

    Create 554-brick-wall.ts

---
+++ code/medium/554-brick-wall.ts
@@ -0,0 +1,13 @@
+function leastBricks(wall: number[][]): number {
+    const counts = {};
+    let max = 0;
+    for (let row of wall) {
+        let sum = 0;
+        for (let i = 0; i < row.length - 1; i++) {
+            sum += row[i];
+            counts[sum] = (counts[sum] || 0) + 1;
+            max = Math.max(max, counts[sum]);
+        }
+    }
+    return wall.length - max;
+};
