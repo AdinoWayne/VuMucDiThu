d2103ef9fac438f8b16d48c8ae5e4ac186cd79ba
Quang Trung <gnurt10@gmail.com>
2023-03-02 11:18:02 +0700

    Update 474-ones-and-zeroes.ts

--- code/medium/474-ones-and-zeroes.ts
+++ code/medium/474-ones-and-zeroes.ts
@@ -23,3 +23,19 @@ const getZeroOne = (str) => {
     one: length - zero
   }
 }
+
+// This is a dynamic programming solution to the "Ones and Zeroes" problem.
+// The function findMaxForm takes in an array of strings strs, and two integers m and n,
+//   and returns the maximum number of strings that can be formed with m zeros and n ones.
+
+// The function initializes a two-dimensional dp array with m+1 rows and n+1 columns,
+// where dp[i][j] represents the maximum number of strings that can be formed with i zeros and j ones.
+
+// Then, for each string in strs, the function gets the number of zeros and ones in the string using the helper function getZeroOne,
+// and updates the dp array using a nested loop. Specifically, it iterates over all possible combinations of zeros and ones,
+// and sets the current dp value to the maximum of the current value and the value obtained by adding the current string to the combination that leaves enough zeros and ones for the remaining strings.
+
+// Finally, the function returns the dp[n][m] value, which represents the maximum number of strings that can be formed with m zeros and n ones.
+
+// The time complexity of this function is O(kLmn), where k is the length of strs, L is the average length of the strings in strs,
+// m is the number of zeros, and n is the number of ones. The space complexity is also O(mn), since the dp array has m+1 rows and n+1 columns.
