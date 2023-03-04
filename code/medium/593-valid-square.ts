5726e58ae91047ec561ffdbabbf36ab1da0e7aad
Quang Trung <gnurt10@gmail.com>
2023-03-04 14:48:47 +0700

    Create 593-valid-square.ts

---
+++ code/medium/593-valid-square.ts
@@ -0,0 +1,18 @@
+function validSquare(p1: number[], p2: number[], p3: number[], p4: number[]): boolean {
+	const distance = (a, b) => {
+		const [aX, aY] = a;
+		const [bX, bY] = b;
+		return (aX - bX) ** 2 + (aY - bY) ** 2;
+	};
+
+	const set = new Set([
+		distance(p1, p2),
+		distance(p1, p3),
+		distance(p1, p4),
+		distance(p2, p3),
+		distance(p2, p4),
+		distance(p3, p4),
+	]);
+
+	return !set.has(0) && set.size === 2;
+};
