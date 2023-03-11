8b824d24402438baf621c87935b050078e446f4d
Quang Trung <gnurt10@gmail.com>
2023-03-11 16:31:47 +0700

    Create 649-dota2-senate.ts

---
+++ code/medium/649-dota2-senate.ts
@@ -0,0 +1,17 @@
+function predictPartyVictory(senate: string): string {
+    var map={}, m=senate.length, q1=[], q2=[];
+    for(var i=0; i<m; i++){
+        if(senate.charAt(i) == 'R') q1.push(i)
+        else{
+            q2.push(i);
+        }
+    }
+    while(q1.length>0 &&q2.length>0){
+        let v1=q1.shift(), v2=q2.shift();
+        if(v1<v2) q1.push(v1+m)
+        else{
+            q2.push(v2+m);
+        }
+    }
+    return q1.length> q2.length? "Radiant": "Dire"
+};
