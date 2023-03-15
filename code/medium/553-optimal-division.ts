780586a074c6ac6b59978c46a8fc3d4fd2be6758
Quang Trung <gnurt10@gmail.com>
2023-03-15 18:48:35 +0700

    Update 553-optimal-division.ts

--- code/medium/553-optimal-division.ts
+++ code/medium/553-optimal-division.ts
@@ -1,6 +1,12 @@
-function optimalDivision(nums: number[]): string {
+function optimalDivision(nums) {
 	const { length } = nums;
-	if (length === 1) return `${nums[0]}`; if (length === 2) return nums.join('/'); return nums.reduce((result, num, index) => { if (index === 0) return `${num}/(`;
-		if (index === length - 1) return result + `${num})`; return result + `${num}/`;
+	if (length === 1) return `${nums[0]}`;
+	if (length === 2) return nums.join('/');
+	return nums.reduce((result, num, index) => {
+		if (index === 0) return `${num}/(`; // a
+		if (index === length - 1) return result + `${num})`; // c
+		return result + `${num}/`; // b
 	}, '');
 };
+
+// [a,b,c] => biggest a/ (b/ c)
