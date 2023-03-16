98b22454872964661e7466dd4eac1444e54cb763
Quang Trung <gnurt10@gmail.com>
2023-03-16 09:53:28 +0700

    Create 667-beautiful-arrangement-ii.ts

---
+++ code/medium/667-beautiful-arrangement-ii.ts
@@ -0,0 +1,13 @@
+function constructArray(n: number, k: number): number[] {
+    let a = Array.from(Array(n).keys()).map( a => a + 1);
+    if(k ===1) return a;
+
+    const len = a.length;
+
+    for(let i=2;i<= k;i++) {
+        let x = a.slice( len - i + 1 ,len);
+        let y = a.slice(0, len - i + 1);
+        a = [...y.reverse(),...x];
+    }
+    return a;
+};
