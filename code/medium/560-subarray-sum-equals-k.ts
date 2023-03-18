c2d1bfb468040a41ef64352ee3223c8bfdd29158
Quang Trung <gnurt10@gmail.com>
2023-03-18 17:39:07 +0700

    Update 560-subarray-sum-equals-k.ts

--- code/medium/560-subarray-sum-equals-k.ts
+++ code/medium/560-subarray-sum-equals-k.ts
@@ -10,3 +10,13 @@ function subarraySum(nums: number[], k: number): number {
     
     return total;
 };
+// index :   0, 1, 2, 3, 4
+// nums[]:  [1, 2, 3, 4, 5]
+// prefix[]:[1, 3, 6, 10, 15]
+
+// prefix[i] - prefix[j] = k where i > j (i, j any index, in our case they are i = 4, j = 2) prefix[i] - k = prefix[j]
+// that from index j + 1 to i, the sum of the elements is k, right na,
+// hence it proves that their exist an subarray from index j + 1 to i whose sum is k
+    
+// Time Complexity --> O(n)
+// Space Complexity --> O(n)
