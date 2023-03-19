9c05360386e22ec8e8868152966148718b20e02a
Quang Trung <gnurt10@gmail.com>
2023-03-19 19:17:31 +0700

    Update 581-shortest-unsorted-continuous-subarray.ts

--- code/medium/581-shortest-unsorted-continuous-subarray.ts
+++ code/medium/581-shortest-unsorted-continuous-subarray.ts
@@ -11,3 +11,15 @@ function findUnsortedSubarray(nums: number[]): number {
     
     return nd-st+1;
 };
+
+// nd = The most right element having greater elements on the left side.
+// st = The most left element having smaller elements on the right side.
+
+// Prove it is effective:
+// According to the definition, we can know that all elements on the right side of nd do not have greater elements on the left side and all
+// elements on the left side of the st do not have greater elements on the right side.
+// Therefore, these two parts are good and we only need to sort the elements between st and st.
+// Therefore we care about the max value counted from the left (and the min value counted from the right).
+
+// TC O(n) 
+// SC O(1)
