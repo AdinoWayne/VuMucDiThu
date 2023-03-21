1abff813df6ba244e6ef127f34c7a3a2d6e69477
Quang Trung <gnurt10@gmail.com>
2023-03-21 17:02:06 +0700

    Create 686-repeated-string-match.ts

---
+++ code/medium/686-repeated-string-match.ts
@@ -0,0 +1,10 @@
+function repeatedStringMatch(a: string, b: string): number {
+    let num=Math.ceil(b.length/a.length);
+    if(a.repeat(num).includes(b)){
+        return num;
+    }
+    if(a.repeat(num+1).includes(b)){
+        return num+1;
+    }
+    return -1;
+};
