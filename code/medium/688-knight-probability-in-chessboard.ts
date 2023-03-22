80a7d2d57a009fa944defea6686f08c4dc4baacf
Quang Trung <gnurt10@gmail.com>
2023-03-22 16:42:25 +0700

    Create 688-knight-probability-in-chessboard.ts

---
+++ code/medium/688-knight-probability-in-chessboard.ts
@@ -0,0 +1,30 @@
+function knightProbability(n: number, k: number, r: number, c: number): number {
+    const dirs = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]];
+    const memo = new Map();
+    
+    return findProbs(k, r, c);
+    
+    function findProbs(k, r, c) {
+        const key = `${k}#${r}#${c}`;
+        
+        // base case
+        if (isOutOfBound(r, c)) return 0.0; // .0 is for completeness sake. Don't really need it in JavaScript
+        if (k === 0) return 1.0;
+        if (memo.has(key)) return memo.get(key);
+        
+        let prob = 0;
+        
+        for (const [dirX, dirY] of dirs) {
+            prob += 0.125 * findProbs(k - 1, r + dirX, c + dirY);
+        }
+        
+        memo.set(key, prob);
+        
+        return prob;
+        
+    }
+    
+    function isOutOfBound(row, col) {
+        return row < 0 || col < 0 || row >= n || col >= n;
+    }
+};
