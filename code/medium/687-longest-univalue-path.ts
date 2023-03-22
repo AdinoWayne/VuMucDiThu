722dd68025fbfc2ba113e317cc3574a55ad4fe17
Quang Trung <gnurt10@gmail.com>
2023-03-22 16:41:10 +0700

    Create 687-longest-univalue-path.ts

---
+++ code/medium/687-longest-univalue-path.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function longestUnivaluePath(root: TreeNode | null): number {
+   let level = 0 
+   function helper( parent, current) {
+		if (current === null) return 0
+		let left = helper(current.val, current.left)
+		let right = helper(current.val, current.right)
+		level = Math.max(level, left + right)
+		return current.val === parent ? Math.max(left, right) + 1 : 0
+    
+   }
+   if (root !== null) helper(root.val, root)
+   return level
+};
