449c72ff355479c465a91667ab4dd048088b7186
Quang Trung <gnurt10@gmail.com>
2023-03-26 09:19:00 +0700

    Create 720-longest-word-in-dictionary.ts

---
+++ code/medium/720-longest-word-in-dictionary.ts
@@ -0,0 +1,15 @@
+function longestWord(words: string[]): string {
+  words.sort();
+  let set = new Set();
+  let res = '';
+
+  for (let word of words) {
+    if (word.length === 1 || set.has(word.slice(0, -1))) {
+      set.add(word);
+      if (word.length > res.length) {
+        res = word;
+      }
+    }
+  }
+  return res;
+};
