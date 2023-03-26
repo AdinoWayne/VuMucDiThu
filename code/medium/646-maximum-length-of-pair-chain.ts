54b6aa58d3122494b7b15c574e0d78e92f97df99
Quang Trung <gnurt10@gmail.com>
2023-03-26 09:49:01 +0700

    Update 646-maximum-length-of-pair-chain.ts

--- code/medium/646-maximum-length-of-pair-chain.ts
+++ code/medium/646-maximum-length-of-pair-chain.ts
@@ -12,3 +12,60 @@ function findLongestChain(pairs: number[][]): number {
     }
     return chain;
 };
+// Time Complexity: O(n*log2n).
+
+// Space Complexity: O(1) as no extra space has been used.
+
+// Structure val
+class val
+{
+    constructor(first,second){
+        this.first = first;
+        this.second = second;
+    }
+};
+ 
+let m = new Map();
+ 
+// Memoisation function
+function findMaxChainLen(p,n,prev,pos)
+{
+     
+    // Check if pair { pos, prev } exists
+    // in m
+    if (m.has(new val(pos, prev)))
+        return m.get(new val(pos, prev));
+ 
+    // Check if pos is >=n
+    if (pos >= n)
+        return 0;
+ 
+    // Check if p[pos].first is
+    // less than prev
+    if (p[pos].first <= prev)
+    {
+        return findMaxChainLen(p, n, prev,
+                                pos + 1);
+    }
+ 
+    else
+    {
+        let ans = Math.max(findMaxChainLen(p, n,
+                            p[pos].second, 0) + 1,
+                    findMaxChainLen(p, n,
+                                prev, pos + 1));
+        m.set(new val(pos, prev),ans);
+        return ans;
+    }
+}
+ 
+// Function to calculate maximum
+// chain length
+function maxChainLen(p,n)
+{
+    m.clear();
+ 
+    // Call memoisation function
+    let ans = findMaxChainLen(p, n, 0, 0);
+    return ans;
+}
