424208d62ec392153be569db46b7a2adbc560694
Quang Trung <gnurt10@gmail.com>
2023-03-28 00:19:58 +0700

    Update 648-replace-words.ts

--- code/medium/648-replace-words.ts
+++ code/medium/648-replace-words.ts
@@ -10,3 +10,50 @@ function replaceWords(dictionary: string[], sentence: string): string {
     })
     return wordArr.reduce((str, word) => str += `${word} `, "").trim();
 };
+
+
+class Trie{
+  isWord: boolean;
+  children: object;
+  constructor(){
+    this.isWord = false;
+    this.children = {};
+  }
+}
+function replaceWords(dict: string[], sentence: string): string {
+  const root = new Trie();
+  for(const word of dict){
+    insertIntoTrie(word, root)
+  }
+  
+  const wordsInS = sentence.split(' ');
+  const res = [];
+  for (const word of wordsInS){
+    const newWord = searchTrie(word, root);
+    res.push(newWord)
+  }
+  return res.join(' ');
+};
+
+function insertIntoTrie(word, root){
+  let node = root;
+  for(const c of word){
+    const children = node.children;
+    if(!children[c]) children[c] = new Trie();
+    node = children[c];
+  }
+  node.isWord = true;
+}
+
+function searchTrie(word, root){
+  let node = root;
+  let newWord = '';
+  for(const c of word){
+    const children = node.children;
+    if(!children[c]) return word;
+    newWord += c;
+    node = children[c];
+    if(node.isWord) return newWord
+  }
+  return word
+}
