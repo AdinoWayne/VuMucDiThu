1581e3ad067826f25b38fb840b1cc71a182bc8b0
Quang Trung <gnurt10@gmail.com>
2023-03-28 21:58:46 +0700

    Create 718-maximum-length-of-repeated-subarray.ts

---
+++ code/medium/718-maximum-length-of-repeated-subarray.ts
@@ -0,0 +1,14 @@
+function findLength(A: number[], B: number[]): number {
+    let l1=A.length, l2=B.length, max=0;
+    for(let offset=-l2+1; offset<l1; offset++){
+    	let l=0;
+    	for(let i=Math.max(0,offset); i<l1 && i-offset<l2; i++){
+    		if(A[i]===B[i-offset]){
+    			l++;
+    			max = Math.max(max, l);
+    		}
+    		else l=0;
+    	}
+    }
+    return max;
+};
