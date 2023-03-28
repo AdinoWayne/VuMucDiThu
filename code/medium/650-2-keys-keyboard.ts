74b1419ddceddc62917c03495b4660e355c6c23b
Quang Trung <gnurt10@gmail.com>
2023-03-29 00:17:18 +0700

    Update 650-2-keys-keyboard.ts

--- code/medium/650-2-keys-keyboard.ts
+++ code/medium/650-2-keys-keyboard.ts
@@ -6,8 +6,11 @@ function minSteps(n: number): number {
     
     for (let i = 2; i <= n; i++) {
         dp[i] = i;
-        for (let j = Math.floor(i / 2); j >= 1; j--) {
+        for (let j = Math.floor(i / 2); j > 1; j--) {
+            // if sequence of length 'j' can be pasted multiple times to get length 'i' sequence
             if (i % j === 0) {
+                 // we just need to paste sequence j (i/j - 1) times, hence additional (i/j) times since we need to copy it first as well.
+                // we don't need checking any smaller length sequences 
                 dp[i] = dp[j] + (i / j);
                 break;
             }
