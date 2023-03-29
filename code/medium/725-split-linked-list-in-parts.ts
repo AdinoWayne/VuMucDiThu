5c13e0c71173168fa21f4564b0a1b693b28940e6
Quang Trung <gnurt10@gmail.com>
2023-03-29 23:14:25 +0700

    Create 725-split-linked-list-in-parts.ts

---
+++ code/medium/725-split-linked-list-in-parts.ts
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function splitListToParts(root: ListNode | null, k: number): Array<ListNode | null> {
+    const res = new Array(k).fill(null); // will contain split linked list 
+    let n = 0; // total # of nodes in the link list
+    
+    let curr = root;
+    
+    // count the # of nodes
+    while (curr != null) {
+        n++;
+        curr = curr.next;
+    }
+    
+    const width = Math.floor(n / k); // initial width of each linked list parts
+    let rem = n % k; // remaining nodes
+    
+    let head = root;
+    let tail = root;
+    
+    for (let i = 0; head != null; i++) {
+        let m = width;
+        
+        if (rem > 0) {
+            m += 1;
+            rem--;
+        }
+        
+        let tail = head;
+        
+        for (let j = 1; j < m; j++) {
+            tail = tail.next;
+        }
+        
+        const next = tail.next;
+        tail.next = null;
+        res[i] = head;
+        head = next;
+    }
+    
+    return res;
+};
