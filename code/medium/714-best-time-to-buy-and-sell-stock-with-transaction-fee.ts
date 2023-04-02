f791582427e9ad1f491834aa9f44bba77b5d3f69
Quang Trung <gnurt10@gmail.com>
2023-04-02 12:27:58 +0700

    Create 714-best-time-to-buy-and-sell-stock-with-transaction-fee.ts

---
+++ code/medium/714-best-time-to-buy-and-sell-stock-with-transaction-fee.ts
@@ -0,0 +1,12 @@
+function maxProfit(prices: number[], fee: number): number {
+    if(prices.length < 2) return 0;
+    let profit = 0, min = prices[0];
+    for(let g=1; g<prices.length; g++){
+        if( prices[g] < min ) min = prices[g];
+        else if( prices[g] > min +  fee ) {
+           profit = profit +  prices[g] - fee - min;
+           min = prices[g] - fee;
+        }
+    }
+    return profit;  
+};
