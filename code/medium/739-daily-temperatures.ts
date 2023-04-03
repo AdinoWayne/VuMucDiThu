f93667ddec2f20fabb27e25f5a02321ed026030a
Quang Trung <gnurt10@gmail.com>
2023-04-03 18:47:00 +0700

    Create 739-daily-temperatures.ts

---
+++ code/medium/739-daily-temperatures.ts
@@ -0,0 +1,12 @@
+function dailyTemperatures(temperatures: number[]): number[] {
+    let res = Array.from({length:temperatures.length},x=>0);
+    let stack = [];
+    for(let i=0; i<temperatures.length; i++){
+    	while(stack.length>0 && temperatures[stack[stack.length-1]]<temperatures[i]){
+    		let j = stack.pop();
+    		res[j] = i-j;
+    	}
+    	stack.push(i);
+    }
+    return res;
+};
