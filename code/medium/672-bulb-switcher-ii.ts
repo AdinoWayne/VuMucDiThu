88fffab0da442e5900bf6c8b3775a5abc97f5c64
Quang Trung <gnurt10@gmail.com>
2023-04-04 14:33:04 +0700

    Update 672-bulb-switcher-ii.ts

--- code/medium/672-bulb-switcher-ii.ts
+++ code/medium/672-bulb-switcher-ii.ts
@@ -4,3 +4,10 @@ function flipLights(n: number, presses: number): number {
     if (n <= 2) return 2 * n;
     return 7 + (presses >= 3 ? 1 : 0);
 };
+
+// 1 + 2 --> 3, 1 + 3 --> 2, 2 + 3 --> 1
+// So, there are only 8 cases.
+
+// All_on, 1, 2, 3, 4, 1+4, 2+4, 3+4
+
+// And we can get all the cases, when n>2 and m>=3.
