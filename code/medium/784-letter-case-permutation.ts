48f1369409285d3603ad740275e053ce6a6f503d
Quang Trung <gnurt10@gmail.com>
2023-04-11 01:07:25 +0700

    Create 784-letter-case-permutation.ts

---
+++ code/medium/784-letter-case-permutation.ts
@@ -0,0 +1,20 @@
+function letterCasePermutation(S: string): string[] {
+    let result = [];
+    dfs("", 0);
+    
+    function dfs(current, i){
+        if(current.length == S.length) {
+            result.push(current);
+            return;
+        }
+        if(S.charAt(i) >= '0' && S.charAt(i) <= '9') {
+            current += S.charAt(i);
+            dfs(current, i + 1);
+        } else {
+            dfs(current + S.charAt(i).toLowerCase(), i + 1);
+            dfs(current + S.charAt(i).toUpperCase(), i + 1);
+        }
+    }
+    
+    return result;
+};
