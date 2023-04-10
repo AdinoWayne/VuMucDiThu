04c2be38fd43bcb944c832132756bd767fdf8e2d
Quang Trung <gnurt10@gmail.com>
2023-04-11 01:08:34 +0700

    Create 785-is-graph-bipartite.ts

---
+++ code/medium/785-is-graph-bipartite.ts
@@ -0,0 +1,25 @@
+function isBipartite(graph: number[][]): boolean {
+  const colors = new Map();
+
+  for (let u = 0; u < graph.length; u++) {
+    if (!dfs(graph, colors, u, 0)) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const dfs = (graph, colors, u, color) => {
+  if (!colors.has(u)) {
+    colors.set(u, color);
+
+    for (const v of graph[u]) {
+      if (!dfs(graph, colors, v, 1 - color) || colors.get(v) === color) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
