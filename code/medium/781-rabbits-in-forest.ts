60b9ee8582fce3764827cb3086d31bdd7a47a49d
Quang Trung <gnurt10@gmail.com>
2023-04-10 08:23:01 +0700

    Create 781-rabbits-in-forest.ts

---
+++ code/medium/781-rabbits-in-forest.ts
@@ -0,0 +1,21 @@
+function numRabbits(answers: number[]): number {
+    const map = new Map();
+    let tot = 0;
+    
+    for (const answer of answers) {
+        if (answer === 0) {
+            tot++;
+            continue;
+        }
+        
+        if (!map.has(answer)) {
+            map.set(answer, answer);
+            tot += answer + 1;
+        } else {
+            map.set(answer, map.get(answer) - 1);
+            
+            if (map.get(answer) === 0) map.delete(answer);
+        }
+    }
+    return tot;
+};
