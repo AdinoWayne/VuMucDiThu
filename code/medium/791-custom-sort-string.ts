ab0873eba684945de017377f4be18962f6b8f542
Quang Trung <gnurt10@gmail.com>
2023-04-17 19:00:28 +0700

    Create 791-custom-sort-string.ts

---
+++ code/medium/791-custom-sort-string.ts
@@ -0,0 +1,14 @@
+function customSortString(order: string, s: string): string {
+  let map = {};
+  // iterate throuhg our string and place  map[string[i]] = i
+  for (let i = 0; i < order.length; i++) {
+    map[order[i]] = i;
+  }
+  for (let i = 0; i < s.length; i++) {
+    if (map[s[i]] == undefined) {
+      map[s[i]] = Infinity;
+    }
+  }
+  let arrS = s.split("");
+ return arrS.sort((a, b) => map[a] - map[b]).join("");
+};
