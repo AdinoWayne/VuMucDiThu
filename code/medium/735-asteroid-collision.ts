7c621140dbbc4b1d95d2132586f7e7df8144f3ae
Quang Trung <gnurt10@gmail.com>
2023-04-21 15:40:46 +0700

    Update 735-asteroid-collision.ts

--- code/medium/735-asteroid-collision.ts
+++ code/medium/735-asteroid-collision.ts
@@ -3,20 +3,10 @@ function asteroidCollision(asteroids: number[]): number[] {
     for (let i = 0; i < asteroids.length; i++) {
         const a = asteroids[i];
         
-        // Negative asteroids to the left of the stack can be ignored. 
-        // They'll never collide. Let's just add it to the answer stack and
-        // move on. I consider this a special case.
         if ((s.length === 0 || s[s.length -1] < 0) && a < 0 ) {
             s.push(a);
-            
-        // If an asteroid a is positive (l to r), it may still collide with an
-        // a negative asteroid further on in the asteroids array
         } else if (a > 0) {
             s.push(a);
-            
-        // a is negative. It can only collide with positive ones in 
-		// the stack. The following will keep on iterating
-        // until it is dealt with.
         } else {
             const pop = s.pop();
             
