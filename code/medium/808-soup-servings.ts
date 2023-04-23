f5e6624f0c3228888bf0d0861e387d5912215406
Quang Trung <gnurt10@gmail.com>
2023-04-23 10:59:15 +0700

    Create 808-soup-servings.ts

---
+++ code/medium/808-soup-servings.ts
@@ -0,0 +1,16 @@
+let memo = new Map();
+function soupServings(N: number): number {
+    memo.clear();
+    return N >= 4800 ? 1 : dfs(N, N);
+};
+
+const dfs = (a, b) => {
+    if (a <= 0 && b <= 0) return 0.5;
+    if (a <= 0) return 1;
+    if (b <= 0) return 0;
+    let spoon = a + ':' + b;
+    if (!memo.has(spoon)) {
+        memo.set(spoon, 0.25 * (dfs(a - 100, b) + dfs(a - 75, b - 25) + dfs(a - 50, b - 50) + dfs(a - 25, b - 75)));
+    }
+    return memo.get(spoon);
+};
