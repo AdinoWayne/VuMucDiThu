abfc371234e2b5cb671ac526c4c2c15286ec8844
Quang Trung <gnurt10@gmail.com>
2023-04-27 10:19:37 +0700

    Create 820-short-encoding-of-words.ts

---
+++ code/medium/820-short-encoding-of-words.ts
@@ -0,0 +1,13 @@
+function minimumLengthEncoding(words: string[]): number {
+    const wordSet = new Set(words)
+    words.forEach(word => {
+      for (var i = 1; i < word.length; i++) {
+        wordSet.delete(word.slice(i))
+      }
+    })
+
+    return [...wordSet].reduce((acc, word) => {
+      acc += word.length + 1
+      return acc
+    }, 0)
+};
