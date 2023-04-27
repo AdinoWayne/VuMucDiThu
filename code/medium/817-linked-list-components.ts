79453eca47e15114bc961ef61d8eb5216bf544fe
Quang Trung <gnurt10@gmail.com>
2023-04-27 10:18:54 +0700

    Create 817-linked-list-components.ts

---
+++ code/medium/817-linked-list-components.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function numComponents(head: ListNode | null, nums: number[]): number {
+    let map=new Map();
+    nums.map((item)=>{
+        map.set(item,!0);
+    });
+    let c=0;
+    while(head){
+        if(map.has(head.val)){
+		while(head&&map.has(head.val))
+		head=head.next;
+		c++;    
+        }             
+        if(head)
+        head=head.next;
+    }
+    return c;  
+};
