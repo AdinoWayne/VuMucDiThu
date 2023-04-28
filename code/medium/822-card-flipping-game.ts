8a47642ee8423a6f3cb9075a427686fa4680ec19
Quang Trung <gnurt10@gmail.com>
2023-04-28 11:02:45 +0700

    Create 822-card-flipping-game.ts

---
+++ code/medium/822-card-flipping-game.ts
@@ -0,0 +1,17 @@
+function flipgame(fronts: number[], backs: number[]): number {
+   var dictionary = new Set();
+    let size = fronts.length;
+    for(let i = 0; i < size; ++i) {
+        if (fronts[i] == backs[i]) {
+            dictionary.add(fronts[i]);
+        }
+    }
+    let min_num = Number.MAX_VALUE;
+    for(let num of fronts) {
+        if (!dictionary.has(num)) min_num = Math.min(min_num, num);
+    }
+    for(let num of backs) {
+        if (!dictionary.has(num)) min_num = Math.min(min_num, num);
+    }
+    return min_num == Number.MAX_VALUE ? 0 : min_num;
+};
