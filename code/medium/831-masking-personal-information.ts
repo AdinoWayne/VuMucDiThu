5af90defc73c4ac7455e10681f3588af39092558
Quang Trung <gnurt10@gmail.com>
2023-05-03 12:51:17 +0700

    Create 831-masking-personal-information.ts

---
+++ code/medium/831-masking-personal-information.ts
@@ -0,0 +1,28 @@
+function maskPII(S: string): string {
+    if (isEmail(S)) return maskEmail(S);
+    return maskPhone(S);
+    
+    function isEmail(str) {
+       return /^[A-Za-z]{2,}@[A-Za-z]{2,}.[A-Za-z]{2,}$/.test(str);
+    }
+    
+    function maskEmail(str) {
+        let res = "";
+        const [local, domain] = str.split("@");
+        
+        res += local.charAt(0).toLowerCase() + "*****" + local.charAt(local.length - 1).toLowerCase();
+        return res + "@" + domain.toLowerCase(); 
+    }
+    
+    function maskPhone(str) {
+        const onlyDigits = str.replace(/\D/g, "");
+        
+        const localNum = "***-***-" + onlyDigits.substring(onlyDigits.length - 4);
+        
+        if (onlyDigits.length === 10) return localNum;
+        
+        let countryNum = "+" + "*".repeat(onlyDigits.length - 10) + "-";
+        
+        return countryNum + localNum;
+    } 
+};
