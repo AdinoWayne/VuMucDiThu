1fdf1ce4241374caa95a03ae084f44c149a64068
Quang Trung <gnurt10@gmail.com>
2023-05-04 18:50:11 +0700

    Update 790-domino-and-tromino-tiling.ts

--- code/medium/790-domino-and-tromino-tiling.ts
+++ code/medium/790-domino-and-tromino-tiling.ts
@@ -7,3 +7,17 @@ function numTilings(n: number): number {
   }
   return count(n)
 };
+
+// n=1: 1
+// n=2: 2
+// n=3: 5
+// n=4: 11 = 2 * 5 + 1
+// n=5: 24 = 2 * 11 + 2
+// n=6: 53 = 2 * 24 + 5
+// n=7: 117 = 2 * 53 + 11
+// n=8: 256 = 2 * 117 + 24
+// n=9: 569 = 2 * 256 + 53
+
+// this gives us the following context: n=i: n[i] = 2 * n[i - 1] + n[i - 3]
+// Time complexity: O(n)
+// Space complexity: O(n)
