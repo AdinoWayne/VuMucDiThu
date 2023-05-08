513ba82d8995e610a95e0cd1fd0e44c611a5b712
Quang Trung <gnurt10@gmail.com>
2023-05-08 14:41:17 +0700

    Update 799-champagne-tower.ts

--- code/medium/799-champagne-tower.ts
+++ code/medium/799-champagne-tower.ts
@@ -11,3 +11,6 @@ function champagneTower(poured: number, query_row: number, query_glass: number):
     }
     return Math.min(1,cups[query_glass]);
 };
+
+// Time complexity: O(n^2)
+// Space complexity: O(n)
