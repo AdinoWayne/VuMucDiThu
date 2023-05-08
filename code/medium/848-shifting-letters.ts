d67386544b2d96d0eb1f65d18d7c55e5f5dcce0c
Quang Trung <gnurt10@gmail.com>
2023-05-08 09:42:46 +0700

    Create 848-shifting-letters.ts

---
+++ code/medium/848-shifting-letters.ts
@@ -0,0 +1,17 @@
+function shiftingLetters(s: string, shifts: number[]): string {
+    let result = ''
+    
+    for(let i=shifts.length-2; i>=0; i--){
+       shifts[i] +=  shifts[i+1];        
+    }
+    
+    for(let i=0; i<s.length; i++){
+        var shift = shifts[i] % 26;
+        let shifting = s[i].charCodeAt(0)-97  + shift
+        if(shifting > 25){
+            shifting = shifting - 26
+        }
+        result +=  String.fromCharCode(shifting +97)
+    }   
+    return result
+};
