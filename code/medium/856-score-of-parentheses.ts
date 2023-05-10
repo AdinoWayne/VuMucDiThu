d5ecec1b6c5a5fe88c3919f34bee1a79b91bb1e2
Quang Trung <gnurt10@gmail.com>
2023-05-10 10:52:06 +0700

    Create 856-score-of-parentheses.ts

---
+++ code/medium/856-score-of-parentheses.ts
@@ -0,0 +1,14 @@
+function scoreOfParentheses(S: string): number {
+    let stack = [0];
+    const arrS = S.split("");
+    for (let i in arrS) {
+        if (S[i] == "(") {
+            stack.push(0);
+        }
+        else {
+            let last = stack.pop();
+            stack[stack.length - 1] += 2 * last || 1
+        }
+    }
+    return stack.pop();
+};
