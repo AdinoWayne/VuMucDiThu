31a4171be40e7a48ca699e61ef78bdfc5288575d
Quang Trung <gnurt10@gmail.com>
2023-05-14 15:13:17 +0700

    Create 873-length-of-longest-fibonacci-subsequence.ts

---
+++ code/medium/873-length-of-longest-fibonacci-subsequence.ts
@@ -0,0 +1,17 @@
+function lenLongestFibSubseq(arr: number[]): number {
+  const map = {};
+  const N = arr.length;
+  const dp = [...Array(N)].map(r => Array(N).fill(2));
+  
+  let max = 0;
+  for (let i = 0; i < N; i++) {
+    map[arr[i]] = i;
+    for (let j = 0; j < i; j++) {
+      if (arr[i]-arr[j] < arr[j] && arr[i]-arr[j] in map) {
+        dp[j][i] = Math.max(dp[j][i], 1 + dp[map[arr[i]-arr[j]]][j]);
+      }
+      max = Math.max(max, dp[j][i]);
+    }
+  }
+  return max === 2 ? 0 : max;
+};
