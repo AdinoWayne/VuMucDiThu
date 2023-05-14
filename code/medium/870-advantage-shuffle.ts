78b0fb477e573b0f2c6c2f441d7dbaf696f0ea32
Quang Trung <gnurt10@gmail.com>
2023-05-14 15:14:18 +0700

    Create 870-advantage-shuffle.ts

---
+++ code/medium/870-advantage-shuffle.ts
@@ -0,0 +1,28 @@
+function advantageCount(A: number[], B: number[]): number[] {
+    const mapB = B.map((n, i) => [n, i]);
+    const len = A.length;
+    const output = [];
+    const useless = []
+    
+    A.sort((a, b) => a - b);
+    mapB.sort((a, b) => a[0] - b[0]);
+    
+    let currB = 0;
+    
+    for(let i = 0; i < len; i++) {
+        const [numB, idxB] = mapB[currB];
+        
+        if(numB >= A[i]) useless.push(A[i]);
+        else {
+            output[idxB] = A[i];
+            currB++;
+        }
+    }
+    
+    for(let i = 0; i < len; i++) {
+        if(output[i] === undefined) {
+            output[i] = useless.pop();
+        }
+    }
+    return output;
+};
