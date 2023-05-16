44d722398039eca2bf2a240f760035b36974e99d
Quang Trung <gnurt10@gmail.com>
2023-05-16 15:22:55 +0700

    Update 835-image-overlap.ts

--- code/medium/835-image-overlap.ts
+++ code/medium/835-image-overlap.ts
@@ -6,6 +6,7 @@ function largestOverlap(img1: number[][], img2: number[][]): number {
 
 	for (let a = 0; a < n; a++) {
 		for (let b = 0; b < n; b++) {
+			//  find n push coordinate has value 1
 			img1[a][b] && img1Values.push({ row: a, col: b });
 			img2[a][b] && img2Values.push({ row: a, col: b });
 		}
@@ -13,6 +14,7 @@ function largestOverlap(img1: number[][], img2: number[][]): number {
 
 	img1Values.forEach(one => {
 		img2Values.forEach(two => {
+			// difference row n col
 			const OffestKey = `${one.row - two.row}-${one.col - two.col}`;
 			const overlap = overlapMap.get(OffestKey) ?? 0;
 			overlapMap.set(OffestKey, overlap + 1);
