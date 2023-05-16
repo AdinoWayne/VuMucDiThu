3b8792df3b022f2ce030e080a7accd9edb33f0a1
Quang Trung <gnurt10@gmail.com>
2023-05-16 09:51:53 +0700

    Update 833-find-and-replace-in-string.ts

--- code/medium/833-find-and-replace-in-string.ts
+++ code/medium/833-find-and-replace-in-string.ts
@@ -1,18 +1,24 @@
 function findReplaceString(S: string, indices: number[], sources: string[], targets: string[]): string {
-  var result:any = S.split('');
+  let result:any = S.split('');
   indices.forEach((indexInS, i) => {
-    var sourceChars = sources[i];
-    var targetChars = targets[i];
-    
-    if (S.slice(indexInS, indexInS + sourceChars.length) === sourceChars) {
+    const sourceChars = sources[i];
+    const targetChars = targets[i];
+    const strSlice = s.slice(indexInS, indexInS + sourceChars.length);
+    if (strSlice === sourceChars) {
       result[indexInS] = [targetChars];
-      var elementsToReplace = sourceChars.length - 1;
+      let elementsToReplace = sourceChars.length - 1;
       while (elementsToReplace > 0) {
+        // if element of sources length > 1;
         indexInS += 1;
-        result[indexInS] = '';
+        result[indexInS] = "";
         elementsToReplace -= 1;
       }
     }
   });
-  return result.join('');
+  return result.join("");
 };
+
+<!--
+Time complexity: O(n + m * k) n is the length of the string S, m is the length of the indices array, and k is the average length of the source characters
+Space complexity: O(n)
+-->
