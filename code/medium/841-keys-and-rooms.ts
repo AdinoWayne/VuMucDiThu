5549fcc6b3b9c4cf69112416a5575cfd3f1c9258
Quang Trung <gnurt10@gmail.com>
2023-05-18 04:31:32 +0700

    Update 841-keys-and-rooms.ts

--- code/medium/841-keys-and-rooms.ts
+++ code/medium/841-keys-and-rooms.ts
@@ -1,18 +1,20 @@
 function canVisitAllRooms(rooms: number[][]): boolean {
-    function dfs(current){
+    function dfs (current: number[]): void {
         current.forEach(node => {
-           if (!(visited.has(node))){
+           if (!(visited.has(node))) {
                visited.add(node);
                total++;
                dfs(rooms[node]);
-           } 
+           }
         });
     }
    
-    let visited = new Set();
+    let visited = new Set<number>();
     visited.add(0);
     let total = 0;
-    
     dfs(rooms[0]);
-    return total == rooms.length-1? true:false;
+    return total == rooms.length-1 ? true : false;
 };
+
+// Time complexity: O(n + E) number of rooms and number of rooms[i]
+// Space comlexity: O(n)
