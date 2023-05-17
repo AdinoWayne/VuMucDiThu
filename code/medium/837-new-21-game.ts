e654cb752c93d63b80ace67f404e3ade1275e227
Quang Trung <gnurt10@gmail.com>
2023-05-17 10:13:24 +0700

    Update 837-new-21-game.ts

--- code/medium/837-new-21-game.ts
+++ code/medium/837-new-21-game.ts
@@ -26,5 +26,9 @@ function new21Game(n: number, k: number, maxPts: number): number {
     }, 0)
 };
 // dp[i] is the probability that we get points i at some moment
+// if i <= k, always add 1.1P(i-1) to P(i)
+// if i > max, remove 0.1P(i - max - 1) from P(i). This is essentially a sliding window problem:
+// P11 = 0.1P10 + 0.1P9 + ... + 0.1P1 = 1.1P10 - 0.1P0
+// if i > k, sliding window's right stops growing, meaning always add P(i-1) instead of 1.1P(i-1) to P(i)
 // Time O(N)
 // Space O(N)
