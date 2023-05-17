c6f8dc3c4b06889bcd39ab358c8e4b1b64fc2a95
Quang Trung <gnurt10@gmail.com>
2023-05-18 04:00:25 +0700

    Create 880-decoded-string-at-index.ts

---
+++ code/medium/880-decoded-string-at-index.ts
@@ -0,0 +1,26 @@
+function decodeAtIndex(S: string, K: number): string {
+  let cnt = 0;
+  let i = 0;
+  for (; i < S.length; ++i) {
+    const ch = S[i];
+    if (/[a-z]/.test(ch)) {
+      cnt++;
+    } else if (/\d/.test(ch)) {
+      cnt *= +ch;
+    }
+    if (cnt >= K) {
+      break;
+    }
+  }
+  for (let j = i; j >= 0; --j) {
+    const ch = S[j];
+    if (/[a-z]/.test(ch)) {
+      if (K === cnt) return S[j];
+      cnt--;
+    } else if (/\d/.test(ch)) {
+      cnt /= +ch;
+      K %= cnt;
+      if (K === 0) K = cnt;
+    }
+  }
+};
