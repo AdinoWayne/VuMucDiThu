08ea0f8f05672454e249f36826aad7768eb112e2
Quang Trung <gnurt10@gmail.com>
2023-05-18 03:59:41 +0700

    Create 877-stone-game.ts

---
+++ code/medium/877-stone-game.ts
@@ -0,0 +1,25 @@
+function stoneGame(piles: number[]): boolean {
+    const n = piles.length;
+    
+    const memo = [];
+    
+    for (let i = 0; i < n; i++) {
+        memo[i] = [];
+    }
+    
+    return topDown(0, n - 1) > 0;
+    
+    function topDown(start, end) {
+        if (start == end) return piles[start];
+        if (memo[start][end]) return memo[start][end];
+        
+        const startPick = piles[start] - topDown(start + 1, end);
+        const endPick = piles[end] - topDown(start, end - 1);
+        
+        const res = Math.max(startPick, endPick);
+        
+        memo[start][end] = res;
+        
+        return res;
+    }
+};
