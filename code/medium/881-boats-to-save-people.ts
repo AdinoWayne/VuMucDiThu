f83f064847a2d25c2307ff3857077f8550500de1
Quang Trung <gnurt10@gmail.com>
2023-05-19 10:13:10 +0700

    Create 881-boats-to-save-people.ts

---
+++ code/medium/881-boats-to-save-people.ts
@@ -0,0 +1,11 @@
+function numRescueBoats(people: number[], limit: number): number {
+    people = people.sort((a, b) => b - a);
+    let i = 0, j = people.length-1;
+    let count = 0;
+    while (i <= j) {
+        if (people[i] + people[j] <= limit) j--;
+        i++;
+        count++;
+    }
+    return count;
+};
