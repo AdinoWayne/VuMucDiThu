802048bee71b928c965a6144872ae2a2c537b655
Quang Trung <gnurt10@gmail.com>
2023-05-19 10:43:19 +0700

    Update 845-longest-mountain-in-array.ts

--- code/medium/845-longest-mountain-in-array.ts
+++ code/medium/845-longest-mountain-in-array.ts
@@ -1,26 +1,21 @@
 function longestMountain(arr: number[]): number {
-	let maxLength = 0
-	for(let i = 1; i < arr.length; i++) {
-		const isPeak = arr[i] > arr[i - 1] && arr[i] > arr[i + 1]
-    if(!isPeak) {
-      continue
-    }
-
-     let left = i - 1,
-        right = i + 1;
-    while(left >= 0 && arr[left] > arr[left - 1]) {
-      left--
-    }
-
-    while(right < arr.length && arr[right] > arr[right + 1]) {
-      right++
-    }
-
-    maxLength = Math.max(maxLength, right - left + 1)
-		
+  let maxLength = 0;
+  for(let i = 1; i < arr.length; i++) {
+	const isPeak = arr[i] > arr[i - 1] && arr[i] > arr[i + 1];
+        if(!isPeak) {
+            continue;
+        }
+        let left = i - 1,
+            right = i + 1;
+        while(left >= 0 && arr[left] > arr[left - 1]) {
+            left--;
+        }
+        while(right < arr.length && arr[right] > arr[right + 1]) {
+            right++;
+        }
+        maxLength = Math.max(maxLength, right - left + 1);
 	}
-
-  return maxLength
+  return maxLength;
 };
 
 // Time complexity: O(n)
