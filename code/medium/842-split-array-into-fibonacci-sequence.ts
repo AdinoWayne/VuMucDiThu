795e2cdbd67bfceac0aa00eda7449fe2157fe71e
Quang Trung <gnurt10@gmail.com>
2023-05-19 10:37:23 +0700

    Update 842-split-array-into-fibonacci-sequence.ts

--- code/medium/842-split-array-into-fibonacci-sequence.ts
+++ code/medium/842-split-array-into-fibonacci-sequence.ts
@@ -22,9 +22,14 @@ function splitIntoFibonacci(S: string): number[] {
         const sum = (+num1 + +num2).toString();
         if(+sum > bit32) return false;
         output.push(sum)
+        // return result 
         if(rest === sum) return output;
+        // if sum is substring of top rest -> recursive -> num2, sum
         if(rest.startsWith(sum)) return isValid(num2, sum, rest.slice(sum.length), output);
         return false;
     }
     return []
 };
+
+// Time complexity: O(n^2)
+// Space complexity: O(n)
