470e0312cc70a91bdfe5b6cc348fc5082d82a975
Quang Trung <gnurt10@gmail.com>
2023-05-22 10:36:53 +0700

    Create 893-groups-of-special-equivalent-strings.ts

---
+++ code/medium/893-groups-of-special-equivalent-strings.ts
@@ -0,0 +1,13 @@
+function numSpecialEquivGroups(words: string[]): number {
+  const groupSet = new Set();
+  words.forEach(a => groupSet.add(transform(a)));
+  return groupSet.size;
+};
+
+const transform = S => {
+  const even = S.split('').filter((_, i) => i % 2 === 0);
+  const odd = S.split('').filter((_, i) => i % 2 === 1);
+  even.sort();
+  odd.sort();
+  return `${even.join('')}${odd.join('')}`;
+};
