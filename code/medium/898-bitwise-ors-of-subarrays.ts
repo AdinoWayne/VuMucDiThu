b4a2ea1034d7569e38cd5981e13128cd2c7668ab
Quang Trung <gnurt10@gmail.com>
2023-05-23 00:39:09 +0700

    Create 898-bitwise-ors-of-subarrays.ts

---
+++ code/medium/898-bitwise-ors-of-subarrays.ts
@@ -0,0 +1,21 @@
+function subarrayBitwiseORs(arr: number[]): number {
+	const bits = [];
+	let L = 0;
+
+	for (let i = 0; i < arr.length; i++) {
+		const R = bits.length;
+		bits.push(arr[i]); // handle subarray with only 1 number
+
+    // create next sub-array and find bitwise OR
+		while (L < R) {
+			const prev = bits[bits.length - 1];
+			const bitwise = bits[L] | prev; // get bitwise OR using prev bit value
+
+			if (bitwise !== prev) bits.push(bitwise);
+
+			L++;
+		}
+	}
+
+	return new Set(bits).size; // get all unique bitwise ORs
+};
