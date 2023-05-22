7c9cdc59f2165682e297a5fd5cc4106b07508db6
Quang Trung <gnurt10@gmail.com>
2023-05-22 10:37:17 +0700

    Create 890-find-and-replace-pattern.ts

---
+++ code/medium/890-find-and-replace-pattern.ts
@@ -0,0 +1,24 @@
+function findAndReplacePattern(words: string[], pattern: string): string[] {
+    let matches = []
+    for(const word of words){
+        if(word.length !== pattern.length)continue
+        let map = new Map(), set = new Set(), equals = true
+        for(let i = 0; i < pattern.length; i++){
+            if(map.has(pattern[i])){
+                if(map.get(pattern[i]) !== word[i]){
+                    equals = false
+                    break                    
+                }
+            }else{
+                if(set.has(word[i])){
+                    equals = false
+                    break
+                }
+                map.set(pattern[i], word[i])
+                set.add(word[i])
+            }
+        }
+        if(equals)matches.push(word)
+    }
+    return matches
+};
