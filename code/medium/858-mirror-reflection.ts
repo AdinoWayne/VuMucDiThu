6fa3c81f2d23778a8aac59714c107e0b943d7c55
Quang Trung <gnurt10@gmail.com>
2023-05-24 10:11:54 +0700

    Update 858-mirror-reflection.ts

--- code/medium/858-mirror-reflection.ts
+++ code/medium/858-mirror-reflection.ts
@@ -5,13 +5,15 @@ function mirrorReflection(p: number, q: number): number {
 		ext /= 2;
 		ref /= 2;
 	}
-	
+
+	// ext * p = ref * q
 	if (ext % 2 == 0 && ref % 2 == 1) return 0;
 	if (ext % 2 == 1 && ref % 2 == 1) return 1;
 	if (ext % 2 == 1 && ref % 2 == 0) return 2;
 	
 	return -1;
 };
-
+// summary:  [ext, ref] = [odd, odd] (1) ; [even, odd] (0) ; [odd, even] (2)
 // Time complexity: O(log(max(p, q)))
 // Space complexity: O(1)
+// ref: https://www.youtube.com/watch?v=_xIBOUWEq1c&ab_channel=AlgorithmsMadeEasy
