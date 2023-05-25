6818bbd10c065dfe6b9bc65570d5117cec33e2d0
Quang Trung <gnurt10@gmail.com>
2023-05-25 11:17:54 +0700

    Update 861-score-after-flipping-matrix.ts

--- code/medium/861-score-after-flipping-matrix.ts
+++ code/medium/861-score-after-flipping-matrix.ts
@@ -1,12 +1,12 @@
 function matrixScore(grid: number[][]): number {
-    let flippedRows = grid.map(item => {
-        if(!item[0]) return item.map(item => item===1 ? 0 : 1);
-        return item;
-    })
+	let flippedRows = grid.map(item => {
+		if(!item[0]) return item.map(item => item===1 ? 0 : 1);
+		return item;
+	})
 	let index=0;
-    let sum=0;
-    
-    while(index <= flippedRows[0].length-1) {
+	let sum=0;
+
+	while(index <= flippedRows[0].length-1) {
 		for(let i=0; i<flippedRows.length; i++) {
 			let item = flippedRows[i];
 			sum+=item[item.length-index];
@@ -18,9 +18,9 @@ function matrixScore(grid: number[][]): number {
 				item.splice(item.length-index, 1, newValue);
 			}
 		} 
-			sum=0;
-			index++;
-    }
+		sum=0;
+		index++;
+	}
 	let res = flippedRows
 	.map(item => item.join(''))
 	.map(item => parseInt(item,2))
