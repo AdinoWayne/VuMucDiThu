402bdbcd69cd607fcf3575535ea451d8a2165d0f
Quang Trung <gnurt10@gmail.com>
2023-05-26 07:16:35 +0700

    Create 910-smallest-range-ii.ts

---
+++ code/medium/910-smallest-range-ii.ts
@@ -0,0 +1,16 @@
+function smallestRangeII(nums: number[], k: number): number {
+    nums = nums.sort((a,b)=>a-b);
+    
+    let min_first = nums[0];
+    let max_last = nums[nums.length-1];
+    
+    let res = max_last - min_first;
+   
+    
+    for(let i=0; i<nums.length-1; i++){
+        
+        res = Math.min(res, Math.max(max_last - k, nums[i]+k) - Math.min(min_first + k, nums[i+1]-k))
+    }
+    
+    return res;
+};
