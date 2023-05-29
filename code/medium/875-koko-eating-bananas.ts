c732cdc9a72b1980b09b5358102bb5e51f5e9523
Quang Trung <gnurt10@gmail.com>
2023-05-29 10:10:34 +0700

    Update 875-koko-eating-bananas.ts

--- code/medium/875-koko-eating-bananas.ts
+++ code/medium/875-koko-eating-bananas.ts
@@ -1,19 +1,20 @@
 function minEatingSpeed(piles: number[], h: number): number {
-let min = 1,
-        max = Math.max(...piles),
-        best = max
+	let min = 1,
+	    max = Math.max(...piles),
+	    best = max
+ 	const time = speed => piles.reduce((sum, pile) => sum + Math.ceil(pile/speed), 0)
 
-    const time = speed => piles.reduce((sum, pile) => sum + Math.ceil(pile/speed), 0)
+	while (min <= max) {
+		const mid = Math.floor((min + max) / 2)
 
-    while (min <= max) {
-        const mid = Math.floor((min + max) / 2)
-		
-        if (time(mid) <= h) {
-            best = mid
-            max = mid - 1
-        } else
-            min = mid + 1
-    }
-
-    return best
+		if (time(mid) <= h) {
+		    best = mid
+		    max = mid - 1
+		} else
+		    min = mid + 1
+	}
+	return best
 };
+
+// Time complexity: O(n)
+// Space complexity: O(1)
