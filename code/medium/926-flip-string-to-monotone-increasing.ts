92dbfb033c215d7e465fbeff9896aaa380d3c686
Quang Trung <gnurt10@gmail.com>
2023-05-31 09:37:02 +0700

    Create 926-flip-string-to-monotone-increasing.ts

---
+++ code/medium/926-flip-string-to-monotone-increasing.ts
@@ -0,0 +1,14 @@
+function minFlipsMonoIncr(s: string): number {
+    let onesAlreadySeen = 0;
+    let res = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '0') {
+            res = Math.min(res + 1 /* flip this '0' to '1' */,
+                        onesAlreadySeen /* flip all '1' we've seen before to '0'*/);
+        } else {
+            // s[i] is '1'
+            onesAlreadySeen++;
+        }
+    }
+    return res;
+};
