6d6bdfd38ebb306a6b785aa3df61d9443d3bb121
Quang Trung <gnurt10@gmail.com>
2023-05-31 09:39:54 +0700

    Create 930-binary-subarrays-with-sum.ts

---
+++ code/medium/930-binary-subarrays-with-sum.ts
@@ -0,0 +1,17 @@
+function numSubarraysWithSum(nums: number[], goal: number): number {
+    return withSumAtMost(nums, goal) - withSumAtMost(nums, goal - 1);
+}
+function withSumAtMost(a, t) {
+    if (t < 0) return 0;
+    let i = 0,
+        sum = 0,
+        cc = 0;
+    for (let j = 0; j < a.length; j++) {
+        sum += a[j];
+        for (; sum > t; i++) {
+            sum -= a[i];
+        }
+        cc += j - i + 1;
+    }
+    return cc;
+}
