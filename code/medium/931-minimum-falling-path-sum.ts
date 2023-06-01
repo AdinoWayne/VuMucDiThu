d3b972030cdc5ccda6c58610952168b3ee2ef56f
Quang Trung <gnurt10@gmail.com>
2023-06-01 09:52:23 +0700

    Create 931-minimum-falling-path-sum.ts

---
+++ code/medium/931-minimum-falling-path-sum.ts
@@ -0,0 +1,21 @@
+function minFallingPathSum(matrix: number[][]): number {
+    let minSum = Infinity;
+    let n = matrix.length;
+    let dp = new Array(n);
+    for (let i = 0; i < n; i++) {
+        dp[i] = [...matrix[i]];
+    }
+    let helper = function(i,j) {
+        if (i == n || j == n || i < 0 || j < 0) return Infinity;
+        if (i == n-1) return matrix[i][j];
+        if (dp[i][j] != matrix[i][j]) return dp[i][j];
+
+        dp[i][j] += Math.min(helper(i+1, j-1), Math.min(helper(i+1, j), helper(i+1, j+1)))
+        return dp[i][j];
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        helper(0,i);
+        minSum = Math.min(minSum, dp[0][i])
+    }
+    return minSum;
+};
