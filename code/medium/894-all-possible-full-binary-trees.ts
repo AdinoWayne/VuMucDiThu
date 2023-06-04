bf578e5a20817fd3171fb10c7d47c0dfb0fa4faf
Quang Trung <gnurt10@gmail.com>
2023-06-04 14:56:59 +0700

    Update 894-all-possible-full-binary-trees.ts

--- code/medium/894-all-possible-full-binary-trees.ts
+++ code/medium/894-all-possible-full-binary-trees.ts
@@ -17,34 +17,30 @@ function allPossibleFBT(n: number): Array<TreeNode | null> {
     return solve(n,dp);
 };
 var solve = function(n,dp) {
+    // even not valid
     if(n % 2 === 0) return dp[n] = []
-    
+
     if(dp[n] !== -1) return dp[n]
-    
+
     if(n === 1) return dp[n] = [new TreeNode(0)]
     
     let ans = []
-    
     n = n - 1
-    
+
     for(let ctr = 1 ; ctr < n ; ctr ++) {
-        
+        // brute force
         let left = solve(ctr,dp)
         let right= solve(n-ctr,dp)
-        
         for(let row = 0 ; row < left.length ; row ++)
             for(let col = 0 ; col < right.length ; col ++) {
-                
                 const node = new TreeNode(0)
-                
                 node.left = left[row]
                 node.right= right[col]
-                
-                ans.push(node)
-                
+                ans.push(node)           
             }
     }
-    
-    return dp[n] = ans
-    
+    return dp[n] = ans;    
 }
+
+// Time complexity: O(2^n)
+// Space complexity: O(2^n)
