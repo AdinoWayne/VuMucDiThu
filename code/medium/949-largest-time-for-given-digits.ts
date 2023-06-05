cbf0ba6c7479dbd90e45c0c1927c9018f116654f
Quang Trung <gnurt10@gmail.com>
2023-06-06 06:52:40 +0700

    Create 949-largest-time-for-given-digits.ts

---
+++ code/medium/949-largest-time-for-given-digits.ts
@@ -0,0 +1,19 @@
+function largestTimeFromDigits(A: number[]): string {
+    let res = '';
+    
+    function permute(arr, str) {
+        if(str.length === 4) {
+            if(+str >= +res) res = str;
+            return;
+        }
+        
+        for(let i = 0; i < arr.length; i++) {
+            if(!str.length && arr[i] > 2) continue;
+            if(str.length == 1 && str[0] == '2' && arr[i] > 3) continue;
+            if(str.length == 2 && arr[i] > 5) continue;
+            permute([...arr.slice(0, i), ...arr.slice(i+1)], str+arr[i]);
+        }
+    }
+    permute(A, '');
+    return res.slice(0, 2) + (res.length ? ':' : '') + res.slice(2);
+};
