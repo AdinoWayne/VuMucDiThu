046176b23ffcf608d077175cfdc568bc680b0385
Quang Trung <gnurt10@gmail.com>
2023-06-08 06:53:06 +0700

    Create 954-array-of-doubled-pairs.ts

---
+++ code/medium/954-array-of-doubled-pairs.ts
@@ -0,0 +1,26 @@
+function canReorderDoubled(arr: number[]): boolean {
+    arr.sort((a, b) => a - b);
+    let counter = new Map();
+    for (let num of arr) {
+        if(!counter.has(num)) {
+            counter.set(num, 0);
+        }
+        counter.set(num, counter.get(num) + 1);
+    }
+    for (let num of arr) {
+        if (!counter.has(num)) {
+            continue;
+        }
+        let pair = num >= 0 ? num * 2 : num / 2;
+        if(!counter.has(pair)) {
+            return false;
+        }
+        for (let k of [num, pair]) {
+            counter.set(k, counter.get(k) - 1);
+            if (counter.get(k) === 0) {
+                counter.delete(k);
+            }
+        }
+    }
+    return true;
+};
