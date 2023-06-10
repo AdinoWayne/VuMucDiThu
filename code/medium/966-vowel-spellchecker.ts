a1073d5c23c63f5a2dd291d9ac89e70a8cb61923
Quang Trung <gnurt10@gmail.com>
2023-06-11 00:33:38 +0700

    Create 966-vowel-spellchecker.ts

---
+++ code/medium/966-vowel-spellchecker.ts
@@ -0,0 +1,22 @@
+function spellchecker(wordlist: string[], queries: string[]): string[] {
+  const REG = /[aeiou]/g;
+  const originSet = new Set();
+  const spellMap = new Map();
+
+  wordlist.forEach((word) => {
+    const lowerWord = word.toLowerCase();
+    const vowelWord = lowerWord.replace(REG, "_");
+    originSet.add(word);
+    if (!spellMap.has(lowerWord)) spellMap.set(lowerWord, word);
+    if (!spellMap.has(vowelWord)) spellMap.set(vowelWord, word);
+  });
+
+  return queries.map((query) => {
+    if (originSet.has(query)) return query;
+    const lowerQuery = query.toLowerCase();
+    if (spellMap.has(lowerQuery)) return spellMap.get(lowerQuery);
+    const vowelQuery = lowerQuery.replace(REG, "_");
+    if (spellMap.has(vowelQuery)) return spellMap.get(vowelQuery);
+    return "";
+  });
+};
