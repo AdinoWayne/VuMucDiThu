484b4301064959155cbd723ae2f77dd4ca591f69
Quang Trung <gnurt10@gmail.com>
2023-06-10 08:19:31 +0700

    Update 916-word-subsets.ts

--- code/medium/916-word-subsets.ts
+++ code/medium/916-word-subsets.ts
@@ -1,6 +1,6 @@
 function wordSubsets(words1: string[], words2: string[]): string[] {
-    const output = []
-    const freqB = Array(26).fill(0);
+    const output = [];
+    const freqB = Array(26).fill(0); // mapB
     
     for(let wordB of words2) {
         const currFreq = getFreq(wordB);
@@ -11,28 +11,28 @@ function wordSubsets(words1: string[], words2: string[]): string[] {
     }
     
     for(let wordA of words1) {
-        const freqA = getFreq(wordA)
+        const freqA = getFreq(wordA); // mapA
         const universal = isSubset(freqA);
         if(universal) output.push(wordA);
     }
     
-    function isSubset(freqA) {
+    function isSubset(freqA: number[]): boolean { // compare is sub
         for(let i = 0; i < freqA.length; i++) {
             if(freqB[i] > freqA[i]) return false;
         }
         return true;
     }
     
-    function getFreq(word) {
+    function getFreq(word: string): number[] {
         const map = Array(26).fill(0);
         for(let char of word) {
-            const idx = char.charCodeAt() - 97
-            map[idx]++
+            const idx = char.charCodeAt() - 97;
+            map[idx]++;
         }
-        return map
+        return map;
     }
     return output;
 };
 
-// Time complexity: O(n)
-// Space complexity: O(n)
+// Time complexity: O(n2)
+// Space complexity: O(1)
