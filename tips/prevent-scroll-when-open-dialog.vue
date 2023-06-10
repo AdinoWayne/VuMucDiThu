5a1eac1bf3033a8188f3b79f70301ff0be81f9ac
Quang Trung <gnurt10@gmail.com>
2023-06-10 22:52:42 +0700

    Create prevent-scroll-when-open-dialog.vue

---
+++ tips/prevent-scroll-when-open-dialog.vue
@@ -0,0 +1,16 @@
+watched: {
+    SIDE_BAR: {
+      handler(value) {
+        if (value) {
+          const val = window.scrollY;
+          document.body.style.position = 'fixed';
+          document.body.style.top = `-${val}px`;
+        } else {
+          const scrollY = document.body.style.top;
+          document.body.style.position = '';
+          document.body.style.top = '';
+          window.scrollTo(0, parseInt(scrollY || '0') * -1);
+        }
+      }
+    }
+}
