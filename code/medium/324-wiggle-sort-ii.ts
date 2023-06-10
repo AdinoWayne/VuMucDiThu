45e4b41e618d672950725cce9ba3e45f76bbb5e1
Quang Trung <gnurt10@gmail.com>
2023-06-11 00:54:57 +0700

    Update 324-wiggle-sort-ii.ts

--- code/medium/324-wiggle-sort-ii.ts
+++ code/medium/324-wiggle-sort-ii.ts
@@ -1,4 +1,11 @@
 /**
+Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
+You may assume the input array always has a valid answer.
+
+Input: nums = [1,5,1,1,6,4]
+Output: [1,6,1,5,1,4]
+Explanation: [1,4,1,5,1,6] is also accepted.
+
  Do not return anything, modify nums in-place instead.
  */
 function wiggleSort(nums: number[]): void {
@@ -16,3 +23,6 @@ function wiggleSort(nums: number[]): void {
         }
     }
 };
+
+// Time complexity: O(nLogN)
+// Space complxity: O(n)
