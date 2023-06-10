f65f90cbde30c4f654f83ba298d5a195dda5aab1
Quang Trung <gnurt10@gmail.com>
2023-06-10 08:27:37 +0700

    Update 915-partition-array-into-disjoint-intervals.ts

--- code/medium/915-partition-array-into-disjoint-intervals.ts
+++ code/medium/915-partition-array-into-disjoint-intervals.ts
@@ -1,13 +1,13 @@
 function partitionDisjoint(nums: number[]): number {
-    let m=nums[0],count=1,max_ele=nums[0];
-    for(let i=0;i<nums.length;i++)
+    let m=nums[0], count=1, max_ele=nums[0];
+    for(let i=0; i<nums.length; i++)
     {
         if(m>nums[i]){
             count=i+1;
-        m=Math.max(max_ele,m);
+            m=Math.max(max_ele, m);
         }   
         else
-            max_ele=Math.max(max_ele,nums[i]);
+            max_ele=Math.max(max_ele, nums[i]);
     }
     return count; 
 };
