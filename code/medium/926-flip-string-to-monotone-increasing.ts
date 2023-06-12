6216b247b1ccfbb7041ebffb8821bd06d5215b9e
Quang Trung <gnurt10@gmail.com>
2023-06-12 23:55:04 +0700

    Update 926-flip-string-to-monotone-increasing.ts

--- code/medium/926-flip-string-to-monotone-increasing.ts
+++ code/medium/926-flip-string-to-monotone-increasing.ts
@@ -1,17 +1,23 @@
 function minFlipsMonoIncr(s: string): number {
-    let onesAlreadySeen = 0;
-    let res = 0;
+    let counter_one = 0;
+    let counter_flip = 0;
     for (let i = 0; i < s.length; i++) {
         if (s[i] == '0') {
-            res = Math.min(res + 1 /* flip this '0' to '1' */,
-                        onesAlreadySeen /* flip all '1' we've seen before to '0'*/);
+            counter_flip = Math.min(counter_flip + 1 /* flip this '0' to '1' */,
+                        counter_one /* flip all '1' we've seen before to '0'*/);
         } else {
             // s[i] is '1'
-            onesAlreadySeen++;
+            counter_one++;
         }
     }
-    return res;
+    return counter_flip;
 };
+// Char=='1'
+// It will not impact our minimum flips just keep the record of count of 1
+// Char=='0'
+// Here will be 2 cases
+// Keep is as 0 and flip all the 1 so far ,for that we need to know about the count the one so far
+// Flip it to 1 and update our count_flip
 
 // Time complexity: O(n)
 // Space complexity: O(1)
