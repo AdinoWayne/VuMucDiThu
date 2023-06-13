aebe8ac4646e57b295193adedb3f2a208132e5ce
Quang Trung <gnurt10@gmail.com>
2023-06-13 07:52:31 +0700

    Create 970-powerful-integers.ts

---
+++ code/medium/970-powerful-integers.ts
@@ -0,0 +1,12 @@
+function powerfulIntegers(x: number, y: number, bound: number): number[] {
+    let ans = new Set<number>();
+    const N = 20;
+    for (let i = 0; i <= N; ++i) {
+        for (let j = 0; j <= N; ++j) {
+            let cand = (x ** i) + (y ** j);
+            if (cand <= bound)
+                ans.add(cand);
+        }
+    }
+    return [...ans];
+};
