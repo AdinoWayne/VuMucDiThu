3ccaad7fc65ebe380d4d1ededdd55b8e3042f5b5
Quang Trung <gnurt10@gmail.com>
2023-06-14 10:38:25 +0700

    Create 973-k-closest-points-to-origin.ts

---
+++ code/medium/973-k-closest-points-to-origin.ts
@@ -0,0 +1,46 @@
+function kClosest(points: number[][], k: number): number[][] {
+    quickSelect(points, k, 0, points.length - 1)
+    return points.slice(0, k)
+};
+
+function quickSelect(points, K, low, high) {
+    if (low >= high) {
+        return
+    }
+    
+    const partPoint = partition(points, low, high)
+    
+    if (partPoint === K - 1) {
+        return
+    }
+    if (partPoint < K - 1) {
+        quickSelect(points, K, partPoint + 1, high)
+    } else {
+        quickSelect(points, K, low, partPoint - 1)
+    }
+}
+
+function partition(points, low, high) {
+    const pivot = points[high]
+    let i = low
+    let j = low
+    while (i < high) {
+        if (getDist(points[i]) < getDist(pivot)) {
+            swap(points, i, j)
+            j++
+        }
+        i++
+    }
+    swap(points, high, j)
+    return j
+}
+
+function getDist(point) {
+    return point[0] * point[0] + point[1] * point[1]
+}
+
+function swap(arr, i, j) {
+    const temp = arr[i] 
+    arr[i] = arr[j]
+    arr[j] = temp
+}
