23dac016fc1c9a29e8f4facf87d250377c677a55
Quang Trung <gnurt10@gmail.com>
2023-06-20 00:22:52 +0700

    Create 986-interval-list-intersections.ts

---
+++ code/medium/986-interval-list-intersections.ts
@@ -0,0 +1,16 @@
+function intervalIntersection(A: number[][], B: number[][]): number[][] {
+    let Ai = 0, Bi = 0;
+    let res = [];
+    
+    while (Ai < A.length && Bi < B.length) {
+        let maxStart = Math.max(A[Ai][0], B[Bi][0]);
+        let minEnd = Math.min(A[Ai][1], B[Bi][1]);
+        
+        if (maxStart <= minEnd) res.push([maxStart, minEnd]) // overlap found
+        
+        if (A[Ai][1] < B[Bi][1]) Ai++;
+        else Bi++;
+    }
+    
+    return res;
+};
