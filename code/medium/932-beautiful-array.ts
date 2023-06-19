f04d896e6d347c59969c92f2ba7dec10d2a3c19f
Quang Trung <gnurt10@gmail.com>
2023-06-19 23:00:45 +0700

    Update 932-beautiful-array.ts

--- code/medium/932-beautiful-array.ts
+++ code/medium/932-beautiful-array.ts
@@ -12,5 +12,10 @@ function beautifulArray(n: number): number[] {
     	return helper(e).concat(helper(o));
     }
 };
+
+// 1,3,5,7,9..... | 2,4,6,8,10..... -> 1,5,9,... | 3,7,11,... -> 1,9... | 5...
+// there is no j in the second part for any i k in the first part
+// there is no j for i in the first part and k in the second part
+
 // TC O(NlogN)
 // SC O(N)
