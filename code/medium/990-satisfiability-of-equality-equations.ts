dc795e7a6177b5bc6a0df0f0071e76d4963540d2
Quang Trung <gnurt10@gmail.com>
2023-06-21 22:48:46 +0700

    Create 990-satisfiability-of-equality-equations.ts

---
+++ code/medium/990-satisfiability-of-equality-equations.ts
@@ -0,0 +1,23 @@
+function equationsPossible(equations: string[]): boolean {
+    let parent = new Map();
+    const find = a => {
+        parent.set(a, parent.get(a) || a);
+        return parent.get(a) === a ? a : find(parent.get(a));
+    }
+    
+    equations.forEach(([a,s,,b]) => {
+        if(s === "="){
+            find(a);
+            find(b);
+            parent.set(find(a), find(b));
+        }
+    })
+    
+    for(let [a,s,,b] of equations){
+        if(s === "!"){
+            if(find(a) === find(b)) return false;
+        }
+    }
+    
+    return true;
+};
