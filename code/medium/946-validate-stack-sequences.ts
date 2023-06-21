c4ac8135d1649accc811202da64a12ec4e602947
Quang Trung <gnurt10@gmail.com>
2023-06-21 23:47:55 +0700

    Update 946-validate-stack-sequences.ts

--- code/medium/946-validate-stack-sequences.ts
+++ code/medium/946-validate-stack-sequences.ts
@@ -1,14 +1,15 @@
 function validateStackSequences(pushed: number[], popped: number[]): boolean {
-    const stack = []
-    let i = 0
-
+    const stack = [];
+    let i = 0;
     for (const el of pushed) {
-        stack.push(el)
+        stack.push(el);
         while (stack.length && stack[stack.length - 1] === popped[i]) {
-            stack.pop()
-            i++
+            stack.pop();
+            i++;
         }
     }
-    
-    return pushed.length === i
+    return pushed.length === i;
 };
+
+// TC O(n)
+// SC O(n)
