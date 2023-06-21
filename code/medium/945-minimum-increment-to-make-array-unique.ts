7b5ce9fe283cb9a73cf6fc874ec2ebfe631f0617
Quang Trung <gnurt10@gmail.com>
2023-06-21 23:39:52 +0700

    Update 945-minimum-increment-to-make-array-unique.ts

--- code/medium/945-minimum-increment-to-make-array-unique.ts
+++ code/medium/945-minimum-increment-to-make-array-unique.ts
@@ -1,19 +1,20 @@
 function minIncrementForUnique(nums: number[]): number {
-  if(!nums || nums.length < 2) { // arrays with 0 or 1 numbers are already unique
-    return 0;
-  }
-  
-    let seen = {};
-    let moves = 0;
-  
-    for(let i=0; i<nums.length; i++ )  {      
-      while(seen[nums[i]] === true) { // if we have seen the num before use a move to incriment it
-        nums[i]++; 
-        moves++;
-      }
-      
-      seen[nums[i]] = true;
+    if(!nums || nums.length < 2) { // arrays with 0 or 1 numbers are already unique
+      return 0;
     }
-  
-  return moves;
+    let seen = {};
+    let output = 0;
+
+    for(let i=0; i<nums.length; i++ )  {      
+        while(seen[nums[i]] === true) {
+            // if we have seen the num before use a move to incriment it
+            nums[i]++; 
+            output++;
+        }
+        seen[nums[i]] = true;
+    }
+    return output;
 };
+
+// TC O(n)
+// SC O(n)
