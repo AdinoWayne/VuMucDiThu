37e6744e539eacad1ae078707d8583fc9c32b23b
Quang Trung <gnurt10@gmail.com>
2023-06-26 23:49:50 +0700

    Update 955-delete-columns-to-make-sorted-ii.ts

--- code/medium/955-delete-columns-to-make-sorted-ii.ts
+++ code/medium/955-delete-columns-to-make-sorted-ii.ts
@@ -3,24 +3,23 @@ function minDeletionSize(strs: string[]): number {
     let inOrder = true, count = 0, deleteIndex = 0;
     while(strs[0].length > 0) {
         inOrder = true;
-        for(let i=0; i<length; i++) {
+        for(let i=0; i<length; i++) { // str.length time
             if(strs[i] > strs[i+1]) {
                 inOrder = false;
                 for(let index = 0; index<strs[i].length;index++) {
                     if(strs[i][index] > strs[i+1][index]) {
-                        deleteIndex = index;
+                        deleteIndex = index; // find index 
                         break;
                     }
                 }
             }
         }
-        for(let i=0; i<length; i++) {
+        for(let i=0; i<length; i++) { // delete column
             strs[i] = strs[i].substring(0,deleteIndex) + strs[i].substring(deleteIndex+1);
         }
         if(inOrder) {
             break;
-        } 
-        else {
+        } else {
             count++;
         }
     }
