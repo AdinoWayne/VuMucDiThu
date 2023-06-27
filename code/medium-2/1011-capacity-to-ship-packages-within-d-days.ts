f5a27c3f3a61593decc391ce41bcedce00ccbe84
Quang Trung <gnurt10@gmail.com>
2023-06-27 07:21:21 +0700

    Create 1011-capacity-to-ship-packages-within-d-days.ts

---
+++ code/medium-2/1011-capacity-to-ship-packages-within-d-days.ts
@@ -0,0 +1,27 @@
+function shipWithinDays(weights: number[], days: number): number {
+  let maxCapacity = 0;
+  let minCapacity = 0;
+
+  for (const weight of weights) {
+    maxCapacity += weight;
+    minCapacity = Math.max(minCapacity, weight);
+  }
+
+  while (minCapacity < maxCapacity) {
+    const capacity = Math.floor((maxCapacity + minCapacity) / 2);
+    let currentDays = 1;
+    let currentLoad = 0;
+    for (const weight of weights) {
+      currentLoad += weight;
+      if (currentLoad > capacity) {
+        currentDays++;
+        currentLoad = weight;
+      }
+    }
+
+    if (currentDays > days) minCapacity = capacity + 1;
+    else maxCapacity = capacity;
+  }
+
+  return minCapacity;
+};
