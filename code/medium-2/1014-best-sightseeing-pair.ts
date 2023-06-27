e9890479fbf8c0b8176f161e7cf06d9040981527
Quang Trung <gnurt10@gmail.com>
2023-06-27 07:22:01 +0700

    Create 1014-best-sightseeing-pair.ts

---
+++ code/medium-2/1014-best-sightseeing-pair.ts
@@ -0,0 +1,12 @@
+function maxScoreSightseeingPair(values: number[]): number {
+    let prevMax = values[0], max = 0;
+
+    for(let i = 1; i < values.length; i++) {
+
+        prevMax--;
+        max = Math.max(max, prevMax + values[i]);
+        prevMax = Math.max(prevMax, values[i]);
+    }
+    
+    return max;
+};
