e80df5945123b6046cff1a43be8a37b49c4d7af2
Quang Trung <gnurt10@gmail.com>
2023-06-28 11:12:15 +0700

    Create 1016-binary-string-with-substrings-representing-1-to-n.ts

---
+++ code/medium-2/1016-binary-string-with-substrings-representing-1-to-n.ts
@@ -0,0 +1,8 @@
+function queryString(s: string, n: number): boolean {
+    for(let i=n;i>=1;i--){
+        let bin=(i>>>0).toString(2);
+        if(s.search(bin)===-1)
+            return false;
+    }
+    return true;
+};
