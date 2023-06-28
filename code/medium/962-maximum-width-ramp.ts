cba29de5c6b3d962a466b438e68da936c5c20883
Quang Trung <gnurt10@gmail.com>
2023-06-28 11:22:43 +0700

    Update 962-maximum-width-ramp.ts

--- code/medium/962-maximum-width-ramp.ts
+++ code/medium/962-maximum-width-ramp.ts
@@ -1,12 +1,24 @@
 function maxWidthRamp(nums: number[]): number {
-    let result = 0;
-    nums.map((el,i) => {
-        let j = nums.length-1;
-        while(nums[j] < nums[i]) j--;
-        result = Math.max(result, j-i);
-    });
-    return result;
-};
+  const n = nums.length;
 
-// TC O(n^2)
-// SC O(1)
+  // Create an array of objects containing the number and its original index
+  const numsWithIndices = nums.map((num, index) => ({ num, index }));
+
+  // Sort the array in ascending order based on the numbers
+  numsWithIndices.sort((a, b) => a.num - b.num);
+
+  let result = 0;
+  let minIndex = n;
+
+  for (const { index } of numsWithIndices) {
+    // Calculate the width of the ramp
+    result = Math.max(result, index - minIndex);
+    // Update the minimum index encountered so far
+    minIndex = Math.min(minIndex, index);
+  }
+
+  return result;
+}
+
+// TC O(nlogn)
+// SC O(n)
