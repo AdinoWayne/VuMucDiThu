af5b3acd2658217a15a174f906ee39fa31bc8141
Quang Trung <gnurt10@gmail.com>
2023-07-01 08:39:20 +0700

    Create 1026-maximum-difference-between-node-and-ancestor.ts

---
+++ code/medium-2/1026-maximum-difference-between-node-and-ancestor.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxAncestorDiff(root: TreeNode | null): number {
+    if(root == null) { return 0; }
+	
+    let result = 0;
+    helper(root, root.val, root.val);
+    
+    function helper(root, max, min) {
+        if(root == null) {
+            result = Math.max(result, max-min);
+            return;
+        }
+        
+        max = Math.max(max, root.val);
+        min = Math.min(min, root.val);
+        
+        helper(root.left, max, min);
+        helper(root.right, max, min);
+    }
+
+    return result;
+};
