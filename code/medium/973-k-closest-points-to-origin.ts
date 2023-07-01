abbf24d66d2755cd62c0980aacc83277263b78fe
Quang Trung <gnurt10@gmail.com>
2023-07-01 11:22:38 +0700

    Update 973-k-closest-points-to-origin.ts

--- code/medium/973-k-closest-points-to-origin.ts
+++ code/medium/973-k-closest-points-to-origin.ts
@@ -5,44 +5,44 @@ function kClosest(points: number[][], k: number): number[][] {
 
 function quickSelect(points, K, low, high) {
     if (low >= high) {
-        return
+        return;
     }
     
-    const partPoint = partition(points, low, high)
+    const partPoint = partition(points, low, high) // count points less than points[high]
     
     if (partPoint === K - 1) {
-        return
+        return;
     }
     if (partPoint < K - 1) {
-        quickSelect(points, K, partPoint + 1, high)
+        quickSelect(points, K, partPoint + 1, high);
     } else {
-        quickSelect(points, K, low, partPoint - 1)
+        quickSelect(points, K, low, partPoint - 1);
     }
 }
 
 function partition(points, low, high) {
-    const pivot = points[high]
-    let i = low
-    let j = low
+    const pivot = points[high];
+    let i = low;
+    let j = low;
     while (i < high) {
         if (getDist(points[i]) < getDist(pivot)) {
-            swap(points, i, j)
-            j++
+            swap(points, i, j);
+            j++;
         }
-        i++
+        i++;
     }
-    swap(points, high, j)
-    return j
+    swap(points, high, j);
+    return j;
 }
 
 function getDist(point) {
-    return point[0] * point[0] + point[1] * point[1]
+    return point[0] * point[0] + point[1] * point[1];
 }
 
 function swap(arr, i, j) {
-    const temp = arr[i] 
-    arr[i] = arr[j]
-    arr[j] = temp
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
 }
 // Time complexity: O(n) in the average case and O(n^2) in the worst case
 // Space complexity: O(1)
