3cd8cca04dfb2ab39cba96c3d099fa935a7922b0
Quang Trung <gnurt10@gmail.com>
2023-07-05 06:41:43 +0700

    Create 1039-minimum-score-triangulation-of-polygon.ts

---
+++ code/medium-2/1039-minimum-score-triangulation-of-polygon.ts
@@ -0,0 +1,22 @@
+function minScoreTriangulation(values: number[]): number {
+  let n = values.length;
+  let dp = new Array(n).fill(-1).map(() => Array(n).fill(-1));
+  return solveMem(values, 0, n - 1, dp);
+};
+
+function solveMem(value, i, j, dp) {
+  // base case
+  if (i + 1 === j) return 0;
+  if (dp[i][j] !== -1) return dp[i][j];
+  let ans = Number.MAX_VALUE;
+  for (let k = i + 1; k < j; k++) {
+    ans = Math.min(
+      ans,
+      value[i] * value[j] * value[k] +
+        solveMem(value, i, k, dp) +
+        solveMem(value, k, j, dp)
+    );
+  }
+  dp[i][j] = ans;
+  return dp[i][j];
+}
