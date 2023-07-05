a2c045cc72b08e7888771123ea2a23e7cf34a15d
Quang Trung <gnurt10@gmail.com>
2023-07-06 04:18:42 +0700

    Create 1042-flower-planting-with-no-adjacent.ts

---
+++ code/medium-2/1042-flower-planting-with-no-adjacent.ts
@@ -0,0 +1,32 @@
+function gardenNoAdj(n: number, paths: number[][]): number[] {
+    const gardens = new Array(n);
+    const adjList = [...new Array(n)].map(() => []);
+    
+    // Create adjancey list
+    for (const [from, to] of paths) {
+        adjList[from - 1].push(to - 1);
+        adjList[to - 1].push(from - 1);
+    }
+    
+    for (let i = 0; i < gardens.length; i++) {
+        const neighborValues = {};
+        const tos = adjList[i];
+        
+        for (const to of tos) {
+            const neighborValue = gardens[to];
+            
+            if (neighborValue) {
+                neighborValues[neighborValue] = true;
+            }
+        }
+
+        for (let val = 1; val <= 4; val++) {
+            if (!neighborValues[val]) {
+                gardens[i] = val;
+                break;
+            }
+        }
+    }
+    
+    return gardens;
+};
