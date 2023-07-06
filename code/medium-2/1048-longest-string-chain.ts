a80a58813bb357285be7043157470a45532e37af
Quang Trung <gnurt10@gmail.com>
2023-07-07 00:44:37 +0700

    Create 1048-longest-string-chain.ts

---
+++ code/medium-2/1048-longest-string-chain.ts
@@ -0,0 +1,19 @@
+function longestStrChain(words: string[]): number {
+    let memory = {}, resultCount = 0, localLongest = 0, prevString = "";
+    
+    words.sort((a,b) => a.length - b.length);
+    
+    for (var word of words){
+        for (var i =0; i<word.length; i++){
+            //remove the current ith char from the string
+            prevString = word.slice(0,i) + word.slice(i+1);
+            // search that string in memory and store the max in localLongest
+            localLongest = Math.max(localLongest, (memory[prevString] || 0)+1)
+        }
+        // holds the current longest chain count;
+        resultCount = Math.max(localLongest, resultCount);
+        memory[word] = localLongest
+        localLongest = 0;
+    }
+    return resultCount;
+};
