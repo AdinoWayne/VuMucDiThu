68cec77cb56be2cd576fc07b5a5d7c25f11b4276
Quang Trung <gnurt10@gmail.com>
2023-07-08 10:56:03 +0700

    Create 1052-grumpy-bookstore-owner.ts

---
+++ code/medium-2/1052-grumpy-bookstore-owner.ts
@@ -0,0 +1,21 @@
+function maxSatisfied(customers: number[], grumpy: number[], minutes: number): number {
+    let satisfied = 0;
+  let maxGrumpySatisfied = 0;
+  
+  for (let i = 0; i < grumpy.length; i++) {
+    if (grumpy[i] === 0) {
+      satisfied += customers[i];
+      customers[i] = i === 0 ? 0 : customers[i - 1];
+      continue;
+    }
+    
+    if (i > 0) {
+      customers[i] += customers[i - 1];
+    }
+    
+    const grumpySatisfied = minutes <= i ? customers[i] - customers[i - minutes] : customers[i];
+    maxGrumpySatisfied = Math.max(maxGrumpySatisfied, grumpySatisfied);
+  }
+  
+  return satisfied + maxGrumpySatisfied;
+};
