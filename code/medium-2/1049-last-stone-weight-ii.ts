011277556380acbc3ee298f4951d2e6a900b23c3
Quang Trung <gnurt10@gmail.com>
2023-07-08 10:56:43 +0700

    Create 1049-last-stone-weight-ii.ts

---
+++ code/medium-2/1049-last-stone-weight-ii.ts
@@ -0,0 +1,21 @@
+function lastStoneWeightII(stones: number[]): number {
+    const memo = new Map();
+    return findMinWeight(0, 0, 0);
+    
+    function findMinWeight(index, weight1, weight2) {
+        const key = `${index}#${weight1}#${weight2}`;
+        
+        // base case
+        if (index == stones.length) return Math.abs(weight1 - weight2);
+        if (memo.has(key)) return memo.get(key);
+        
+        const currWeight = stones[index];
+        const min =  Math.min(
+            findMinWeight(index + 1, weight1 + currWeight, weight2), 
+            findMinWeight(index + 1, weight1, weight2 + currWeight
+        ));
+        
+        memo.set(key, min);
+        return min;
+    }
+};
