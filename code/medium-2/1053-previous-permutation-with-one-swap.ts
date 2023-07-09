cf7c5657daef66ac361886a2f050cc3c95ed2f89
Quang Trung <gnurt10@gmail.com>
2023-07-09 11:42:01 +0700

    Create 1053-previous-permutation-with-one-swap.ts

---
+++ code/medium-2/1053-previous-permutation-with-one-swap.ts
@@ -0,0 +1,19 @@
+function prevPermOpt1(arr: number[]): number[] {
+    for (let index = arr.length - 1; index > 0; index--) {
+        const value = arr[index];
+        const target = arr[index - 1];
+        if (value >= target) continue;
+        let swapPosition = -1;
+        let max = 0;
+
+        for (let position = index; position < arr.length; position++) {
+            const swapValue = arr[position];
+            if (swapValue <= max || swapValue >= target) continue;
+            swapPosition = position;
+            max = swapValue;
+        }
+        [arr[index - 1], arr[swapPosition]] = [arr[swapPosition], arr[index - 1]];
+        break;
+    }
+    return arr;
+};
