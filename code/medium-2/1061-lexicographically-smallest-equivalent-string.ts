616ecfa24c942757f0ef87636ea587908314d551
Quang Trung <gnurt10@gmail.com>
2023-07-10 06:46:48 +0700

    Create 1061-lexicographically-smallest-equivalent-string.ts

---
+++ code/medium-2/1061-lexicographically-smallest-equivalent-string.ts
@@ -0,0 +1,30 @@
+function smallestEquivalentString(s1: string, s2: string, baseStr: string): string {
+    const link = {};
+    const find = (a)=>{
+        if(link[a] == null) link[a] = a;
+        while(link[a] != a){
+            link[a] = link[link[a]];
+            a = link[a];
+        }
+        return a;
+    }
+    const union = (a, b)=>{
+        a = find(a);
+        b = find(b);
+        if(a > b){
+            let temp = b;
+            b = a;
+            a = temp;
+        }
+        link[b] = a;
+    }
+    for(let i = 0; i < s1.length; i++){
+        union(s1[i], s2[i]);
+    }
+
+    let ans = "";
+    for(const a of baseStr){
+        ans+=find(a);
+    }
+    return ans;
+};
