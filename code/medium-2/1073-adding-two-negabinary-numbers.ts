6823864279f2e0c4c4befb621c274256407ccbfa
Quang Trung <gnurt10@gmail.com>
2023-07-11 06:55:33 +0700

    Create 1073-adding-two-negabinary-numbers.ts

---
+++ code/medium-2/1073-adding-two-negabinary-numbers.ts
@@ -0,0 +1,42 @@
+function addNegabinary(a: number[], b: number[]): number[] {
+    // reverse first
+    a = a.reverse(), b = b.reverse();
+
+    // set c as third array, get max as number of loops
+    let c = dp(Math.max(a.length, b.length));
+
+    // remove 0
+    while (c.length > 1 && c[0] == 0) {
+        c.shift();
+    }
+
+    // result
+    return c;
+
+    // dp
+    function dp(max, r = []) {
+        for (let i = 0; i <= max; i++) {
+            // fault-tolerant
+            r[i] = (a[i] || 0) + (b[i] || 0) + (r[i] || 0);
+
+            if (r[i] == -1) {
+                r[i] = 1;
+                r[i + 1] = 1;
+                continue;
+            }
+
+            if (r[i] == 2) {
+                r[i] = 0;
+                r[i + 1] = -1;
+                continue;
+            }
+
+            if (r[i] == 3) {
+                r[i] = 1;
+                r[i + 1] = -1;
+                continue;
+            }
+        }
+        return r.reverse();
+    }
+};
