fda4ef0e039dded07fdcd3f563a7da32b64f14fa
Quang Trung <gnurt10@gmail.com>
2023-07-11 06:57:15 +0700

    Create 1079-letter-tile-possibilities.ts

---
+++ code/medium-2/1079-letter-tile-possibilities.ts
@@ -0,0 +1,19 @@
+function numTilePossibilities(tiles: string): number {
+	let map = new Map();
+	for(let i = 0; i < tiles.length; i++) {
+		map.set(tiles[i], (map.get(tiles[i]) || 0) +  1);
+	}
+	return helper(map);
+};
+
+function helper(map) {
+    let sum = 0;
+    for(let [key, value] of map.entries()) {
+        if(value === 0) continue;
+        sum++;
+        map.set(key, map.get(key) - 1);
+        sum += helper(map);
+        map.set(key, map.get(key) + 1);
+    }
+    return sum
+}
