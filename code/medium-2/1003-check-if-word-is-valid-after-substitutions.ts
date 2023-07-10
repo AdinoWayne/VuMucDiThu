b3a4ba220b624931b978435f902a7974a8f1a710
Quang Trung <gnurt10@gmail.com>
2023-07-10 07:07:48 +0700

    Update 1003-check-if-word-is-valid-after-substitutions.ts

--- code/medium-2/1003-check-if-word-is-valid-after-substitutions.ts
+++ code/medium-2/1003-check-if-word-is-valid-after-substitutions.ts
@@ -4,3 +4,5 @@ function isValid(S: string): boolean {
     }
     return S.length === 0;
 };
+// Since the split and join methods have a time complexity of O(n), the overall time complexity of the function is O(n^2).
+// The space complexity is O(n) because the input string S is stored in memory
