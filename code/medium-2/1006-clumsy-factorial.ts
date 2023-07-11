0fd87490e0105b80517ba32ddfc3db00fc840401
Quang Trung <gnurt10@gmail.com>
2023-07-11 07:32:01 +0700

    Update 1006-clumsy-factorial.ts

--- code/medium-2/1006-clumsy-factorial.ts
+++ code/medium-2/1006-clumsy-factorial.ts
@@ -1,24 +1,20 @@
 function clumsy(N: number): number {
-    if(N < 3) return N
-    
-    let ans = Math.floor((N) * (N-1) / (N-2))
-    
-    N -= 3
-    
-    while(N > 0){
-        
+    if(N < 3) return N;
+    let ans = Math.floor((N) * (N-1) / (N-2));
+    N -= 3;
+    while (N > 0) {
         if(N > 3){
-            ans += ((N) - Math.floor((N-1) * (N-2) / (N-3)))
+            ans += ((N) - Math.floor((N-1) * (N-2) / (N-3)));
         } else if(N === 3){
-            ans += ((N) - (N-1) * (N-2))
+            ans += ((N) - (N-1) * (N-2));
         } else if(N === 2){
-            ans += ((N) - (N-1))
+            ans += ((N) - (N-1));
         } else if(N === 1){
-            ans += (N)
+            ans += (N);
         }
-            
-        N -= 4
+        N -= 4;
     }
-    
-    return ans
+    return ans;
 };
+// TC O(n)
+// SC O(1)
