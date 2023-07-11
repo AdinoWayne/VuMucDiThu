9b12ab846291523f965af1de685c54e7e37a7db1
Quang Trung <gnurt10@gmail.com>
2023-07-12 06:25:07 +0700

    Create 1081-smallest-subsequence-of-distinct-characters.ts

---
+++ code/medium-2/1081-smallest-subsequence-of-distinct-characters.ts
@@ -0,0 +1,21 @@
+function smallestSubsequence(s: string): string {
+    let st = [], occurence = [], seen = [];
+
+    for(let i=0; i<s.length; i++){
+        occurence[s[i]] = i;
+    }
+
+    for(let i=0; i<s.length; i++){
+        if(seen[s[i]]) continue;
+
+        while(st.length > 0 && s[i] < st[st.length-1] && occurence[st[st.length-1]] > i) {
+            let ch = st.pop();
+            seen[ch] = false;
+        }
+
+        st.push(s[i]);
+        seen[s[i]] = true;
+    }
+
+    return st.join("");
+};
