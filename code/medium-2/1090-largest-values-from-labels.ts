ae0e8faa681a89db422fb8ecd9034795a1d36d96
Quang Trung <gnurt10@gmail.com>
2023-07-13 06:53:29 +0700

    Create 1090-largest-values-from-labels.ts

---
+++ code/medium-2/1090-largest-values-from-labels.ts
@@ -0,0 +1,15 @@
+function largestValsFromLabels(values: number[], labels: number[], numWanted: number, useLimit: number): number {
+    let avl = values //
+        .map((v, i) => [v, labels[i]])
+        .sort((a, b) => b[0] - a[0]);
+    let m = new Map();
+    let sum = 0;
+    for (let [v, l] of avl) {
+        if (!numWanted) break;
+        if (m.get(l) >= useLimit) continue;
+        m.set(l, (m.get(l) || 0) + 1);
+        sum += v;
+        numWanted--;
+    }
+    return sum;
+};
