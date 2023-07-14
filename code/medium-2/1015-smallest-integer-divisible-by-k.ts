d28d2dd37cd90fa295817330f4ee36a0e64841bf
Quang Trung <gnurt10@gmail.com>
2023-07-14 07:54:40 +0700

    Update 1015-smallest-integer-divisible-by-k.ts

--- code/medium-2/1015-smallest-integer-divisible-by-k.ts
+++ code/medium-2/1015-smallest-integer-divisible-by-k.ts
@@ -18,5 +18,14 @@ function smallestRepunitDivByK(k: number): number {
     }
     return count
 };
+
+// N next = (N * 10) + 1 : the next value of N 1 -> 11 -> 111 ...
+// remainder = N%K : the remainder
+
+// (N next)%K = ((N * 10) + 1)%K : we can mod both sides of our next remainder equation
+// (N next)%K = ((N * 10)%K + 1%K)%K : the equation can then be rearranged. "It is useful to know that modulos can be taken anywhere in the calculation if it involves only addition and multiplication." - duke-cps102
+// (N next)%K = ((N * 10)%K + 1)%K : 1 mod anything is 1
+// (N next)%K = ((N * 10)%K + 1)%K : 1 mod anything is 1
+// remainder next = (remainder*10 + 1) %K : knowing that N%K is our remainder, we have now have a general equation that can find our next remainder value without knowing the value of N.
 // TC O(n)
 // SC O(n)
