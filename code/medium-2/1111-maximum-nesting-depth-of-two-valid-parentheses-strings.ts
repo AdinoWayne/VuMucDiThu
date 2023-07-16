45227fa89328fd8fa519a406704ecbc95ab5aaed
Quang Trung <gnurt10@gmail.com>
2023-07-17 06:40:55 +0700

    Create 1111-maximum-nesting-depth-of-two-valid-parentheses-strings.ts

---
+++ code/medium-2/1111-maximum-nesting-depth-of-two-valid-parentheses-strings.ts
@@ -0,0 +1,27 @@
+function maxDepthAfterSplit(seq: string): number[] {
+    let res = new Array(seq.length).fill(0);
+    let stack0 = [];
+    let stack1 = [];
+    
+    for(let i=0; i<seq.length; i++){
+        if(seq[i] === "("){
+            if(stack0 <= stack1) {
+                stack0.push("(");
+                res[i] = 0;
+            } else {
+                stack1.push("(");
+                res[i] = 1;
+            }
+        } else {
+            if(stack0 <= stack1){
+                stack1.pop();
+                res[i] = 1;
+            } else {
+                stack0.pop();
+                res[i] = 0;
+            }
+        }
+    }
+    
+    return res;
+};
