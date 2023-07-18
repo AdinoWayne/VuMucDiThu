c54b9c6951760e73820e74e45e6eb4b3eb01d83c
Quang Trung <gnurt10@gmail.com>
2023-07-18 07:01:48 +0700

    Create 1124-longest-well-performing-interval.ts

---
+++ code/medium-2/1124-longest-well-performing-interval.ts
@@ -0,0 +1,34 @@
+function longestWPI(hours: number[]): number {
+    let res = 0;
+    let count = 0;
+    let arr = [];
+    // prefix sum
+    for(let i=0; i<hours.length; i++){
+        let h = hours[i];
+        if(h > 8){
+            count ++;
+        }else{
+            count --;
+        }
+        arr.push(count);
+        // consider start from 0
+        if(count > 0){
+            res = i+1;
+        }
+    }
+    // consider start from 1
+    for(let i=0; i<arr.length-1; i++){
+        for(let j=arr.length-1; j>=i+1; j--){
+		    // look for the longest, if found then break
+            if(arr[j] - arr[i] > 0){
+                let len = j-i;
+                if(len > res){
+                    res = len;
+                }
+                break;
+            }
+        }
+    }
+
+    return res; 
+};
