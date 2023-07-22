dd6414925590c97f5c8c855a7156ffa0d8a1b422
Quang Trung <gnurt10@gmail.com>
2023-07-22 08:36:13 +0700

    Create 1141-decrease-elements-to-make-array-zigzag.ts

---
+++ code/medium-2/1141-decrease-elements-to-make-array-zigzag.ts
@@ -0,0 +1,20 @@
+function movesToMakeZigzag(nums: number[]): number {
+   let even = zig(nums, 0, 0);
+   let odd = zig(nums, 0, 1);
+   return Math.min(even, odd);
+};
+
+function zig(nums, style, start){
+    let arr = [...nums];
+    for(let i = start; i < nums.length; i += 2){
+       if(arr[i] >= arr[i - 1]){
+           style += arr[i] - arr[i - 1] + 1;
+           arr[i] = arr[i - 1] - 1; 
+       }
+       if(arr[i] >= arr[i + 1]){
+           style += arr[i] - arr[i + 1] + 1;
+           arr[i] = arr[i + 1] - 1; 
+       }
+   }
+   return style;
+}
