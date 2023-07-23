006df49edc0d7c60c0f1ab3858100bc3238e5541
Quang Trung <gnurt10@gmail.com>
2023-07-24 06:44:24 +0700

    Create 1161-maximum-level-sum-of-a-binary-tree.ts

---
+++ code/medium-2/1161-maximum-level-sum-of-a-binary-tree.ts
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxLevelSum(root: TreeNode | null): number {
+    if (!root)
+        return 1;
+    
+    const queue = [root];
+    const levels = [];
+    
+    while (queue.length) {
+        const level = [];
+        let size = queue.length;
+        
+        for (let i = 0; i < size; i++) {
+            const node = queue.shift();
+            level.push(node.val);
+            
+            if (node.left)
+                queue.push(node.left);
+            if (node.right)
+                queue.push(node.right);
+        }
+        
+        levels.push(level.reduce((acc, curr) => acc + curr));
+    }
+    
+    return levels.indexOf(Math.max(...levels)) + 1;
+};
