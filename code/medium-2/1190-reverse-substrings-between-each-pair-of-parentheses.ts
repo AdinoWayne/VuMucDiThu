8bbdfc4b669c49f0b6472d9f81d95e6a5bd833f4
Quang Trung <gnurt10@gmail.com>
2023-07-28 06:24:53 +0700

    Create 1190-reverse-substrings-between-each-pair-of-parentheses.ts

---
+++ code/medium-2/1190-reverse-substrings-between-each-pair-of-parentheses.ts
@@ -0,0 +1,11 @@
+function reverseParentheses(s: string): string {
+    while (s.includes("(")) { // Check if s has parentheses;
+        s = s.replace(/\([^\(\)]+\)|\(\)/g, (match) => { 
+		
+			// If parentheses are detected, remove them and reverse all of the content inside 
+            return match.slice(1, -1).split("").reverse().join("");
+        })
+    }
+    
+    return s;
+};
