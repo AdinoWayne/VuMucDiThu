5f481b45fce05f4c69ef951086b5c1743fc59218
Quang Trung <gnurt10@gmail.com>
2023-07-31 06:35:56 +0700

    Create 1218-longest-arithmetic-subsequence-of-given-difference.ts

---
+++ code/medium-2/1218-longest-arithmetic-subsequence-of-given-difference.ts
@@ -0,0 +1,14 @@
+function longestSubsequence(arr: number[], difference: number): number {
+    const map = new Map();
+    let max = 0;
+    
+    for (const num of arr) {
+        const diff = num - difference;
+        const len = (map.get(diff) ?? 0) + 1;
+        
+        map.set(num, len);
+        max = Math.max(len, max);
+    }
+    
+    return max;
+};
