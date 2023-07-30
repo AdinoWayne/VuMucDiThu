897622ffcbea9a1381f3f8c3c73912ce4e31b378
Quang Trung <gnurt10@gmail.com>
2023-07-30 09:49:40 +0700

    Create 1208-get-equal-substrings-within-budget.ts

---
+++ code/medium-2/1208-get-equal-substrings-within-budget.ts
@@ -0,0 +1,11 @@
+function equalSubstring(s: string, t: string, maxCost: number): number {
+  let left = -1;
+  for (let right = 0; right < s.length; ++right) {
+    maxCost -= Math.abs(s.charCodeAt(right) - t.charCodeAt(right));
+    if (maxCost < 0) {
+      ++left;
+      maxCost += Math.abs(s.charCodeAt(left) - t.charCodeAt(left));
+    }
+  }
+  return s.length - left - 1;
+};
