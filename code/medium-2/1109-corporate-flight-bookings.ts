ba2b3dbe3498831e096635c204f2f12d242b2f5f
Quang Trung <gnurt10@gmail.com>
2023-08-01 07:47:15 +0700

    Update 1109-corporate-flight-bookings.ts

--- code/medium-2/1109-corporate-flight-bookings.ts
+++ code/medium-2/1109-corporate-flight-bookings.ts
@@ -26,5 +26,16 @@ function corpFlightBookings(bookings: number[][], n: number): number[] {
   }
   return result;
 };
+
+// Input: bookings = [[1,2,10],[2,3,20],[2,5,25]], n = 5
+// Output: [10,55,45,25,25]
+// Explanation:
+// Flight labels:        1   2   3   4   5
+// Booking 1 reserved:  10  10
+// Booking 2 reserved:      20  20
+// Booking 3 reserved:      25  25  25  25
+// Total seats:         10  55  45  25  25
+// Hence, answer = [10,55,45,25,25]
+
 // TC O(n)
 // SC o(n)
