f1c01df14296d70f855b38fbbdd4b3c51d8f8230
Quang Trung <gnurt10@gmail.com>
2023-08-03 06:59:50 +0700

    Create 1237-find-positive-integer-solution-for-a-given-equation.ts

---
+++ code/medium-2/1237-find-positive-integer-solution-for-a-given-equation.ts
@@ -0,0 +1,17 @@
+/**
+ * // This is the CustomFunction's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class CustomFunction {
+ *      f(x: number, y: number): number {}
+ * }
+ */
+
+function findSolution(customfunction: CustomFunction, z: number): number[][] {
+    const ret = [];
+    for (let x = 1, y = 1000; x <= 1000 && y >= 1;) {
+        const val = customfunction.f(x, y);
+        val === z && ret.push([x, y]);
+        val < z ? ++x : --y;
+    }
+    return ret;
+};
