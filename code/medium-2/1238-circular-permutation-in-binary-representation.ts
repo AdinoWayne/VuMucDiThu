c5a54612934ab29a47123694c1b574a116d0a74a
Quang Trung <gnurt10@gmail.com>
2023-08-03 07:00:42 +0700

    Create 1238-circular-permutation-in-binary-representation.ts

---
+++ code/medium-2/1238-circular-permutation-in-binary-representation.ts
@@ -0,0 +1,12 @@
+function circularPermutation(n: number, start: number): number[] {
+    let a = grayCodeStartingWith(n, start);
+    return a;
+};
+
+var grayCodeStartingWith = function (n, start) {
+    let a = [];
+    for (let i = 0; i < 2 ** n; i++) {
+        a.push(i ^ (i / 2) ^ start);
+    }
+    return a;
+};
