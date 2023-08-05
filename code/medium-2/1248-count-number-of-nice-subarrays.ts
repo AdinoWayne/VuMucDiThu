4fbe5ac588d79de4223df7f102364ff84db0f2f2
Quang Trung <gnurt10@gmail.com>
2023-08-05 10:39:41 +0700

    Create 1248-count-number-of-nice-subarrays.ts

---
+++ code/medium-2/1248-count-number-of-nice-subarrays.ts
@@ -0,0 +1,27 @@
+function numberOfSubarrays(nums: number[], k: number): number {
+    let count = 0;
+    let odd = 0;
+    let l = 0, r = -1;
+    while(r < nums.length-1) {
+        r++;
+        if (nums[r]%2 !== 0) odd++;
+        if (odd === k) {
+            let right = 1;
+            let left = 1;
+            // move r if next element is even
+            while(r < nums.length-1 && nums[r+1]%2 === 0) {
+                r++;
+                right++;
+            }
+            // move l if current element for l is even
+            while(l < r && nums[l]%2 === 0) {
+                l++;
+                left++;
+            }
+            count += left*right;
+            l++;
+            odd--;
+        }
+    }
+    return count;
+};
