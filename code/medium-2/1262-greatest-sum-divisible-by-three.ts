af9377e0581113ab0b3b862a700565bdfadf3b4f
Quang Trung <gnurt10@gmail.com>
2023-08-07 06:57:20 +0700

    Create 1262-greatest-sum-divisible-by-three.ts

---
+++ code/medium-2/1262-greatest-sum-divisible-by-three.ts
@@ -0,0 +1,22 @@
+function maxSumDivThree(nums: number[]): number {
+    let remainOne = 10 ** 4;
+    let remainTwo = 10 ** 4;
+    let sum = 0;
+
+    nums.forEach(num => {
+        sum += num;
+        if (num % 3 === 1) {
+            remainTwo = Math.min(remainTwo, remainOne + num);
+            remainOne = Math.min(remainOne, num);
+        } else if (num % 3 === 2) {
+            remainOne = Math.min(remainOne, remainTwo + num);
+            remainTwo = Math.min(remainTwo, num);
+        }
+    });
+
+    const remainSum = sum % 3;
+
+    if (remainSum === 0) return sum;
+    if (remainSum === 1) return sum - remainOne;
+    return sum - remainTwo;
+};
