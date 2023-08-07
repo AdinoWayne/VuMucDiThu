c138fa38bcf22bf68844f8ad0d6288ad5b7c6b07
Quang Trung <gnurt10@gmail.com>
2023-08-08 06:42:42 +0700

    Create 1268-search-suggestions-system.ts

---
+++ code/medium-2/1268-search-suggestions-system.ts
@@ -0,0 +1,9 @@
+function suggestedProducts(products: string[], searchWord: string): string[][] {
+    products.sort();
+    let res = [];
+    for (let i=0;i<searchWord.length;i++) {
+        products = products.filter((p)=>p[i]==searchWord[i]);
+        res.push(products.slice(0, 3));
+    }
+    return res;
+};
