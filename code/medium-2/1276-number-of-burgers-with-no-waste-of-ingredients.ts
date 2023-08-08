ae04a23f9c43e222bad8edb0b71338e92457adcc
Quang Trung <gnurt10@gmail.com>
2023-08-09 06:35:54 +0700

    Create 1276-number-of-burgers-with-no-waste-of-ingredients.ts

---
+++ code/medium-2/1276-number-of-burgers-with-no-waste-of-ingredients.ts
@@ -0,0 +1,11 @@
+function numOfBurgers(tomatoSlices: number, cheeseSlices: number): number[] {
+    if (cheeseSlices * 2 > tomatoSlices) return [];
+    if (tomatoSlices % 2 === 1) return [];
+    if (tomatoSlices === 0 && cheeseSlices === 0) return [0, 0];
+
+    const maxSmallBurger = tomatoSlices / 2;
+    const jumboBurger = maxSmallBurger - cheeseSlices;
+    const smallBurger = maxSmallBurger - jumboBurger * 2;
+
+    return smallBurger >= 0 ? [jumboBurger, smallBurger] : [];
+};
