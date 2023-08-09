cd0b18240c47f3539c60eb05d4c6a1dcac3eaed9
Quang Trung <gnurt10@gmail.com>
2023-08-10 06:44:22 +0700

    Create 1283-find-the-smallest-divisor-given-a-threshold.ts

---
+++ code/medium-2/1283-find-the-smallest-divisor-given-a-threshold.ts
@@ -0,0 +1,13 @@
+function smallestDivisor(nums: number[], threshold: number): number {
+  // Use 10e6 as the max
+  let left = 1, right = 10e6;
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    let sum = 0;
+    for (let i = 0; i < nums.length; ++i) {
+      sum += Math.ceil(nums[i] / mid);
+    }
+    sum > threshold ? (left = mid + 1) : (right = mid);
+  }
+  return left;
+};
