aac358aa4e2a110833399aac2a74cdd1285346ad
Quang Trung <gnurt10@gmail.com>
2023-08-10 06:43:25 +0700

    Create 1282-group-the-people-given-the-group-size-they-belong-to.ts

---
+++ code/medium-2/1282-group-the-people-given-the-group-size-they-belong-to.ts
@@ -0,0 +1,20 @@
+function groupThePeople(groupSizes: number[]): number[][] {
+    if (!groupSizes.length) return [];
+
+    const result = [];
+    const map = new Map();
+
+    for (let i = 0; i < groupSizes.length; i++) {
+        const groupSize = groupSizes[i];
+
+        if (!map.get(groupSize)) map.set(groupSize, []);
+        map.get(groupSize).push(i);
+
+        if (map.get(groupSize).length === groupSize) {
+            result.push(map.get(groupSize));
+            map.delete(groupSize);
+        }
+    }
+
+    return result;
+};
