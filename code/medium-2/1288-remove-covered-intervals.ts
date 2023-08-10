9657c8aedcaaa6c4da8f1f1ed53ec5bfe6ec2e8c
Quang Trung <gnurt10@gmail.com>
2023-08-11 06:46:41 +0700

    Create 1288-remove-covered-intervals.ts

---
+++ code/medium-2/1288-remove-covered-intervals.ts
@@ -0,0 +1,11 @@
+function removeCoveredIntervals(intervals: number[][]): number {
+    intervals.sort((a, b) => (a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]))
+    let count = 0, cur = 0
+    for(let interval of intervals){
+        if(cur < interval[1]){
+            cur = interval[1]
+            count++
+        }
+    }
+    return count
+};
