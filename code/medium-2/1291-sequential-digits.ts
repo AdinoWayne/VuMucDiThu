870424469cd8df3538ddfc85574a1e841f63a693
Quang Trung <gnurt10@gmail.com>
2023-08-12 07:44:39 +0700

    Create 1291-sequential-digits.ts

---
+++ code/medium-2/1291-sequential-digits.ts
@@ -0,0 +1,20 @@
+function sequentialDigits(low: number, high: number): number[] {
+    const digits = '123456789';
+    const ans = [];
+    
+    const minLen = low.toString().length;
+    const maxLen = high.toString().length;
+    
+    for (let windowSize = minLen; windowSize <= maxLen; ++windowSize) {
+        for (let i = 0; i + windowSize <= digits.length; ++i) {
+            const num = parseInt(digits.substring(i, i + windowSize));
+            
+            if (num >= low && num <= high) {
+                ans.push(num);
+            }
+        }
+    }
+    
+    
+    return ans;
+};
