f75083f06ac2853c9139acae27224d4021c6bd74
Quang Trung <gnurt10@gmail.com>
2023-08-14 06:51:56 +0700

    Create 1300-sum-of-mutated-array-closest-to-target.ts

---
+++ code/medium-2/1300-sum-of-mutated-array-closest-to-target.ts
@@ -0,0 +1,14 @@
+function findBestValue(arr: number[], target: number): number {
+    arr.sort((a, b) => a - b);
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        const lengthOfNumsToRight = arr.length - i;
+        const currentNum = arr[i];
+        if (sum + (currentNum * lengthOfNumsToRight) > target) {
+            return Math.ceil((target - sum) / lengthOfNumsToRight - 0.5);
+        }
+        sum += currentNum;
+    }
+
+    return arr[arr.length - 1];
+};
