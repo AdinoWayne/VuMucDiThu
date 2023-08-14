907a18744fa5c5e7e342588a83914f7cc7be4460
Quang Trung <gnurt10@gmail.com>
2023-08-15 06:54:28 +0700

    Create 1305-all-elements-in-two-binary-search-trees.ts

---
+++ code/medium-2/1305-all-elements-in-two-binary-search-trees.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function getAllElements(root1: TreeNode | null, root2: TreeNode | null): number[] {
+    var list = []
+    if (root1 != null) {
+        list.push(root1.val)
+        getVal(root1, list)
+    }
+    if (root2 != null) {
+        list.push(root2.val)
+        getVal(root2, list)
+    }
+    return list.sort((a, b) => a-b) // return the sorted list
+}
+var getVal = function(node, list) { // get the values recursively
+    if (node.left!=null) {
+        list.push(node.left.val) // add value to list
+        getVal(node.left, list)
+    }
+    if (node.right!=null) {
+        list.push(node.right.val) // add value to list
+        getVal(node.right, list)
+    }
+}
