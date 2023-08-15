e8daf24b5e98f75192a3378bc3266ac22ddb01e0
Quang Trung <gnurt10@gmail.com>
2023-08-16 06:43:12 +0700

    Create 1310-xor-queries-of-a-subarray.ts

---
+++ code/medium-2/1310-xor-queries-of-a-subarray.ts
@@ -0,0 +1,17 @@
+function xorQueries(arr: number[], queries: number[][]): number[] {
+    const xor_arr = [], res_arr = [];
+    xor_arr[0] = arr[0];
+    //create the prefix xor of arr
+    // if arr= [1,3,4,8] 
+    // so xor_arr = [1, 1^3, 1^3^4, 1^3^4^8]
+    for ( var i=1 ; i<arr.length; i++ ) {
+         xor_arr[i] = xor_arr[i-1]^arr[i]
+    }
+
+    for (const [start, end] of queries) {
+        res_arr.push(xor_arr[start]^xor_arr[end]^arr[start])
+    	                           
+    }
+
+    return res_arr;
+};
