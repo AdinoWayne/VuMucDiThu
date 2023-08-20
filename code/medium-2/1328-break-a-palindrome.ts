9641ffd50009b0e37595187261ee69cf25037076
Quang Trung <gnurt10@gmail.com>
2023-08-20 11:32:54 +0700

    Create 1328-break-a-palindrome.ts

---
+++ code/medium-2/1328-break-a-palindrome.ts
@@ -0,0 +1,17 @@
+function breakPalindrome(palindrome: string): string {
+  let result = palindrome.split("");
+
+  for (let i = 0; i < Math.floor(result.length / 2); i++) {
+    if (result[i] !== "a") {
+      result[i] = "a";
+      return result.join("");
+    }
+  }
+
+  if (result.length === 1) {
+    return "";
+  } else {
+    result[result.length - 1] = "b";
+    return result.join("");
+  }
+};
