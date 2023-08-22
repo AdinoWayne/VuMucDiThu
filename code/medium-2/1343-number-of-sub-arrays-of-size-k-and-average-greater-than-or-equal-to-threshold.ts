ee09a0af2f3d6f30e1cd1a38f6bfec92f3b5f056
Quang Trung <gnurt10@gmail.com>
2023-08-23 00:04:12 +0700

    Create 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.ts

---
+++ code/medium-2/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.ts
@@ -0,0 +1,13 @@
+function numOfSubarrays(arr: number[], k: number, threshold: number): number {
+    let count = 0, windowSize = 0, windowSum = 0;
+    for(let i = 0; i < arr.length + 1; i++){
+        if(windowSize === k){
+            if(windowSum / k >= threshold) count++;
+            windowSum -= arr[i - k];
+            windowSize--;
+        }
+        windowSum += arr[i];
+        windowSize++;
+    }
+    return count;
+};
