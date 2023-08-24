b0ecfb0bef434b22342b03b27e4d515cbed2a698
Quang Trung <gnurt10@gmail.com>
2023-08-24 07:53:51 +0700

    Update 1276-number-of-burgers-with-no-waste-of-ingredients.ts

--- code/medium-2/1276-number-of-burgers-with-no-waste-of-ingredients.ts
+++ code/medium-2/1276-number-of-burgers-with-no-waste-of-ingredients.ts
@@ -9,5 +9,14 @@ function numOfBurgers(tomatoSlices: number, cheeseSlices: number): number[] {
 
     return smallBurger >= 0 ? [jumboBurger, smallBurger] : [];
 };
+// 4 * Jumbo Burger + 2 * Small Burger = tomatoSlices
+// Jumbo Burger + Small Burger = cheeseSlices
+
+// -> 4x + 2y = tomatoSlices
+// x + y = cheeseSlices
+
+// -> x = (tomatoSlices - 2 * cheeseSlices) / 2
+// y = cheeseSlices - x
+
 // TC O(1)
 // SC O(1)
