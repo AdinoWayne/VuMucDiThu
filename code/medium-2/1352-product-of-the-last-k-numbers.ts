ca63dfad75fa879a20b2814e8e2334448f5491bf
Quang Trung <gnurt10@gmail.com>
2023-08-25 00:12:18 +0700

    Create 1352-product-of-the-last-k-numbers.ts

---
+++ code/medium-2/1352-product-of-the-last-k-numbers.ts
@@ -0,0 +1,30 @@
+class ProductOfNumbers {
+    queue: any;
+    constructor() {
+        this.queue = [];
+    }
+
+    add(num: number): void {
+        this.queue.push(num);
+    }
+
+    getProduct(k: number): number {
+        let sum = 1;
+        
+        let i = this.queue.length - 1;
+        
+        while(k > 0) {
+            sum *= this.queue[i];
+            i--;
+            k--;
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * var obj = new ProductOfNumbers()
+ * obj.add(num)
+ * var param_2 = obj.getProduct(k)
+ */
