3bd12a0ddf975cf5e3ae52c6f9d1252939bd42d9
Quang Trung <gnurt10@gmail.com>
2023-08-26 16:44:14 +0700

    Create 1358-number-of-substrings-containing-all-three-characters.ts

---
+++ code/medium-2/1358-number-of-substrings-containing-all-three-characters.ts
@@ -0,0 +1,20 @@
+function numberOfSubstrings(s: string): number {
+    let a = 0, b = 0, c = 0, result = 0;
+
+    for (let i = 0, j = 0; i < s.length; i++) {
+
+        if (s[i] == 'a') a++;
+        if (s[i] == 'b') b++;
+        if (s[i] == 'c') c++;
+
+        while (a && b && c) {
+            result += s.length - i;
+
+            if (s[j] === 'a') a--;
+            if (s[j] === 'b') b--;
+            if (s[j] === 'c') c--;
+            j++;
+        }
+    }
+    return result;
+};
