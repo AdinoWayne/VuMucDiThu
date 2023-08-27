13dae5d299f99ccce17afc0ee6d73a546bbf75ef
Quang Trung <gnurt10@gmail.com>
2023-08-27 15:39:29 +0700

    Create 1362-closest-divisors.ts

---
+++ code/medium-2/1362-closest-divisors.ts
@@ -0,0 +1,18 @@
+function closestDivisors(num: number): number[] {
+     const n2 = num + 2;
+      const n1 = num + 1;
+
+      let div = Math.floor(Math.sqrt(n2));
+
+      while (div > 0) {
+          if (n1 % div == 0) {
+              return [div, n1 / div];
+          }
+
+          if (n2 % div == 0) {
+            return [div, n2 / div];
+          }
+
+          div--;
+      }
+};
