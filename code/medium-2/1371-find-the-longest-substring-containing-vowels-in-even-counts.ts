265dbc4469da77bdb293f877446477ad7913db32
Quang Trung <gnurt10@gmail.com>
2023-08-29 00:56:22 +0700

    Create 1371-find-the-longest-substring-containing-vowels-in-even-counts.ts

---
+++ code/medium-2/1371-find-the-longest-substring-containing-vowels-in-even-counts.ts
@@ -0,0 +1,11 @@
+function findTheLongestSubstring(s: string): number {
+    let n = s.length, m = new Map([[0, -1]]), res = 0, cur = 0, mask = [1, 2, 4, 8, 16];
+    for (let i = 0; i < n; i++) {
+        let idx = 'aeiou'.indexOf(s[i]);
+        if (idx != -1) cur ^= mask[idx];
+        if (!m.has(cur)) m.set(cur, i); // set min index for cur
+        let len = i - m.get(cur);
+        res = Math.max(res, len);
+    }
+    return res;
+};
