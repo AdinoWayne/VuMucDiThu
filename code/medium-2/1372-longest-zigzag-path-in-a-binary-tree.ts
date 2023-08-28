1855070e310f9958017e4697f026250c88a028c5
Quang Trung <gnurt10@gmail.com>
2023-08-29 00:54:35 +0700

    Create 1372-longest-zigzag-path-in-a-binary-tree.ts

---
+++ code/medium-2/1372-longest-zigzag-path-in-a-binary-tree.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+const LEFT = 0
+const RIGHT = 1
+let ans = 0
+
+function longestZigZag(root: TreeNode | null): number {
+    ans = 0
+    const left = 1 + recLongestZigZag(root.left, LEFT)
+    const right = 1 + recLongestZigZag(root.right, RIGHT)
+    ans = Math.max(
+        left,
+        right,
+        ans,
+    )
+    return ans
+}
+
+const recLongestZigZag = (root, dir) => {
+    if (root === null) return -1
+    const left = 1 + recLongestZigZag(root.left, LEFT)
+    const right = 1 + recLongestZigZag(root.right, RIGHT)
+    ans = Math.max(ans, left, right)
+    return dir === LEFT ? right : left
+}
