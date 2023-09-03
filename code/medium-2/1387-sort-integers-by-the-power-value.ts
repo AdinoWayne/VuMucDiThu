b518fe9cf7702ae6979fde5850170927d5f15cd4
Quang Trung <gnurt10@gmail.com>
2023-09-03 21:13:30 +0700

    Create 1387-sort-integers-by-the-power-value.ts

---
+++ code/medium-2/1387-sort-integers-by-the-power-value.ts
@@ -0,0 +1,17 @@
+function getKth(lo: number, hi: number, k: number): number {
+    const findPower = num => {
+        let power = 0
+        while (num > 1) {
+            power++
+            num = num % 2 ? 3 * num + 1 : num / 2
+        }
+        return power
+    }
+    const result = [], map = {}
+    for (let i = lo; i <= hi; i++) {
+        result.push(i)
+        map[i] = findPower(i)
+    }
+    result.sort((a, b) => map[a] - map[b])
+    return result[k - 1]
+};
