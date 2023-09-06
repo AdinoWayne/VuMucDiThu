3b4872a180031f6906d56d04fef9afb252a0ad2a
Quang Trung <gnurt10@gmail.com>
2023-09-07 00:51:15 +0700

    Create 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.ts

---
+++ code/medium-2/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.ts
@@ -0,0 +1,10 @@
+function numSteps(s: string): number {
+    let i = 0, n = BigInt("0b"+s);
+    
+    while(n!=1n){
+        n = n%2n==1n ? n+1n : n/2n;
+        i++;
+    }
+    
+    return i;
+};
