56525b2a8097ab5c28468843a33239afa371f74b
Quang Trung <gnurt10@gmail.com>
2023-09-08 06:56:57 +0700

    Create 1410-html-entity-parser.ts

---
+++ code/medium-2/1410-html-entity-parser.ts
@@ -0,0 +1,55 @@
+function entityParser(text: string): string {
+    const n = text.length;
+    const ent = {
+        "&quot;": '"',
+        "&apos;": "'",
+        "&amp;": "&",
+        "&gt;": ">",
+        "&lt;": "<",
+        "&frasl;": "/"
+    };
+    
+    let res = "";
+    let start = 0;
+    
+    while (start < n) {
+        const startChar = text.charAt(start);
+        
+        if (startChar != "&") {
+            res += startChar;
+            start++;
+            continue;
+        }
+        
+        let str = "&";
+        let end = start + 1;
+        
+        while (end < n) {
+            const endChar = text.charAt(end);
+            
+            if (endChar === ";") {
+                str += endChar;
+                
+                res += ent[str] ?? str;
+                
+                str = "";
+                end++;
+                break;
+            }
+            else if (endChar === "&") {
+                res += str;
+                str = "";
+                break;
+            }
+            else {
+                str += endChar;
+                end++;
+            }
+        }
+        
+        res += str;
+        start = end;
+    }
+    
+    return res;
+};
