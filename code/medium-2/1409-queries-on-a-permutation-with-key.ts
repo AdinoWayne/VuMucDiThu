912f11db1dc26414a7619bafe6c6214423744c95
Quang Trung <gnurt10@gmail.com>
2023-09-08 06:55:43 +0700

    Create 1409-queries-on-a-permutation-with-key.ts

---
+++ code/medium-2/1409-queries-on-a-permutation-with-key.ts
@@ -0,0 +1,11 @@
+function processQueries(queries: number[], m: number): number[] {
+    let p = [],final=[]
+    for(let i=1;i<=m;i++)p.push(i)
+    for(let i=0;i<queries.length;i++){
+        let index = p.indexOf(queries[i])
+        p.unshift(p[index])
+        p.splice(index+1,1)
+        final.push(index)
+    }
+    return final
+};
