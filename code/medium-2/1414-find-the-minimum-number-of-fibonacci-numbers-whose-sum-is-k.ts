6116a0346e1caa6c5ecddc4893314a296c191766
Quang Trung <gnurt10@gmail.com>
2023-09-09 12:18:18 +0700

    Create 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.ts

---
+++ code/medium-2/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.ts
@@ -0,0 +1,29 @@
+function findMinFibonacciNumbers(k: number): number {
+    var fib = [];
+	//constructing Fibonacci table
+    calcFib(fib,k);
+    var count = 0, j = fib.length - 1;
+    while(k > 0) 
+    { 
+        // divide k by j'th Fibonacci term to find number of terms it contributes in sum. 
+        count += Math.floor(k / fib[j]); 
+        k %= fib[j]; 
+        j--; 
+    } 
+    return count; 
+};
+var calcFib = function(fib, k) {
+    var i = 3, nextTerm = 0;
+    fib.push(0); 
+	fib.push(1); 
+	fib.push(1); 
+    while(true) 
+	{ 
+        nextTerm = fib[i - 1] + fib[i - 2]; 
+        if(nextTerm>k){
+			return; 
+		}
+        fib.push(nextTerm); 
+        i++; 
+	} 
+};
