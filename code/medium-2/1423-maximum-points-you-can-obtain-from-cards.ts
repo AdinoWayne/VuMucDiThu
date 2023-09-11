a622251e203321c3769f5df7f3efe29bf91477de
Quang Trung <gnurt10@gmail.com>
2023-09-12 00:41:22 +0700

    Create 1423-maximum-points-you-can-obtain-from-cards.ts

---
+++ code/medium-2/1423-maximum-points-you-can-obtain-from-cards.ts
@@ -0,0 +1,26 @@
+function maxScore(cardPoints: number[], k: number): number {
+    // Edge cases
+    if (k === cardPoints.length) {
+        return cardPoints.reduce((acc, curr) => acc+curr, 0)
+    }
+    if (k === 0) return 0
+    
+    let right = 0
+    let left = k - 1
+    const length = cardPoints.length - 1
+    let currentSum = cardPoints.slice(0, k).reduce((acc, curr) => acc + curr, 0)
+    let maxSum = currentSum
+
+    while (left >= 0) {
+        currentSum -= cardPoints[left]
+        left -= 1
+        currentSum += cardPoints[length - right]
+        right += 1
+        
+        if (currentSum > maxSum) {
+            maxSum = currentSum
+        }
+    }
+    
+    return maxSum
+};
