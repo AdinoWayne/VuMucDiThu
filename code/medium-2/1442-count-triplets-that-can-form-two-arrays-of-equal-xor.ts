2c0da7adc2c7c4163eaddc37759692dafef3cd1e
Quang Trung <gnurt10@gmail.com>
2023-09-13 00:31:13 +0700

    Create 1442-count-triplets-that-can-form-two-arrays-of-equal-xor.ts

---
+++ code/medium-2/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.ts
@@ -0,0 +1,13 @@
+function countTriplets(arr: number[]): number {
+    let count = 0
+    for(let i=0;i<arr.length;i++){
+        let xor = arr[i]
+        for(let j=i+1;j<arr.length;j++){
+            xor ^= arr[j]
+            if(xor == 0){
+                count += (j-i)
+            }
+        }
+    }
+    return count
+};
