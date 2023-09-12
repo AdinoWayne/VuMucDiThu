047ac8436b6cf868b86c0b297162cc210dc77fdc
Quang Trung <gnurt10@gmail.com>
2023-09-13 00:31:53 +0700

    Create 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.ts

---
+++ code/medium-2/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.ts
@@ -0,0 +1,24 @@
+function longestSubarray(nums: number[], limit: number): number {
+  const maxQue = [];
+  const minQue = [];
+  let maxTop = -1, minTop = -1;
+  let maxBottom = 0, minBottom = 0;
+  let max = 0;
+  for (let left = -1, right = 0; right < nums.length; ++right) {
+    const val = nums[right];
+    while (maxTop >= maxBottom && val > maxQue[maxTop]) --maxTop;
+    while (minTop >= minBottom && val < minQue[minTop]) --minTop;
+    maxQue[++maxTop] = val;
+    minQue[++minTop] = val;
+    if (maxQue[maxBottom] - minQue[minBottom] <= limit) {
+      right - left > max && (max = right - left);
+    } else {
+      while (maxQue[maxBottom] - minQue[minBottom] > limit) {
+        ++left;
+        maxQue[maxBottom] === nums[left] && ++maxBottom;
+        minQue[minBottom] === nums[left] && ++minBottom;
+      }
+    }
+  }
+  return max;
+};
