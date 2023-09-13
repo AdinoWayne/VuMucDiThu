816b760c12a6967e716ae17604e3e2a3b2d07997
Quang Trung <gnurt10@gmail.com>
2023-09-14 00:29:40 +0700

    Create 1443-minimum-time-to-collect-all-apples-in-a-tree.js

---
+++ code/medium-2/1443-minimum-time-to-collect-all-apples-in-a-tree.js
@@ -0,0 +1,22 @@
+function minTime(n: number, edges: number[][], hasApple: boolean[]): number {
+    const parentMap = new Map();
+    let answer = 0;
+    for (let i = 0; i < edges.length; i++) {
+        parentMap.set(edges[i][1], edges[i][0]);
+    }
+    for (let i = 0; i < hasApple.length; i++) {
+        if (hasApple[i]) {
+            let pointer = i;
+            while (pointer !== 0) {
+                const parent = parentMap.get(pointer);
+                answer += 2;
+                if (hasApple[parent]) {
+                    break;
+                }
+                hasApple[parent] = true;
+                pointer = parent;
+            }
+        }
+    }
+    return answer;
+};
