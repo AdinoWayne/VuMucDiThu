679b637a4d7d4b60c73db20a741cee9afd97303c
Quang Trung <gnurt10@gmail.com>
2023-09-15 06:29:29 +0700

    Create 1461-rearrange-words-in-a-sentence.ts

---
+++ code/medium-2/1461-rearrange-words-in-a-sentence.ts
@@ -0,0 +1,20 @@
+function arrangeWords(text: string): string {
+    const arr = text.toLowerCase().split(' ');
+    const map = {};
+    for(const word of arr) {
+        let wordLen = word.length;
+        if(wordLen in map) {
+            map[wordLen].push(word);
+        }
+        else
+            map[wordLen] = [word];
+    }
+    const keys = Object.keys(map);
+    keys.sort((a: any,b: any) => a - b)
+    let ans = [];
+    for(let key of keys) {
+        ans.push(...map[key]);
+    }
+    ans[0] = ans[0].charAt(0).toUpperCase() + ans[0].substr(1);
+    return ans.join(' ');
+};
