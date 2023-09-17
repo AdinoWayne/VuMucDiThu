00cf090fc53198332d5ff66220ca45bf7e1913fe
Quang Trung <gnurt10@gmail.com>
2023-09-17 14:26:12 +0700

    Create 1461-check-if-a-string-contains-all-binary-codes-of-size-k.ts

---
+++ code/medium-2/1461-check-if-a-string-contains-all-binary-codes-of-size-k.ts
@@ -0,0 +1,11 @@
+function hasAllCodes(s: string, k: number): boolean {
+    let count = 0;
+    const target = Math.pow(2,k);
+    const set = new Set();
+    
+    for (let i = 0; i < s.length-k+1; i++) {
+        set.add(s.substring(i, i+k));
+    }
+    
+    return set.size == target;
+};
