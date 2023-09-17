caef00ef628f8b86661507eb290a25eb970b999e
Quang Trung <gnurt10@gmail.com>
2023-09-18 00:31:45 +0700

    Create 14620-course-schedule-iv.ts

---
+++ code/medium-2/14620-course-schedule-iv.ts
@@ -0,0 +1,22 @@
+function checkIfPrerequisite(n: number, prerequisites: number[][], queries: number[][]): boolean[] {
+  let g = Array.from({ length: n }, () => new Array(n).fill(0))
+  for (let [u, v] of prerequisites) {
+    g[u][v] = 1
+  }
+
+  for (let mid = 0; mid < n; mid++) {
+    for (let u = 0; u < n; u++) {
+      for (let v = 0; v < n; v++) {
+        if (g[u][mid] && g[mid][v]) {
+          g[u][v] = 1
+        }
+      }
+    }
+  }
+
+  let an = []
+  for (let [u, v] of queries) {
+    an.push(g[u][v])
+  }
+  return an
+};
