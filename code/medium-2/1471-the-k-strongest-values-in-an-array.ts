ffed10e48998519000e390a91379d43b7192ee4b
Quang Trung <gnurt10@gmail.com>
2023-09-19 06:08:31 +0700

    Create 1471-the-k-strongest-values-in-an-array.ts

---
+++ code/medium-2/1471-the-k-strongest-values-in-an-array.ts
@@ -0,0 +1,33 @@
+function getStrongest(arr: number[], k: number): number[] {
+    const n = arr.length;
+    
+    arr.sort((a, b) => a - b);
+    
+    let left = 0;
+    let right = n - 1;
+    
+    const idx = Math.floor((n - 1) / 2);
+    const median = arr[idx];
+    
+    const res = [];
+    
+    while (k > 0) {
+        const leftNum = arr[left];
+        const rightNum = arr[right];
+        
+        const leftStr = Math.abs(leftNum - median);
+        const rightStr = Math.abs(rightNum - median);
+        
+        if ((leftStr > rightStr) || (leftStr === rightStr && leftNum > rightNum)) {
+            res.push(leftNum);
+            left++;
+        }
+        else {
+            res.push(rightNum);
+            right--;
+        }
+        k--;
+    }
+    
+    return res;
+};
