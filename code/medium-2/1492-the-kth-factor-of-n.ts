15587d50712d823c1789069aa13b26febfa58d96
Quang Trung <gnurt10@gmail.com>
2023-09-22 00:03:09 +0700

    Create 1492-the-kth-factor-of-n.ts

---
+++ code/medium-2/1492-the-kth-factor-of-n.ts
@@ -0,0 +1,9 @@
+function kthFactor(n: number, k: number): number {
+    for(let i = 1; i <= n; i++) {
+        if((n % i) === 0) {
+            k--
+        }
+        if(k === 0) return i
+    }
+    return -1
+};
