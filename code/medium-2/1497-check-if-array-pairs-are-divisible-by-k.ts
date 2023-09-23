2ec501e14f35821c3291963905d48716e792f462
Quang Trung <gnurt10@gmail.com>
2023-09-23 10:26:21 +0700

    Create 1497-check-if-array-pairs-are-divisible-by-k.ts

---
+++ code/medium-2/1497-check-if-array-pairs-are-divisible-by-k.ts
@@ -0,0 +1,37 @@
+function canArrange(arr: number[], k: number): boolean {
+    const n = arr.length;
+    const map = new Map();
+    
+    for (let i = 0; i < n; i++) {
+        const num = arr[i];
+        let rem = num % k;
+        
+        if (rem < 0) rem += k;
+        
+        if (!map.has(rem)) map.set(rem, 0);
+        map.set(rem, map.get(rem) + 1);
+    }
+    
+    for (const [rem, count] of map) {
+        if (rem === 0) {
+            if (count % 2 != 0) {
+                return false;
+            }
+            map.delete(0);
+        }
+        else {
+            const counterPart = k - rem;
+            
+            if (!map.has(counterPart)) return false;
+            
+            const otherCount = map.get(counterPart);
+            
+            if (count != otherCount) return false;
+            
+            map.delete(rem);
+            map.delete(counterPart);
+        }
+    }
+   
+    return map.size === 0;
+};
