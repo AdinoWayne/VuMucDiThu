b6b02a2a2e00e8793ea4f544e4b730f1d34fffb9
Quang Trung <gnurt10@gmail.com>
2023-09-26 00:31:02 +0700

    Create 1513-number-of-substrings-with-only-1s.ts

---
+++ code/medium-2/1513-number-of-substrings-with-only-1s.ts
@@ -0,0 +1,15 @@
+function numSub(s: string): number {
+    let result = 0;
+    let len = 0;    
+    
+    for (let i = 0; i < s.length; ++i) {
+        if (s[i] === "1") {
+            len++;
+            result += len;
+        } else {
+            len = 0;
+        }
+    }
+    
+    return result % 1000000007;
+};
