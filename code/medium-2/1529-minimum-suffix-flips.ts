2919a424d2885dddea30020ecb50bdce3483a963
Quang Trung <gnurt10@gmail.com>
2023-09-28 03:31:36 +0700

    Create 1529-minimum-suffix-flips.ts

---
+++ code/medium-2/1529-minimum-suffix-flips.ts
@@ -0,0 +1,11 @@
+function minFlips(target: string): number {
+    let current = 0;
+    let result = 0;
+
+    for (const value of target) {
+        if (current === +value) continue;
+        current ^= 1;
+        result += 1;
+    }
+    return result;
+};
