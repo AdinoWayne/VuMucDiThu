5edc9a0ec668c710273fba668ccfeacf83c86f6c
Quang Trung <gnurt10@gmail.com>
2023-09-29 00:09:10 +0700

    Create 1535-find-the-winner-of-an-array-game.ts

---
+++ code/medium-2/1535-find-the-winner-of-an-array-game.ts
@@ -0,0 +1,14 @@
+function getWinner(arr: number[], k: number): number {
+    let i = 1, winner = arr[0], winCount = 0;
+    while (i < arr.length && winCount < k) {
+        if (winner < arr[i]) {
+            winner = arr[i];
+            winCount = 1;
+        } else {
+            winCount++;
+        }
+        i++;
+    }
+    
+    return winner;
+};
