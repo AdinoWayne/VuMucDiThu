a7ffb218a45f0c35ca4ba44fcccaf80daa84fa06
Quang Trung <gnurt10@gmail.com>
2023-10-02 01:06:38 +0700

    Create 1541-minimum-insertions-to-balance-a-parentheses-string.ts

---
+++ code/medium-2/1541-minimum-insertions-to-balance-a-parentheses-string.ts
@@ -0,0 +1,21 @@
+function minInsertions(s: string): number {
+    let rightNeeded = 0;
+    let leftNeeded = 0;
+    for (const char of s) {
+        if (char === "(") {
+            if (rightNeeded % 2 === 0) {
+                rightNeeded += 2;  
+            } else {                
+                rightNeeded++;
+                leftNeeded++;
+            }
+        } else {
+            rightNeeded--;
+            if (rightNeeded === -1 ){
+                leftNeeded++;
+                rightNeeded = 1;
+            }
+        }
+    }
+    return leftNeeded + rightNeeded;
+};
