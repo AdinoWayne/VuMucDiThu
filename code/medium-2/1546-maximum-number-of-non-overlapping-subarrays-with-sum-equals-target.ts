407cec89b1bd514225202db5c829c2d1238b0d3c
Quang Trung <gnurt10@gmail.com>
2023-10-03 06:54:09 +0700

    Create 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.ts

---
+++ code/medium-2/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.ts
@@ -0,0 +1,15 @@
+function maxNonOverlapping(nums: number[], target: number): number {
+    const seen = new Set();
+    let total = 0, result = 0;
+    
+    for(let n of nums) {
+        total += n;
+        
+        if(total === target || seen.has(total - target)) {
+            total = 0;
+            result++;
+            seen.clear()
+        } else seen.add(total)
+    }
+    return result;
+};
