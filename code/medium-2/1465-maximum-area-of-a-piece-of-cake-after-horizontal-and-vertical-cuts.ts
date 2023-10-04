811e9ce334d61ed4b10c0fee84e97e97c9309f92
Quang Trung <gnurt10@gmail.com>
2023-10-05 00:16:16 +0700

    Update 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.ts

--- code/medium-2/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.ts
+++ code/medium-2/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.ts
@@ -1,20 +1,15 @@
 function maxArea(h: number, w: number, horizontalCuts: number[], verticalCuts: number[]): any {
     let mod = 1e9 + 7;
-    
     horizontalCuts.sort(function(a, b) {
         return a - b;
     });
-    
     verticalCuts.sort(function(a, b) {
         return a - b;
     });
-    
     let maxHeight = verticalCuts[0];
     let maxWidth = horizontalCuts[0]
-    
     verticalCuts.push(w);
     horizontalCuts.push(h);
-    
     for (var i = 0; i < verticalCuts.length - 1; i++) {
         if (verticalCuts[i+1] - verticalCuts[i] > maxHeight) {
             maxHeight = verticalCuts[i+1] - verticalCuts[i];
@@ -25,6 +20,7 @@ function maxArea(h: number, w: number, horizontalCuts: number[], verticalCuts: n
             maxWidth = horizontalCuts[i+1] - horizontalCuts[i];
         }
     }
-    
     return BigInt(maxHeight) * BigInt(maxWidth) % BigInt(mod)
 };
+// TC O(nLogn)
+// SC O(n)
