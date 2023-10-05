ebe6e5a5df7d858c3a0b8d69f0b323b907c7a74f
Quang Trung <gnurt10@gmail.com>
2023-10-06 00:07:24 +0700

    Create 1561-maximum-number-of-coins-you-can-get.ts

---
+++ code/medium-2/1561-maximum-number-of-coins-you-can-get.ts
@@ -0,0 +1,7 @@
+function maxCoins(piles: number[]): number {
+    piles.sort((a,b)=>a-b);
+    let ans = 0;
+    for (let i =0, j= piles.length -2; i < j; i++, j-=2)
+        ans += piles[j];
+    return ans;
+};
