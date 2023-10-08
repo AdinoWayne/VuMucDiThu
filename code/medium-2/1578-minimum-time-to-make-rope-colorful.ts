28e7c4f004c21a6e0ccd93e23a82aec41efba689
Quang Trung <gnurt10@gmail.com>
2023-10-09 00:36:00 +0700

    Create 1578-minimum-time-to-make-rope-colorful.ts

---
+++ code/medium-2/1578-minimum-time-to-make-rope-colorful.ts
@@ -0,0 +1,12 @@
+function minCost(colors: string, neededTime: number[]): number {
+  let res = 0;
+  for (let i = 0, max = 0; i < colors.length; i++) {
+    res += neededTime[i];
+    max = Math.max(max, neededTime[i]);
+    if (colors[i] !== colors[i + 1]) {
+      res -= max;
+      max = 0;
+    }
+  }
+  return res;
+};
