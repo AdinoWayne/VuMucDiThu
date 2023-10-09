744d80793e5013c1e6e7457769006b42ed9356b9
Quang Trung <gnurt10@gmail.com>
2023-10-10 00:49:30 +0700

    Create 1589-maximum-sum-obtained-of-any-permutation.ts

---
+++ code/medium-2/1589-maximum-sum-obtained-of-any-permutation.ts
@@ -0,0 +1,20 @@
+function maxSumRangeQuery(nums: number[], requests: number[][]): number {
+    const MODULO = 10 ** 9 + 7;
+    const size = nums.length;
+    const frequency = Array(size).fill(0);
+
+    for (const [start, end] of requests) {
+        frequency[start] += 1;
+        if (end + 1 === size) continue;
+        frequency[end + 1] -= 1;
+    }
+    for (let index = 1; index < size; index++) {
+        frequency[index] += frequency[index - 1];
+    }
+    frequency.sort((a, b) => a - b);
+    nums.sort((a, b) => a - b);
+
+    return nums.reduce((result, num, index) => {
+        return (result + num * frequency[index]) % MODULO;
+    }, 0);
+};
