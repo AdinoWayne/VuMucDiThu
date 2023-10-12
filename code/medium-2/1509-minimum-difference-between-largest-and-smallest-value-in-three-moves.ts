0d93b2c2478c235d66ddece4453c80bf306b8cd2
Quang Trung <gnurt10@gmail.com>
2023-10-13 00:45:18 +0700

    Update 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.ts

--- code/medium-2/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.ts
+++ code/medium-2/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.ts
@@ -1,17 +1,15 @@
 function minDifference(nums: number[]): number {
     if (nums.length <= 4) return 0
     nums.sort((a,b) => a-b)
-    
     let i = 0;
     let j = nums.length - 4
     let min = Infinity
-    
     while(i <= 3){
         min = Math.min(nums[j] - nums[i], min)
         i++
         j++
     }
-    
-    
     return min
 };
+// TC O(nLogn)
+// SC O(1)
