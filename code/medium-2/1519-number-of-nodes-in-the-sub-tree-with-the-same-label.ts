3504e1ff94cd3bd48453786eff9cbbe44fb9f6cd
Quang Trung <gnurt10@gmail.com>
2023-10-15 08:18:27 +0700

    Update 1519-number-of-nodes-in-the-sub-tree-with-the-same-label.ts

--- code/medium-2/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.ts
+++ code/medium-2/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.ts
@@ -1,84 +1,42 @@
 function countSubTrees(n: number, edges: number[][], labels: string): number[] {
-  // build edge map
-  let edgeMap = {};
-  for (let i = 0; i < edges.length; i++) {
-    const [a, b] = edges[i];
-    if (edgeMap[a]) {
-      edgeMap[a].push(b);
-    } else {
-      edgeMap[a] = [b];
-    }
-    if (edgeMap[b]) {
-      edgeMap[b].push(a);
-    } else {
-      edgeMap[b] = [a];
-    }
-  }
-  // build levels
-  let level = 0;
-  let levels = {};
-  let nodes = {};
-  let ans = [];
-  levels[0] = [];
-  let q = [0];
-  let newq = [];
-  while (q.length > 0) {
-    const i = q.pop();
-    const node = {
-      number: i,
-      children: [],
-    };
-    const nei = edgeMap[i];
-    for (let i = 0; i < nei.length; i++) {
-      const element = nei[i];
-      if (nodes[element]) {
-        nodes[element].children.push(node);
-      } else {
-        newq.push(element);
-      }
-    }
-    nodes[i] = node;
-    levels[level].push(node);
-    if (q.length === 0) {
-      level++;
-      levels[level] = [];
-      q = newq;
-      newq = [];
-    }
-  }
-  level--;
-  // go from bottom level to top level, accumulating label count
-  for (let i = level; i >= 0; i--) {
-    const currLevel = levels[i];
-    for (let j = 0; j < currLevel.length; j++) {
-      const element = currLevel[j];
-      if (element.children.length === 0) {
-        ans[element.number] = 1;
-        element.labelMap = {
-          [labels[element.number]]: 1,
-        };
-      } else {
-        element.labelMap = {
-          [labels[element.number]]: 1,
-        };
-        for (let k = 0; k < element.children.length; k++) {
-          const child = element.children[k];
-          if (child.labelMap) {
-            for (const key in child.labelMap) {
-              if (child.labelMap.hasOwnProperty(key)) {
-                if (element.labelMap[key]) {
-                  element.labelMap[key] =
-                    element.labelMap[key] + child.labelMap[key];
-                } else {
-                  element.labelMap[key] = child.labelMap[key];
-                }
-              }
-            }
-          }
-        }
-        ans[element.number] = element.labelMap[labels[element.number]];
-      }
-    }
-  }
-  return ans;
+   const adjList = createAdjacencyList(n, edges);
+    // Create an array to store the count of each label
+    const ans = Array(n).fill(0);
+
+    dfs(0, -1, adjList, labels, ans);
+
+    return ans;
 };
+
+const dfs = (curNode, prevNode, adjList, labels, ans) => {
+    // Create an array to keep track of the count of each label
+    const count = Array(26).fill(0);
+
+    // Visit each child of the current node
+    for(const child of adjList[curNode]){
+        if(child !== prevNode){
+            // Recursively visit the child and update the count array
+            const childCount = dfs(child, curNode, adjList, labels, ans);
+            for(let i=0; i<26; i++){
+                count[i] += childCount[i];
+            }
+        }
+    }
+
+    // Increment the count of the label for the current node
+    const labelIndex = labels.charCodeAt(curNode) - 'a'.charCodeAt(0);
+    ans[curNode] = ++count[labelIndex];
+
+    return count; 
+}
+
+const createAdjacencyList = (n, edges) => {
+    const adjList = Array(n).fill(null).map(() => []);
+
+    for(const edge of edges){
+        const [node1, node2] = edge;
+        adjList[node1].push(node2);
+        adjList[node2].push(node1);
+    }
+    return adjList;
+}
