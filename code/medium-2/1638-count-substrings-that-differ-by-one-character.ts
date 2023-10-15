20f5202c44bd60bc0e0d3b65e6652ed9a3ca93d9
Quang Trung <gnurt10@gmail.com>
2023-10-16 00:21:38 +0700

    Create 1638-count-substrings-that-differ-by-one-character.ts

---
+++ code/medium-2/1638-count-substrings-that-differ-by-one-character.ts
@@ -0,0 +1,26 @@
+function countSubstrings(s: string, t: string): number {
+    const m = s.length;
+    const n = t.length;
+    let result = 0;
+
+    const checkSubstring = (a, b) => {
+        let current  = 0;
+        let previous = 0;
+
+        while (a < m && b < n) {
+            current += 1;
+
+            if (s[a] !== t[b]) {
+               previous = current;
+               current = 0;
+            }
+            result += previous;
+            a += 1;
+            b += 1;
+        }
+    };
+
+    for (let index = 0; index < m; index++) checkSubstring(index, 0);
+    for (let index = 1; index < n; index++) checkSubstring(0, index);
+    return result;
+};
