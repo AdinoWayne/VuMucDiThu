799742f36dcafd0d82057f191cd1e09bc30a281b
Quang Trung <gnurt10@gmail.com>
2023-10-18 00:20:15 +0700

    Update 1536-minimum-swaps-to-arrange-a-binary-grid.ts

--- code/medium-2/1536-minimum-swaps-to-arrange-a-binary-grid.ts
+++ code/medium-2/1536-minimum-swaps-to-arrange-a-binary-grid.ts
@@ -1,27 +1,22 @@
 function minSwaps(grid: number[][]): number {
     const arr = [];
     let count = 0;
-    
     for(let row of grid) {
         arr.push(row.lastIndexOf(1));
     }
-    
     function swap(i, j) {
         [arr[i], arr[j]] = [arr[j], arr[i]];
         count++;
     }
-    
     for(let i = 0; i < arr.length; i++) {
         // if num <= i pass b/c it's its correct spot
         if(arr[i] <= i) continue;
         let j = i;
-        
         // scan forward looking for a num <= i
         while(arr[j] > i) {
             j++;
             if(j >= arr.length) return -1;
         }
-        
         // swap as you move back to the right spot
         for(let k = j; k > i; k--) {
             swap(k, k-1);
@@ -29,3 +24,5 @@ function minSwaps(grid: number[][]): number {
     }
     return count;
 };
+// TC O(n2)
+// SC O(n)
