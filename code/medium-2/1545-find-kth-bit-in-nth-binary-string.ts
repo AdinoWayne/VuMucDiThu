4f4c985cbfef81a703cd2ff959a1dacdecddfa82
Quang Trung <gnurt10@gmail.com>
2023-10-20 07:08:08 +0700

    Update 1545-find-kth-bit-in-nth-binary-string.ts

--- code/medium-2/1545-find-kth-bit-in-nth-binary-string.ts
+++ code/medium-2/1545-find-kth-bit-in-nth-binary-string.ts
@@ -22,5 +22,12 @@ function findKthBit(n: number, k: number): string {
 
    return (count%2 == 0) ? '0' : '1';
 };
+// The idea is to check the invert times of the specific digits
+// ex: n = 4, k = 11 (011100110110001)
+// s[11] = ~s[5] = ~(~s[3]) = ~(~(~s[1])) = '1'
+// From above case, we can notice that
+// 8 < 11 < 16, the mirror number is 16 - 11 = 5;
+// 4 < 5 < 8, the mirror number is 8 - 5 = 3;
+// 2 < 3 < 4, the mirror number is 4 - 3 = 1;
 // TC O(n)
 // SC O(1)
