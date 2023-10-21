809bc1ff331dbe9253bff04d8c615ef8ed97244e
Quang Trung <gnurt10@gmail.com>
2023-10-21 22:50:44 +0700

    Create 1669-merge-in-between-linked-lists.ts

---
+++ code/medium-2/1669-merge-in-between-linked-lists.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeInBetween(list1: ListNode | null, a: number, b: number, list2: ListNode | null): ListNode | null {
+    let start = list1;
+    let end = list1;
+    
+    for (let i = 0; i <= b && start != null && end != null; i++) {
+        if (i < a - 1) start = start.next;
+        if (i <= b) end = end.next;
+    }
+    
+    let tail = list2;
+    
+    while (tail.next != null) {
+        tail = tail.next;
+    }
+    
+    start.next = list2;
+    tail.next = end;
+    
+    return list1;
+};
