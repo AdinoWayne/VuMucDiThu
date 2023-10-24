347e2086d8168fa819579490df4153d1ce2b6e92
Quang Trung <gnurt10@gmail.com>
2023-10-25 00:08:47 +0700

    Create 1686-stone-game-vi.ts

---
+++ code/medium-2/1686-stone-game-vi.ts
@@ -0,0 +1,21 @@
+function stoneGameVI(aliceValues: number[], bobValues: number[]): number {
+  const scores = [0, 0];
+  let turn = 0;
+  const N = bobValues.length;
+
+  const tactics = aliceValues.reduce((acc, v, i) => {
+    acc.push({
+      0: v,
+      1: bobValues[i],
+      s: v + bobValues[i],
+    })
+    return acc;
+  }, []).sort((a, b) => b.s - a.s);
+  
+  for (let i = 0; i < N; i++) {
+    scores[turn] += tactics[i][turn];
+    turn ^= 1;
+  }
+  
+  return scores[0] > scores[1] ? 1 : (scores[0] < scores[1] ? -1 : 0);
+};
