ccb35e2eec12f44b2f46d698b8cbb59fad850602
Quang Trung <gnurt10@gmail.com>
2023-10-26 00:15:20 +0700

    Create 1695-maximum-erasure-value.ts

---
+++ code/medium-2/1695-maximum-erasure-value.ts
@@ -0,0 +1,9 @@
+function maximumUniqueSubarray(N: number[]): number {
+    let a = new Int8Array(10001), sum = 0, max = 0, len = N.length
+    for (let l = 0, r = 0; r < len; r++) {
+        a[N[r]]++, sum += N[r]
+        while (a[N[r]] > 1) a[N[l]]--, sum -= N[l++]
+        max = sum > max ? sum : max
+    }
+    return max
+};
