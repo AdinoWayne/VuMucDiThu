69cad6f5b88ad302d64f38012643fcc3bb547e6a
Quang Trung <gnurt10@gmail.com>
2023-10-28 23:30:02 +0700

    Create 1712-ways-to-split-array-into-three-subarrays.ts

---
+++ code/medium-2/1712-ways-to-split-array-into-three-subarrays.ts
@@ -0,0 +1,17 @@
+function waysToSplit(N: number[]): number {
+    let len = N.length
+    for (let i = 1; i < len; i++)
+        N[i] += N[i - 1]
+    let max = N[len - 1], ans = 0
+    if (max === 0)
+        return ((len - 2) * (len - 1) / 2) % 1000000007
+    for (let i = 0, j = 1, k = 1; N[i] <= max / 3; i++) {
+        let lo = N[i] * 2, hi = (max + N[i]) / 2
+        while (j === i || N[j] < lo)
+            j++
+        while (N[k] <= hi)
+            k++
+        ans += k - j
+    }
+    return ans % 1000000007
+};
