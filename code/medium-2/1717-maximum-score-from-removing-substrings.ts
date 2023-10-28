050118d9e99b0e71e701138eb7febec691e5c7ba
Quang Trung <gnurt10@gmail.com>
2023-10-28 23:29:35 +0700

    Create 1717-maximum-score-from-removing-substrings.ts

---
+++ code/medium-2/1717-maximum-score-from-removing-substrings.ts
@@ -0,0 +1,12 @@
+function maximumGain(s: string, X: number, Y: number): number {
+    const S = s.split('')
+    let len = S.length, ans = 0, a = "a", b = "b", i, j
+    if (Y > X) [a,b,X,Y] = [b,a,Y,X]
+    for (let t = 0; t < 2; t++) {
+        for (i = j = 2; j <= len; S[i++] = S[j++])
+            if (i > 1 && S[i-2] === a && S[i-1] === b)
+                ans += X, i -= 2
+        if (!t) len = i - 1, S.splice(len), [a,b,X,Y] = [b,a,Y,X]
+    }
+    return ans
+};
