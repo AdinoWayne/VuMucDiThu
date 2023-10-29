10e8445fd6b043151f4311a92d272610122d7ea2
Quang Trung <gnurt10@gmail.com>
2023-10-30 00:28:20 +0700

    Create 1722-minimize-hamming-distance-after-swap-operations.ts

---
+++ code/medium-2/1722-minimize-hamming-distance-after-swap-operations.ts
@@ -0,0 +1,32 @@
+function minimumHammingDistance(source: number[], target: number[], allowedSwaps: number[][]): number {
+    const size = source.length;
+    const union = [];
+    const sourceMap = new Map();
+    const find = (node) => union[node] === node ? node : find(union[node]);
+    let result = 0;
+
+    for (let index = 0; index < size; index++) union[index] = index;
+    for (const [a, b] of allowedSwaps) {
+        const parentA = find(a);
+        const parentB = find(b);
+        
+        if (parentA == parentB) continue;
+        union[parentA] = parentB; 
+    }
+    for (let index = 0; index < size; index++) {
+        const parent = find(index);
+        const group = sourceMap.get(parent);
+        const current = source[index];
+
+        group 
+            ? group.set(current, (group.get(current) ?? 0) + 1)
+            : sourceMap.set(parent, new Map([[current, 1]]));
+    }
+    for (let index = 0; index < size; index++) {
+        const group = sourceMap.get(find(index));
+        const count = group.get(target[index]);
+
+        count ? group.set(target[index], count - 1) : result += 1;
+    }
+    return result;
+};
