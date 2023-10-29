28a6a49a90c25fbb4146e907536c673c2d7a6f70
Quang Trung <gnurt10@gmail.com>
2023-10-29 11:04:54 +0700

    Create 1711-count-good-meals.ts

---
+++ code/medium-2/1711-count-good-meals.ts
@@ -0,0 +1,9 @@
+function countPairs(deliciousness: number[]): number {
+    let m = new Map(), ans = 0
+    for (let n of deliciousness) m.set(n, (m.get(n) || 0) + 1)           // Push D into a standard frequency map
+    for (let [k,v] of m)                                     // For each [key, value] pair
+        for (let i = 1 << 21; i >= 2 * k && i; i >>= 1)      // Iterate through power sums using bitwise shifts
+            if (i === 2 * k) ans += v * (v - 1) / 2          // If  k is its own matching number, add pairs to ans
+            else if (m.has(i - k)) ans += m.get(i - k) * v   // Otherwise, add product with matching number freq.
+    return ans % 1000000007
+};
