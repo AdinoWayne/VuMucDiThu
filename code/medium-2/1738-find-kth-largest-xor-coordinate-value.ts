78f8b930b84bd2d4250a64032d3e88cb37cf51f1
Quang Trung <gnurt10@gmail.com>
2023-11-01 06:45:59 +0700

    Create 1738-find-kth-largest-xor-coordinate-value.ts

---
+++ code/medium-2/1738-find-kth-largest-xor-coordinate-value.ts
@@ -0,0 +1,12 @@
+function kthLargestValue(M: number[][], k: number): number {
+    let y = M.length, x = M[0].length, ans = new Uint32Array(x*y), h = 0
+    for (let i = 0; i < y; i++)
+        for (let j = 0; j < x; j++) {
+            let cell = M[i][j]
+            if (i > 0) cell ^= M[i-1][j]
+            if (j > 0) cell ^= M[i][j-1]
+            if (i > 0 && j > 0) cell ^= M[i-1][j-1]
+            ans[h++] = M[i][j] = cell
+        }
+    return ans.sort()[x*y-k]
+};
