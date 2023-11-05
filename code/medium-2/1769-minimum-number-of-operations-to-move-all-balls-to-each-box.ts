596c4596afcbfc1d4aa8c86a59021471faa028b5
Quang Trung <gnurt10@gmail.com>
2023-11-05 18:57:43 +0700

    Create 1769-minimum-number-of-operations-to-move-all-balls-to-each-box.ts

---
+++ code/medium-2/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.ts
@@ -0,0 +1,21 @@
+function minOperations(boxes: string): number[] {
+      const result = Array(boxes.length).fill(0);
+  let notEmpty = 0;
+  let runningSum = 0;
+  
+  for (let i = 0; i < boxes.length; ++i) {
+    result[i] += runningSum;
+    if (boxes[i] === '1') ++notEmpty;
+    runningSum += notEmpty;
+  }
+  notEmpty = 0;
+  runningSum = 0;
+  
+  for (let i = boxes.length - 1; i >= 0; --i) {
+    result[i] += runningSum;
+    if (boxes[i] === '1') ++notEmpty;
+    runningSum += notEmpty;
+  }
+  
+  return result;
+};
