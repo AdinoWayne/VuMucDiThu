e1f38031e7b87a4e28d4c4b10b1a908878eb4158
Quang Trung <gnurt10@gmail.com>
2023-11-06 00:47:26 +0700

    Create 1781-sum-of-beauty-of-all-substrings.ts

---
+++ code/medium-2/1781-sum-of-beauty-of-all-substrings.ts
@@ -0,0 +1,25 @@
+function beautySum(s: string): number {
+    let map = new Map();
+    let res = 0;
+    for(let i = 0; i < s.length; i++) {
+        map.set(s[i], 1);
+        for(let j = i + 1; j < s.length; j++) {
+            map.set(s[j], (map.get(s[j]) || 0) + 1);
+            if(map.size >= 2) {
+                res += mostFreq(map)
+            }
+        }
+        map.clear();
+    }
+    return res
+};
+
+const mostFreq = (map) => {
+    let min = Infinity;
+    let max = -Infinity;
+    for(const k of map.values()) {
+        min = Math.min(min , k)
+        max = Math.max(max, k)
+    }
+    return max - min
+}
