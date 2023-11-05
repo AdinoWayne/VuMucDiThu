90291ff9ec5c6a4e3b2df56a33dec5fb075ea28e
Quang Trung <gnurt10@gmail.com>
2023-11-06 00:50:10 +0700

    Update 1641-count-sorted-vowel-strings.ts

--- code/medium-2/1641-count-sorted-vowel-strings.ts
+++ code/medium-2/1641-count-sorted-vowel-strings.ts
@@ -1,20 +1,18 @@
 function countVowelStrings(n: number): number {
     let res = 0
-    
     helper(n, 0)
-
     function helper(n, startIdx) { 
         // base case
         if (n === 0) {
             res++
             return
         }
-
         // recursive case
         for (let i = startIdx; i < 5; i++) {
             helper(n - 1, i)
         }
     }
-
     return res
 };
+// Time Complexity: O(5^n)
+// Space Complexity: O(n)
