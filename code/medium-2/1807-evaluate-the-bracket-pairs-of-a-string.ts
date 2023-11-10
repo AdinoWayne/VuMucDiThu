d1c9d7ba6901db529d58c9ed4d89708e053217a9
Quang Trung <gnurt10@gmail.com>
2023-11-10 07:11:11 +0700

    Create 1807-evaluate-the-bracket-pairs-of-a-string.ts

---
+++ code/medium-2/1807-evaluate-the-bracket-pairs-of-a-string.ts
@@ -0,0 +1,21 @@
+function evaluate(s: string, knowledge: string[][]): string {
+    let stack = [];
+    let hash = {};
+    for(let tuple of knowledge){
+        let [key, value] = tuple;
+        hash[key] = value;
+    }
+    for(let char of s){
+        if(char === ')'){
+            let key = '';
+            while(stack[stack.length-1] !== '('){
+                key = stack.pop() + key;
+            }
+            stack.pop();
+            stack.push(hash[key] || '?');
+        } else {
+            stack.push(char);
+        }
+    }
+    return stack.join('');
+};
