0ebc99a3e9eb7af20d44c3a7e00b22cbdd2f9217
Quang Trung <gnurt10@gmail.com>
2023-11-10 07:11:36 +0700

    Create 1813-sentence-similarity-iii.ts

---
+++ code/medium-2/1813-sentence-similarity-iii.ts
@@ -0,0 +1,22 @@
+function areSentencesSimilar(sentence1: string, sentence2: string): boolean {
+    let a1 = sentence1.split(" ");
+    let a2 = sentence2.split(" ");
+    return a1.length <= a2.length ? ok(a1, a2) : ok(a2, a1);
+};
+
+const ok = (short, long) => {
+    let re = []; // record short array length
+    while (1) {
+        if (short[0] == long[0]) {
+            short.shift();
+            long.shift();
+        }
+        if (short[short.length - 1] == long[long.length - 1]) {
+            short.pop();
+            long.pop();
+        }
+        if (re[0] == short.length) break; // short array length stop changing, loop end
+        re.unshift(short.length);
+    }
+    return short.length == 0;
+};
