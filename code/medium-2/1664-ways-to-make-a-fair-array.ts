a91e0ba76a39ecfa29dff5ce72640b3940142612
Quang Trung <gnurt10@gmail.com>
2023-11-12 00:44:43 +0700

    Update 1664-ways-to-make-a-fair-array.ts

--- code/medium-2/1664-ways-to-make-a-fair-array.ts
+++ code/medium-2/1664-ways-to-make-a-fair-array.ts
@@ -1,33 +1,37 @@
 function waysToMakeFair(nums: number[]): number {
-    let oddSum = 0; 
-    let evenSum = 0 ;
+    let prevEven = 0;
+    let prevOdd = 0;
+    let nextEven = 0;
+    let nextOdd = 0;
+
+    for (let index = 0; index < nums.length; index++) {
+        if (index % 2 === 0) {
+            nextEven += nums[index];
+        } else {
+            nextOdd += nums[index];
+        }
+    }
+
     let count = 0;
-    
-    for (let i = 0; i < nums.length; i++) {
-        if (i % 2 === 0) {
-            evenSum += nums[i];
+
+    for (let index = 0; index < nums.length; index++) {
+        if (index % 2 === 0) {
+            nextEven -= nums[index];
         } else {
-            oddSum += nums[i];
+            nextOdd -= nums[index];
+        }
+
+        if (prevEven + nextOdd === prevOdd + nextEven) {
+            count++;
+        }
+
+        if (index % 2 === 0) {
+            prevEven += nums[index];
+        } else {
+            prevOdd += nums[index];
         }
     }
-    
-    
-    for (let i = 0; i < nums.length; i++) {
-        if (i % 2 === 0) {
-            evenSum -= nums[i];
-            if (evenSum === oddSum) {
-                count++; 
-            }
-            oddSum += nums[i];
-        } else {
-            oddSum -= nums[i];
-            if (evenSum === oddSum) {
-                count++;
-            }
-            evenSum += nums[i]
-        }
-    }
-    
+
     return count;
 };
 // Time Complexity: O(n)
