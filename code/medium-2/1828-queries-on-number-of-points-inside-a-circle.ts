c0fbfeb60b90be17e2ab76c88c3e5a160bb0f611
Quang Trung <gnurt10@gmail.com>
2023-11-13 00:37:35 +0700

    Create 1828-queries-on-number-of-points-inside-a-circle.ts

---
+++ code/medium-2/1828-queries-on-number-of-points-inside-a-circle.ts
@@ -0,0 +1,11 @@
+function countPoints(points: number[][], queries: number[][]): number[] {
+    let result = [];
+    for (let [x,y,radius] of queries) {
+        let count = 0;
+        for (let [a,b] of points) {
+            count += (x- a) * (x- a) + (y-b) * (y-b) <= radius*radius ? 1 : 0;
+        }
+        result.push(count)
+    }
+    return result;
+};
