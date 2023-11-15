6dd4f17112d17a61d65c43a55aebf122a4740283
Quang Trung <gnurt10@gmail.com>
2023-11-16 00:15:09 +0700

    Create 1839-longest-substring-of-all-vowels-in-order.ts

---
+++ code/medium-2/1839-longest-substring-of-all-vowels-in-order.ts
@@ -0,0 +1,22 @@
+const mx = Math.max;
+const t = 'aeiou';
+function longestBeautifulSubstring(s: string): number {
+    let n = s.length;
+    let res = 0;
+    for (let i = 0; i < n; i++) {
+        if (s[i] == 'a') { // if want the longest, so have to keep as much as "a" comes left of "eiou", so the answer should start with "a"
+            let right = i;
+            let flag = 1;
+            for (let j = 0; j < 5; j++) {
+                if (s[right] != t[j]) {
+                    flag = 0;
+                    break;
+                }
+                while (right < n && s[right] == t[j]) right++;
+            }
+            if (flag) res = mx(res, right - i);
+            i = right - 1;
+        }
+    }
+    return res;
+};
