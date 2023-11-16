8238a322a36a55c1df8d0fa8151075c14ece6609
Quang Trung <gnurt10@gmail.com>
2023-11-17 00:43:18 +0700

    Create 1855-maximum-distance-between-a-pair-of-values.ts

---
+++ code/medium-2/1855-maximum-distance-between-a-pair-of-values.ts
@@ -0,0 +1,20 @@
+function maxDistance(nums1: number[], nums2: number[]): number {
+    let i = 0, j = 0;
+    
+    let ans = 0;
+    while (i < nums1.length && j < nums2.length) {
+        // maintain the i <= j invariant
+        j = Math.max(j, i);
+        
+        // we want to maximize j so move it forward whenever possible
+        while (nums1[i] <= nums2[j]) {
+            ans = Math.max(ans, j - i);
+            j++;
+        }
+        
+        // we want to minimize i so move it forward only to maintain invariants
+        i++;
+    }
+    
+    return ans;
+};
