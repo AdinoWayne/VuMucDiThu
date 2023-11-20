b081eb786818016e8100753f0929694487b872ce
Quang Trung <gnurt10@gmail.com>
2023-11-21 00:16:47 +0700

    Create 1881-maximum-value-after-insertion.ts

---
+++ code/medium-2/1881-maximum-value-after-insertion.ts
@@ -0,0 +1,13 @@
+function maxValue(n: string, x: number): string {
+    let i;
+    if (n[0] !== '-') {
+        for (i = 0; i < n.length; i++) {
+            if (Number(n[i]) < x) break;
+        }        
+    } else {
+        for (i = 1; i < n.length; i++) {
+            if (Number(n[i]) > x) break;
+        }
+    }
+    return n.slice(0, i) + x + n.slice(i)
+};
