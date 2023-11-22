2c153a88565fd432d321a95d676081e82dabcac6
Quang Trung <gnurt10@gmail.com>
2023-11-22 07:23:16 +0700

    Create 1884-egg-drop-with-2-eggs-and-n-floors.ts

---
+++ code/medium-2/1884-egg-drop-with-2-eggs-and-n-floors.ts
@@ -0,0 +1,10 @@
+function twoEggDrop(n: number): number {
+    let k = 2, dp = Array(k + 1).fill(0), res = 0;
+    while (dp[k] < n) {
+        for (let i = k; i > 0; i--) {
+            dp[i] = dp[i] + dp[i - 1] + 1;
+        }
+        res++;
+    }
+    return res;
+};
