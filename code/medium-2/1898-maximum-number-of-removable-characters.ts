75ddd83833254071c8b89edd66929231ef7fb842
Quang Trung <gnurt10@gmail.com>
2023-11-23 00:31:12 +0700

    Create 1898-maximum-number-of-removable-characters.ts

---
+++ code/medium-2/1898-maximum-number-of-removable-characters.ts
@@ -0,0 +1,30 @@
+function maximumRemovals(s: string, p: string, removable: number[]): number {
+    const isSubWithRemoved = (removed) => {
+        let pp = 0
+        
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] === p[pp] && !removed.has(i)) {
+                pp++
+            }
+            
+            if (pp === p.length) return true
+        }
+        return false
+    }
+    
+    let l = 0
+    let r = removable.length - 1
+    let mid
+    // standard binary search with checking if p is still substring
+    while (l <= r) {
+        mid = l + Math.floor((r-l)/2)
+        
+        let removed = new Set(removable.slice(0, mid+1))
+        if (isSubWithRemoved(removed)) {
+            l = mid + 1
+        } else {
+            r = mid - 1
+        }
+    }
+    return l
+};
