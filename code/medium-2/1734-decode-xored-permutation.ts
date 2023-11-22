b75db5dc1444e4daf7865eceafcb70976306284e
Quang Trung <gnurt10@gmail.com>
2023-11-22 07:57:26 +0700

    Update 1734-decode-xored-permutation.ts

--- code/medium-2/1734-decode-xored-permutation.ts
+++ code/medium-2/1734-decode-xored-permutation.ts
@@ -5,5 +5,11 @@ function decode(encoded: number[]): number[] {
     for (let i = 0; i < len; i++) perm[i+1] = perm[i] ^ encoded[i]
     return perm
 };
+// /problems/decode-xored-permutation/solutions/1035374/javascript-python-simple-5-line-solution-w-explanation-beats-100/
+// Thus we can simplify the equation for the XOR of all numbers from 1 to N for all odd values of N to (N + 1 >> 1) % 2.
+// Also, since XOR is its own inverse function, we can work the encoding equation backwards:
+// if:  E[i] = perm[i] ^ perm[i+1]           // By the encoding definition
+//  ⇒:  perm[i+1] = E[i] ^ perm[i]           // Inverted to solve for perm[i+1]
+
 // TC O(nLogn)
 // SC O(n)
