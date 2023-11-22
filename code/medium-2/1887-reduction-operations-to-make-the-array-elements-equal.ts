0799827ac97523541a34bdb8b938e3a45a2bff1c
Quang Trung <gnurt10@gmail.com>
2023-11-22 07:26:00 +0700

    Create 1887-reduction-operations-to-make-the-array-elements-equal.ts

---
+++ code/medium-2/1887-reduction-operations-to-make-the-array-elements-equal.ts
@@ -0,0 +1,8 @@
+function reductionOperations(nums: number[]): number {
+   nums.sort((a,b)=>a-b);
+    let count = 0;
+    for(let i = nums.length - 1;i>0;i--)
+        if(nums[i] !== nums[i-1])
+            count += nums.length - i
+    return count
+};
