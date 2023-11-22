5b281a845ee67b84821283acdb26c2eae0a27ce6
Quang Trung <gnurt10@gmail.com>
2023-11-23 00:29:51 +0700

    Create 1894-find-the-student-that-will-replace-the-chalk.ts

---
+++ code/medium-2/1894-find-the-student-that-will-replace-the-chalk.ts
@@ -0,0 +1,22 @@
+function chalkReplacer(chalk: number[], k: number): number {
+    let arr = new Array(chalk.length)
+    let sum = 0
+    for(let i =0; i< chalk.length; i++){
+        sum += chalk[i]
+        arr[i]= sum
+    }
+    
+    k = k%sum
+    if(k==0 || k < chalk[0])
+        return 0
+    let left = 0, right = chalk.length-1
+    while(left < right){
+        let mid = left + ((right - left) >> 1)
+        if(arr[mid] > k){
+            right = mid
+        }else{
+            left = mid + 1
+        }
+    }
+    return left
+};
