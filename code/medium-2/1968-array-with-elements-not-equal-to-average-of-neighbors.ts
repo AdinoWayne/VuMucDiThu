06d959089bee347755ac1932f2b94fa0448f042c
Quang Trung <gnurt10@gmail.com>
2023-12-03 09:02:57 +0700

    Create 1968-array-with-elements-not-equal-to-average-of-neighbors.ts

---
+++ code/medium-2/1968-array-with-elements-not-equal-to-average-of-neighbors.ts
@@ -0,0 +1,12 @@
+function rearrangeArray(nums: number[]): number[] {
+  nums.sort((a, b) => a-b); // sorts array in ascending order  
+  let result = [];
+  let left = 0;
+  let right = nums.length-1;  
+  while(left < right){
+    result.push(nums[left++]);
+    result.push(nums[right--]);
+  }  
+  if(left == right) result.push(nums[left]); // In case array length is odd we only need to insert the middle element once.  
+  return result;
+};
