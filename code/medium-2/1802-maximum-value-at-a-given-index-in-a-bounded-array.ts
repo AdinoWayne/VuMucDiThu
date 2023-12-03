e6e681ec79c3c88e88e23f633f414ed0cb43cabc
Quang Trung <gnurt10@gmail.com>
2023-12-03 10:18:40 +0700

    Update 1802-maximum-value-at-a-given-index-in-a-bounded-array.ts

--- code/medium-2/1802-maximum-value-at-a-given-index-in-a-bounded-array.ts
+++ code/medium-2/1802-maximum-value-at-a-given-index-in-a-bounded-array.ts
@@ -20,3 +20,11 @@ function maxValue(n: number, index: number, maxSum: number): number {
 };
 // TC O(log(maxSum))
 // SC O(1)
+
+Notes on findSum:
+
+// We have to consider two scenarios. If len < num, we can use the arithmetic formula normally, else we have to consider the trailing 1's.
+// Example, findSum(10, 3) means we find the sum of 10 + 9 + 8 whereas findSum(3, 5) means finding the sum of 3 + 2 + 1 + 1 + 1.
+// The formula for arithmetic progression is length of series * (largest number + smallest number) / 2
+// For the case of len < num, we get len * ( (num) + (num - len + 1) ) / 2.
+// For the case of len > num, we get num * ( num + 1 ) / 2 + (len - num) where (len - num) accounts for the trailing 1's.
