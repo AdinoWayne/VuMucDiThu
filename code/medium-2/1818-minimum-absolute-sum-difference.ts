4c2d296b9fded645579edc707c37dc65c7cbfb4f
Quang Trung <gnurt10@gmail.com>
2023-12-06 00:58:58 +0700

    Update 1818-minimum-absolute-sum-difference.ts

--- code/medium-2/1818-minimum-absolute-sum-difference.ts
+++ code/medium-2/1818-minimum-absolute-sum-difference.ts
@@ -7,23 +7,17 @@ function minAbsoluteSumDiff(nums1: number[], nums2: number[]): number {
         dists.push(dist);
         sum += dist;
     }
-
     nums1 = nums1.sort((a, b) => a - b);
-
     let ans = sum;
-
     for (let i = 0; i < nums1.length; i++) {
         let left = 0;
         let right = nums1.length - 1;
-        
         const oldDist = dists[i];
         let newDist = oldDist;
-        
         while (left <= right) {
             const j = Math.floor((left + right) / 2);
             const diff = nums1[j] - nums2[i];
             const dist = Math.abs(diff);
-            
             if (dist < newDist) {
                 newDist = dist;
                 
@@ -31,17 +25,15 @@ function minAbsoluteSumDiff(nums1: number[], nums2: number[]): number {
                     break;
                 }
             }
-            
             if (diff < 0) {
                 left = j + 1;
             } else {
                 right = j - 1;
             }
         }
-
         ans = Math.min(ans, (sum - oldDist) + newDist);
     }
-
-    
     return ans % (1e9 + 7);
 };
+// TC O(nLogn)
+// SC O(n)
