dfe7bfea5e08b836055b758f2ec570d8b88db7ba
Quang Trung <gnurt10@gmail.com>
2023-12-06 00:56:23 +0700

    Create 1996-the-number-of-weak-characters-in-the-game.ts

---
+++ code/medium-2/1996-the-number-of-weak-characters-in-the-game.ts
@@ -0,0 +1,9 @@
+function numberOfWeakCharacters(properties: number[][]): number {
+    properties.sort((a,b) => (a[0] == b[0]) ? (a[1]-b[1]) : (b[0]-a[0]))
+    let count = 0, max = 0
+    for(let arr of properties){
+        if(arr[1] < max)    count++
+        max = Math.max(max, arr[1])
+    }
+    return count
+};
