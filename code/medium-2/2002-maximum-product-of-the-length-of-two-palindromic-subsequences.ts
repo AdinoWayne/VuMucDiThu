231adfb1da283b38aaeefdc63301fd6a3cd2662d
Quang Trung <gnurt10@gmail.com>
2023-12-07 07:50:35 +0700

    Create 2002-maximum-product-of-the-length-of-two-palindromic-subsequences.ts

---
+++ code/medium-2/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.ts
@@ -0,0 +1,27 @@
+function maxProduct(s: string): number {
+    const len = s.length
+    const m = {} // hashmap for palindrome, ex: { 1: 1, 2:1, ... , 128: 1, 130, 2 }
+
+    for (let mask=1;mask<(1<<len);mask++) { // iterate all cases 
+        let subseq = "" // temporary palindrome subsequence string
+        for (let i=0;i<len;i++) { // iterate the total length of string
+            if (mask & (1 << i)) { // if true, found a character index for the mask
+                subseq += s[i] // append the character to the subsequence
+            }
+        }
+        if (subseq == [...subseq].reverse().join('')) { // check whether or not subseq is palindrome string
+            m[mask] = subseq.length // if it's palindrome, add it to the hashmap
+        }
+    }
+    
+    let res = 0
+    // iterate nested loop to find the max product
+    for (const [m1,] of Object.entries(m)) { 
+        for (const [m2,] of Object.entries(m)) {
+            if ((+m1 & +m2) == 0) { // found disjoint subseq
+               res = Math.max(res, m[m1] * m[m2])
+            }
+        }
+    }
+    return res
+};
