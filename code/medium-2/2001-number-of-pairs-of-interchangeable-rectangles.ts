0f6b2b82659b50a1a55bf302b7179ba485077d48
Quang Trung <gnurt10@gmail.com>
2023-12-07 07:49:50 +0700

    Create 2001-number-of-pairs-of-interchangeable-rectangles.ts

---
+++ code/medium-2/2001-number-of-pairs-of-interchangeable-rectangles.ts
@@ -0,0 +1,15 @@
+function interchangeableRectangles(rectangles: number[][]): number {
+    let ratio = {}
+    let res = 0
+    for (let i = 0; i < rectangles.length; i++) {
+        const rectangle = rectangles[i]
+        let r = rectangle[0]/rectangle[1]
+        
+        if (!ratio[r]) {
+            ratio[r] = 0
+        }
+        res += ratio[r]
+        ratio[r]++
+    }
+    return res
+};
