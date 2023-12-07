f389979ee99327056283f54f697e6c9d09f71788
Quang Trung <gnurt10@gmail.com>
2023-12-08 00:42:06 +0700

    Update 1823-find-the-winner-of-the-circular-game.ts

--- code/medium-2/1823-find-the-winner-of-the-circular-game.ts
+++ code/medium-2/1823-find-the-winner-of-the-circular-game.ts
@@ -3,14 +3,15 @@ function findTheWinner(n: number, k: number): number {
     for(let i = 1; i <= n;i++){
         que.push(i)
     }
-
     while(que.length > 1){
-		let deleteCount = k-1
-			while(deleteCount > 0){
-				que.push(que.shift())    //Rotate Elements  
-				deleteCount--
-			}
-        que.shift()                    // Delete kth element
+	let deleteCount = k-1
+	while(deleteCount > 0) {
+	  que.push(que.shift())    //Rotate Elements  
+	  deleteCount--
+	}
+        que.shift() // Delete kth element
     }
     return que.shift()  
 };
+// TC O(n2) 
+// SC O(n)
