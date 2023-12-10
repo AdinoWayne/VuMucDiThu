47bd1a4f2a42f57828f7ce9beffaf113e57fdf27
Quang Trung <gnurt10@gmail.com>
2023-12-11 00:56:35 +0700

    Create 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.ts

---
+++ code/medium-2/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.ts
@@ -0,0 +1,16 @@
+function numOfPairs(nums: string[], target: string): number {
+    var count = 0;
+    var x = 0;
+    while (x < nums.length) {
+        for (let y = 0; y<nums.length; y++) {
+            if (nums[x] + nums[y] == target) {
+                count += 1;
+                if (x == y) {
+                    count -= 1;
+                }
+            }
+        }
+        x++;
+    }
+    return count;
+};
