1155a89c61c64c07615a70caabe656441f14fac6
Quang Trung <gnurt10@gmail.com>
2023-12-12 00:09:37 +0700

    Create 2028-find-missing-observations.ts

---
+++ code/medium-2/2028-find-missing-observations.ts
@@ -0,0 +1,8 @@
+function missingRolls(rolls: number[], mean: number, n: number): number[] {
+    const diff = mean * (rolls.length + n) - rolls.reduce((a, b) => a + b);
+    if (diff < n) return []; // Cannot be covered even by case 1 1 1 ...
+    if (diff > 6 * n) return []; // Cannot be covered even by case 6 6 6 ...
+    return Array(n)
+        .fill(Math.ceil(diff / n), 0, diff % n)
+        .fill(Math.floor(diff / n), diff % n);
+};
