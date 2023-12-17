50a43e202d74218b6b9859875305b183b0b361ae
Quang Trung <gnurt10@gmail.com>
2023-12-17 11:56:34 +0700

    Update 1860-incremental-memory-leak.ts

--- code/medium-2/1860-incremental-memory-leak.ts
+++ code/medium-2/1860-incremental-memory-leak.ts
@@ -1,10 +1,12 @@
 function memLeak(memory1: number, memory2: number): number[] {
     let i;
-    for(i = 1; memory1 >= i || memory2 >= i ; i++){    
-		if(memory1 < memory2){
-			memory2 -= i;
-		} 
+    for (i = 1; memory1 >= i || memory2 >= i ; i++) {    
+	if(memory1 < memory2){
+		memory2 -= i;
+	} 
         else memory1 -= i;
-    }        
-    return [i,memory1,memory2];
+    }
+    return [i, memory1, memory2];
 };
+// TC O(n)
+// SC O(1)
