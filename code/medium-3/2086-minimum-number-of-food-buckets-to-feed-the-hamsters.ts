d47c5fd530c37ddfa4d67139591bf3264337b223
Quang Trung <gnurt10@gmail.com>
2023-12-18 00:31:18 +0700

    Create 2086-minimum-number-of-food-buckets-to-feed-the-hamsters.ts

---
+++ code/medium-3/2086-minimum-number-of-food-buckets-to-feed-the-hamsters.ts
@@ -0,0 +1,24 @@
+function minimumBuckets(hamsters: string): number {
+    const street = [...hamsters];
+    let buckets = 0;
+
+    
+    for (let i = 0; i < street.length; i++) {
+        if (street[i] === "H") {
+            if (street[i - 1] === 'B') continue;
+            if (street[i + 1] === '.') {
+                street[i + 1] = 'B';
+                buckets++;
+                continue;
+            }
+            if (street[i - 1] === '.') {
+                street[i - 1] = 'B';
+                buckets++;
+                continue;
+            }
+            else return -1;
+        }            
+    }
+    
+    return buckets;
+};
