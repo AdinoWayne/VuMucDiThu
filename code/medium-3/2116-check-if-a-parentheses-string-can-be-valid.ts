414cad5aa7b8108520c75f3f2fd61b444bff8689
Quang Trung <gnurt10@gmail.com>
2023-12-24 18:37:25 +0700

    Create 2116-check-if-a-parentheses-string-can-be-valid.ts

---
+++ code/medium-3/2116-check-if-a-parentheses-string-can-be-valid.ts
@@ -0,0 +1,39 @@
+function canBeValid(s: string, locked: string): boolean {
+    let flip = 0;
+    let open = 0;
+    let close = 0;
+    if(s.length % 2) {
+        return false;
+    }
+    for(let i = 0; i< s.length; i++) {
+        if(!+locked[i]) {
+            ++flip;
+        } else {
+            if(s[i] == "(") {
+                ++open;
+            } else {
+                ++close;
+            }
+        }
+        if(flip + open < close) {
+            return false;
+        }
+    }
+    flip = 0; open = 0; close = 0;
+    
+    for(let i = s.length-1; i>=0; i--) {
+        if(!+locked[i]) {
+            ++flip;
+        } else {
+            if(s[i] == "(") {
+                ++open;
+            } else {
+                ++close;
+            }
+        }
+        if(flip + close < open) {
+            return false;
+        }
+    }
+    return true;
+};
