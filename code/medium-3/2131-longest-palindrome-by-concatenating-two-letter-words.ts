1d959bdbf871ef23fbe6154f6544a1dd9b69836c
Quang Trung <gnurt10@gmail.com>
2023-12-27 00:03:53 +0700

    Create 2131-longest-palindrome-by-concatenating-two-letter-words.ts

---
+++ code/medium-3/2131-longest-palindrome-by-concatenating-two-letter-words.ts
@@ -0,0 +1,23 @@
+function longestPalindrome(words: string[]): number {
+    const obj = {};
+    let counter = 0;
+    
+    for(let word of words) {
+        const reversedWord = reverse(word);
+        if(obj[word]) {
+            obj[word]--;
+            counter += 4;
+        } else {
+            obj[reversedWord] = obj[reversedWord] + 1 || 1;
+        } 
+    }
+    
+    const hashGotMorePalindromes = Object.keys(obj).filter(str => obj[str] && reverse(str) === str);
+    
+    if(hashGotMorePalindromes.length) counter += 2;
+    
+    return counter;
+};
+
+// We define a reverse function to reverse our string.
+const reverse = str => str.split('').reverse().join('');
