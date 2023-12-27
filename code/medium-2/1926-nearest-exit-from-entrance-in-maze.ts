a222c131f84631dba4c6b47c6272c08cd63c40ac
Quang Trung <gnurt10@gmail.com>
2023-12-28 00:35:18 +0700

    Update 1926-nearest-exit-from-entrance-in-maze.ts

--- code/medium-2/1926-nearest-exit-from-entrance-in-maze.ts
+++ code/medium-2/1926-nearest-exit-from-entrance-in-maze.ts
@@ -3,18 +3,13 @@ function nearestExit(maze: string[][], entrance: number[]): number {
     const mazeHeight = maze[0].length;
     let hasCheckedEntranceCell = false;
     let stepsCounter = 0;
-
     const queue = [{ x: entrance[0], y: entrance[1] }];
-    
     while (queue.length > 0) {
         const l = queue.length;
-
         for (let i = 0; i < l ; i++) {
             const position = queue.shift();
-            
             if (maze[position.x][position.y] === '.') {
-                maze[position.x][position.y] = 'x'; // visited
-
+                maze[position.x][position.y] = 'x';
                 if (hasCheckedEntranceCell && (
                     position.x === 0 ||
                     position.y === 0 ||
@@ -22,19 +17,16 @@ function nearestExit(maze: string[][], entrance: number[]): number {
                     position.y === mazeHeight -1)) {
                     return stepsCounter;
                 }
-
                 hasCheckedEntranceCell = true;
-
                 if (position.x + 1 < mazeWidth) queue.push({ x: position.x + 1, y: position.y });
                 if (position.y + 1 < mazeHeight) queue.push({ x: position.x, y: position.y + 1 });
                 if (position.x - 1 >= 0) queue.push({ x: position.x - 1, y: position.y });
                 if (position.y - 1 >= 0) queue.push({ x: position.x, y: position.y - 1 });
             }
         }
-        
         stepsCounter++;
     }
-
-    
     return -1;
 };
+// TC O(n)
+// SC O(n)
