7f92c35a5eaf07c2ed8ae78b78342ef43618442c
Quang Trung <gnurt10@gmail.com>
2023-12-29 09:27:19 +0700

    Create 2146-k-highest-ranked-items-within-a-price-range.ts

---
+++ code/medium-3/2146-k-highest-ranked-items-within-a-price-range.ts
@@ -0,0 +1,22 @@
+function numberOfArrays(differences: number[], lower: number, upper: number): number {
+    let dp = new Array(differences.length+1)
+    
+    dp[0] = 0
+    let min = dp[0]
+    let max = dp[0]
+    for(let i = 1; i <= differences.length; i++){
+        dp[i] = dp[i-1] + differences[i-1]
+        min = Math.min(dp[i], min)
+        max = Math.max(dp[i], max)
+    }
+    
+    let shift = min - lower
+    max = max - shift
+    
+    let count = upper - max + 1
+    if(count <= 0){
+        return 0
+    }
+
+    return count
+};
