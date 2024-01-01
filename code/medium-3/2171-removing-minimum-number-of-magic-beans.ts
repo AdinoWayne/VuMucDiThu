82114a67e7f63cdd682295dca2d36999bc87d860
Quang Trung <gnurt10@gmail.com>
2024-01-01 18:58:47 +0700

    Create 2171-removing-minimum-number-of-magic-beans.ts

---
+++ code/medium-3/2171-removing-minimum-number-of-magic-beans.ts
@@ -0,0 +1,13 @@
+function minimumRemoval(beans: number[]): number {
+    const N = beans.length;
+    const sum = beans.reduce((acc, c) => c + acc, 0);
+
+    beans.sort((a,b) => a - b);
+
+    let ans = Infinity;
+    for(let i = 0 ; i < beans.length; i++) {
+        ans = Math.min(ans, sum - (N - i) * beans[i])
+    }
+
+    return ans;
+};
