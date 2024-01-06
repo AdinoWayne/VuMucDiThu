43d73aade159cccb7a147d4cf20e5d8c940c784f
Quang Trung <gnurt10@gmail.com>
2024-01-06 21:29:59 +0700

    Create 2216-minimum-deletions-to-make-array-beautiful.ts

---
+++ code/medium-3/2216-minimum-deletions-to-make-array-beautiful.ts
@@ -0,0 +1,11 @@
+function minDeletion(nums: number[]): number {
+    var count = 0;
+    for( let i = 0; i < nums.length ; i++ ) {
+        if( i % 2 === 0 && nums[i] === nums[i + 1] ) {
+            nums.shift();
+            i--;
+            count++;
+        }
+    }
+    return nums.length % 2 !== 0 ? count + 1 : count;
+};
