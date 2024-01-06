73b84596d6a19eb335455e58f03b9dcbc3330034
Quang Trung <gnurt10@gmail.com>
2024-01-06 21:31:14 +0700

    Create 2221-find-triangular-sum-of-an-array.ts

---
+++ code/medium-3/2221-find-triangular-sum-of-an-array.ts
@@ -0,0 +1,10 @@
+function triangularSum(nums: number[]): number {
+    let start = 0, end = nums.length - 1;
+    while (start < end) {
+        for (let i = start; i < end; i++) {
+            nums[i] = (nums[i] + nums[i + 1]) % 10;
+        }
+        end--;
+    }
+    return nums[0];
+};
