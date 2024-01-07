e331f17ccb687582c62c9c253fc5e69bf1271151
Quang Trung <gnurt10@gmail.com>
2024-01-07 23:06:26 +0700

    Create 2225-find-players-with-zero-or-one-losses.ts

---
+++ code/medium-3/2225-find-players-with-zero-or-one-losses.ts
@@ -0,0 +1,17 @@
+function findWinners(matches: number[][]): number[][] {
+    let p = new Set();
+    let ml = new Map();
+
+    for (let [w, l] of matches) {
+        p.add(w);
+        p.add(l);
+        ml.set(l, (ml.get(l) || 0) + 1);
+    }
+
+    let ans = [[], []];
+    for (let x of p) {
+        if (ml.has(x) === false) ans[0].push(x);
+        if (ml.get(x) === 1)     ans[1].push(x);
+    }
+    return ans.map((arr) => arr.sort((a, b) => a - b));
+};
