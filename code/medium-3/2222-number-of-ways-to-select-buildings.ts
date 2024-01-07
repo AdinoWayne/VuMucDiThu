636a76bdf7896f88a47ae9a05efb4931c7e4400e
Quang Trung <gnurt10@gmail.com>
2024-01-07 23:06:45 +0700

    Create 2222-number-of-ways-to-select-buildings.ts

---
+++ code/medium-3/2222-number-of-ways-to-select-buildings.ts
@@ -0,0 +1,16 @@
+function numberOfWays(s: string): number {
+    let result = 0,
+    zeroes = s.replaceAll('1', '').length,
+    ones = s.length - zeroes,
+    curZeroes = 0,
+    curOnes = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        s[i] === '0'
+        ? (result += curOnes * (ones - curOnes))
+        : (result += curZeroes * (zeroes - curZeroes));
+        
+        s[i] === '0' ? curZeroes++ : curOnes++;
+    }
+    return result;
+};
