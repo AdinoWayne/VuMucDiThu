900c02ae44ccc1960130644730316404ef84679e
Quang Trung <gnurt10@gmail.com>
2024-01-08 01:04:20 +0700

    Create 2240-number-of-ways-to-buy-pens-and-pencils.ts

---
+++ code/medium-3/2240-number-of-ways-to-buy-pens-and-pencils.ts
@@ -0,0 +1,9 @@
+function waysToBuyPensPencils(total: number, cost1: number, cost2: number): number {
+    let res = 0;
+    let i = 0
+    while (cost1 * i <= total){
+        res += 1 +  Math.trunc((total - cost1 * i) / cost2)
+        i++
+    }  
+    return res
+};
