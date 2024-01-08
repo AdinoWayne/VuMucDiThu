c4443cc9a3ee195f915d186c08e62d7f321e93cd
Quang Trung <gnurt10@gmail.com>
2024-01-09 00:32:07 +0700

    Create 2244-minimum-rounds-to-complete-all-tasks.ts

---
+++ code/medium-3/2244-minimum-rounds-to-complete-all-tasks.ts
@@ -0,0 +1,15 @@
+function minimumRounds(tasks: number[]): number {
+    const counts = {};
+    for (let task of tasks) {
+        counts[task] = (counts[task] || 0) + 1;
+    }
+    let rounds = 0;
+    for (let count of Object.values(counts)) {
+        const num = Number(count);
+        if (num < 2) {
+            return -1;
+        }
+        rounds += Math.ceil(num / 3);
+    }
+    return rounds;
+};
