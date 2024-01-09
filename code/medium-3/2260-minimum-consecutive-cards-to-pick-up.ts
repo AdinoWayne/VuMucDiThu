0f67eef687009e5a5de2f290b85eee0d5544048f
Quang Trung <gnurt10@gmail.com>
2024-01-10 00:08:32 +0700

    Create 2260-minimum-consecutive-cards-to-pick-up.ts

---
+++ code/medium-3/2260-minimum-consecutive-cards-to-pick-up.ts
@@ -0,0 +1,11 @@
+function minimumCardPickup(cards: number[]): number {
+    let ans=Infinity;
+    let obj={};
+    for(let i=0; i<cards.length; i++){
+      if(obj[cards[i]]===undefined){ 
+        obj[cards[i]]=i;
+      }
+      else{ ans=Math.min(ans,i-obj[cards[i]]); obj[cards[i]]=i; }
+    }
+    return ans===Infinity ?-1 :ans+1;  
+};
