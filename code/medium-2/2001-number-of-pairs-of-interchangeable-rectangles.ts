3f6e29ce16310dcd500d02383b26fba24b0f42e6
Quang Trung <gnurt10@gmail.com>
2024-01-10 08:09:11 +0700

    Update 2001-number-of-pairs-of-interchangeable-rectangles.ts

--- code/medium-2/2001-number-of-pairs-of-interchangeable-rectangles.ts
+++ code/medium-2/2001-number-of-pairs-of-interchangeable-rectangles.ts
@@ -15,3 +15,6 @@ function interchangeableRectangles(rectangles: number[][]): number {
 };
 // TC O(n)
 // SC O(n)
+// Idea is very easy. We need to find the ratio of width and height for each rectangle in the vector.
+// Now, if we have already seen that ratio earlier in the map, simply add the count of ratio to result. It includes all the possbile combinations of rectangle pairs with same ratio.
+// Also, increment the count of current ratio in the map as it denotes the number of rectangle with current ratio.
