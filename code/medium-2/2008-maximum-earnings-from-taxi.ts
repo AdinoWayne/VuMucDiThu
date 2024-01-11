03096b472df3580add28d224d699dd6f95fae19f
Quang Trung <gnurt10@gmail.com>
2024-01-12 00:13:25 +0700

    Update 2008-maximum-earnings-from-taxi.ts

--- code/medium-2/2008-maximum-earnings-from-taxi.ts
+++ code/medium-2/2008-maximum-earnings-from-taxi.ts
@@ -2,15 +2,12 @@ function maxTaxiEarnings(n: number, rides: number[][]): number {
     if(n === 1){
         return rides[0][1] - rides[0][0] + rides[0][2];
     }
-    //sort rides by end point
     rides.sort((a,b) => a[1] - b[1]);
     let dp = [];
     dp[0] = 0;
     let j = 0;
-    // i will be the end point
     for(let i = 1; i <= n; i++){
         dp[i] = dp[i-1];
-        //loop through rides to find ride that has an end point i
         while(j < rides.length && rides[j][1] === i){
             let tempRide = rides[j];
             let tempEarn = tempRide[1] - tempRide[0] + tempRide[2];
@@ -18,6 +15,7 @@ function maxTaxiEarnings(n: number, rides: number[][]): number {
             j++;
         }
     }
-    
     return dp[n];
 };
+// TC O(nLogn)
+// SC O(n)
