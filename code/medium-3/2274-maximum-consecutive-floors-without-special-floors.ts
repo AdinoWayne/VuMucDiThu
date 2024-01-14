21665040d0edc5202b7096bfea4b1faca4420770
Quang Trung <gnurt10@gmail.com>
2024-01-14 22:27:34 +0700

    Create 2274-maximum-consecutive-floors-without-special-floors.ts

---
+++ code/medium-3/2274-maximum-consecutive-floors-without-special-floors.ts
@@ -0,0 +1,10 @@
+function maxConsecutive(bottom: number, top: number, special: number[]): number {
+    special.sort((a,b)=>b-a)
+    let max=-Infinity
+    for(let i=0;i<special.length-1;i++){
+    max=Math.max(max,special[i]-special[i+1]-1)
+    }
+
+    max=Math.max(max,special[special.length-1]-bottom,top-special[0])
+    return max
+};
