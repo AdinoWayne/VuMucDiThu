62a58f16455d3e1b812212ae42c4f45b8b67bbc2
Quang Trung <gnurt10@gmail.com>
2024-01-15 07:24:55 +0700

    Create 2284-sender-with-largest-word-count.ts

---
+++ code/medium-3/2284-sender-with-largest-word-count.ts
@@ -0,0 +1,14 @@
+function largestWordCount(messages: string[], senders: string[]): string {
+    let wordCount = {}
+    let result = ''
+    let maxCount = -Infinity
+    for (let i = 0; i < messages.length;i++) {
+        let count=messages[i].split(' ').length
+        wordCount[senders[i]] = wordCount[senders[i]] == undefined ? count : wordCount[senders[i]] + count;
+        if (wordCount[senders[i]]  > maxCount || (wordCount[senders[i]]  == maxCount && senders[i] > result)) {
+            maxCount = wordCount[senders[i]];
+            result = senders[i];
+        }
+    }
+    return result;
+};
