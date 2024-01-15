686c835e6099e9af5340e35429bd9e18ff54833a
Quang Trung <gnurt10@gmail.com>
2024-01-15 07:44:24 +0700

    Update 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.ts

--- code/medium-2/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.ts
+++ code/medium-2/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.ts
@@ -16,3 +16,27 @@ function numOfPairs(nums: string[], target: string): number {
 };
 // TC O(n^2)
 // SC O(1)
+
+function numOfPairs(nums: string[], target: string): number {
+        const freq = new Map();
+    for (const num of nums) {
+        if (num.length < target.length) {
+        freq.set(num, (freq.get(num) || 0) + 1);
+        }
+    }
+    let res = 0;
+    for (const [s, frq] of freq.entries()) {
+        if (target.startsWith(s)) {
+        if (s + s === target) {
+            res += frq * (frq - 1);
+        } else {
+            const other = target.slice(s.length);
+            res += frq * (freq.get(other) || 0);
+        }
+        }
+    }
+
+    return res;
+};
+// TC O(2n)
+// SC O(n2)
