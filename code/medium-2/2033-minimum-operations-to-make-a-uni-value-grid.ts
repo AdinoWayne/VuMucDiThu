ffe0c550e6bf5c3a891e844d2290c788a7ec46ad
Quang Trung <gnurt10@gmail.com>
2024-01-16 00:15:07 +0700

    Update 2033-minimum-operations-to-make-a-uni-value-grid.ts

--- code/medium-2/2033-minimum-operations-to-make-a-uni-value-grid.ts
+++ code/medium-2/2033-minimum-operations-to-make-a-uni-value-grid.ts
@@ -5,29 +5,21 @@ function minOperations(grid: number[][], x: number): number {
             nums.push(grid[i][j])
         }
     }
-    
-    // 1. only one single number, return directly
     if (nums.length < 2) {
         return 0
     }
-    
-    // 2. sorting all grid numbers in One - Dimension array
     nums.sort((a, b) => a -b) 
-    
-    // 3. find the median, which is the final target equal number index
-	const n = nums.length
+    const n = nums.length
     const median = Math.floor(n/2)
-    
-    // 4. calculate the times of add and subtract
     let ans = 0
     for (let i=0; i< n; i++) {
         const distance = Math.abs(nums[i] - nums[median])
-        // 4.1. number i can not get to the median number, return -1
         if (distance % x) {
             return -1
         }
-        // 4.2. the operations required for number i to get to the median number
         ans += (distance / x)
     }
     return ans
 };
+// TC O(nLogn)
+// SC O(n)
