347d8425673e9d531853c657b1c250b905d2ce9a
Quang Trung <gnurt10@gmail.com>
2024-01-17 00:26:22 +0700

    Create 2300-successful-pairs-of-spells-and-potions.ts

---
+++ code/medium-3/2300-successful-pairs-of-spells-and-potions.ts
@@ -0,0 +1,25 @@
+function successfulPairs(spells: number[], potions: number[], success: number): number[] {
+    const pairs = [];
+    potions.sort((a, b) => a - b);
+    const spellsLength = spells.length;
+    const potionsLength = potions.length;
+
+    for (let i = 0; i < spellsLength; i++) {
+        let count = 0;
+        // binary search
+        let left = 0;
+        let right = potionsLength - 1;
+        while (left <= right) {
+            // const mid = Math.floor((left + right) / 2);
+            const mid = (left + right) >> 1;
+            if (spells[i] * potions[mid] >= success) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        count = potionsLength - left;
+        pairs.push(count);
+    }
+    return pairs;
+};
