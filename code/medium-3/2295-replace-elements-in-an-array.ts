259f79c6b6c4d7273b265a6f8c1dd31a5503b1cc
Quang Trung <gnurt10@gmail.com>
2024-01-17 00:25:21 +0700

    Create 2295-replace-elements-in-an-array.ts

---
+++ code/medium-3/2295-replace-elements-in-an-array.ts
@@ -0,0 +1,20 @@
+function arrayChange(nums: number[], operations: number[][]): number[] {
+    let map = new Map()
+    for(let i = 0; i < nums.length; i++){
+        let num = nums[i]
+        map.set(num, i)
+    }
+    for(let op of operations){
+        let key = op[0]
+        let value = op[1]
+        if(map.has(key)){
+            const idx = map.get(key)
+            map.set(value, idx)
+            map.delete(key)
+        }
+    }
+    for(let [key, idx] of map){
+        nums[idx] = key
+    }
+    return nums
+};
