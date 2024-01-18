07151d41865b571955d7c1ba385968f99d0abd75
Quang Trung <gnurt10@gmail.com>
2024-01-18 07:01:45 +0700

    Create 2305-fair-distribution-of-cookies.ts

---
+++ code/medium-3/2305-fair-distribution-of-cookies.ts
@@ -0,0 +1,19 @@
+function distributeCookies(cookies: number[], k: number): number {
+    let ans = Infinity;
+    let bag = Array(k).fill(0)
+    function backtrack(i) {
+        if(i >= cookies.length) {
+            let max = -Infinity;
+            for(let b of bag) max = Math.max(max, b);
+            ans = Math.min(ans, max);
+            return;
+        }
+        for(let j=0;j<k;j++) {
+            bag[j] += cookies[i];
+            backtrack(i+1);
+            bag[j] -= cookies[i];
+        }
+    }
+    backtrack(0);
+    return ans;
+};
