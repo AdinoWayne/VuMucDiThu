8f5e7b9f4eab6df90ae4c4309e5d7138f8bf76b6
Quang Trung <gnurt10@gmail.com>
2024-01-19 00:02:31 +0700

    Create 2320-count-number-of-ways-to-place-houses.ts

---
+++ code/medium-3/2320-count-number-of-ways-to-place-houses.ts
@@ -0,0 +1,14 @@
+function countHousePlacements(n: number): any {
+    const MOD = 1000000007n
+    let house = 1n
+    let space = 1n
+    let total = house + space
+
+    for (let i = 2; i < n + 1; i++) {
+        house = space
+        space = total
+        total = (house + space) % MOD
+    }
+
+    return (total * total) % MOD
+};
