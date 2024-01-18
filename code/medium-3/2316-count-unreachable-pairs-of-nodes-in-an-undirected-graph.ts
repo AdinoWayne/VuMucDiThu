ec95f4468e1967c297babafeb5f6a6899b52a269
Quang Trung <gnurt10@gmail.com>
2024-01-19 00:02:11 +0700

    Create 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.ts

---
+++ code/medium-3/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.ts
@@ -0,0 +1,39 @@
+function countPairs(n: number, edges: number[][]): number {
+    const arr = new Array(n).fill(-1);
+
+    const find = (a) => {
+        return(arr[a] < 0) ? a : find(arr[a]);
+    };
+
+    const union = (a, b) => {
+        const pa = find(a), pb = find(b);
+
+        if(pa === pb) return;
+
+        if(arr[pa] <= arr[pb]) {
+            arr[pa] += arr[pb];
+            arr[pb] = pa;
+        } else {
+            arr[pb] += arr[pa];
+            arr[pa] = pb;   
+        }
+    }
+
+    for(const [a,b] of edges) {
+        union(a,b);
+    }
+
+    let totalCount = 0, ans = 0;
+    
+    for(const connected of arr) {
+
+        if(connected > -1) continue;
+        
+        const countConnected = -1 * connected;
+
+        ans += totalCount  *  countConnected ;
+        totalCount += countConnected;
+    }
+
+    return ans;
+};
