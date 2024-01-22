ed3231ec5377a1a115e1d472758f6b8692cda708
Quang Trung <gnurt10@gmail.com>
2024-01-23 01:08:33 +0700

    Create 2342-max-sum-of-a-pair-with-equal-sum-of-digits.ts

---
+++ code/medium-3/2342-max-sum-of-a-pair-with-equal-sum-of-digits.ts
@@ -0,0 +1,31 @@
+function maximumSum(nums: number[]): number {
+    const hash = {}
+    
+    for (let num of nums) {
+        const str = num + ''
+        
+        let sum = 0
+        
+        for (let ch of str) {
+            sum += parseInt(ch)
+        }
+        
+        if (!(sum in hash)) {
+            hash[sum] = []
+        }
+        
+        hash[sum].push(num)
+    }
+    
+    let max = -1
+    
+    for (let prop in hash) {
+        hash[prop].sort((a, b) => b - a)
+        
+        if (hash[prop].length > 1) {
+            max = Math.max(max, hash[prop][0] + hash[prop][1])
+        }
+    }
+    
+    return max
+};
