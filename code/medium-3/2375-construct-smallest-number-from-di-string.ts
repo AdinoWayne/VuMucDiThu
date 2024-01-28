08784dad9cf0d2c38b5724d8254f4d410a15598d
Quang Trung <gnurt10@gmail.com>
2024-01-29 00:49:06 +0700

    Create 2375-construct-smallest-number-from-di-string.ts

---
+++ code/medium-3/2375-construct-smallest-number-from-di-string.ts
@@ -0,0 +1,14 @@
+function smallestNumber(pattern: string): string {
+    const ans = [];
+    let stack = [];
+    
+    for (let i = 0; i <= pattern.length; i++) {
+        stack.push(i + 1);
+        if (pattern[i] === 'I' || i === pattern.length) {
+            ans.push(...stack.reverse());
+            stack = [];
+        }
+    }
+    
+    return ans.join('');
+};
