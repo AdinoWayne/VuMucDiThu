0b7a2b42b48d5f9d6e871541b945d14280d04068
Quang Trung <gnurt10@gmail.com>
2024-01-29 00:49:30 +0700

    Create 2380-time-needed-to-rearrange-a-binary-string.ts

---
+++ code/medium-3/2380-time-needed-to-rearrange-a-binary-string.ts
@@ -0,0 +1,22 @@
+function secondsToRemoveOccurrences(s: string): number {
+    let cnt = -1
+    
+    function count(s){
+        cnt++
+        let str = ""
+        let isvalid = true
+        for(let i=0; i<s.length; i++){
+            if(i < s.length-1 && s[i] < s[i+1]){
+                isvalid = false
+                str += s[i+1]
+                str += s[i]
+                i++;
+            }else if(i < s.length){
+                str += s[i];
+            }
+        }
+        return isvalid == true ? cnt : count(str);
+    }
+    
+    return count(s)
+};
