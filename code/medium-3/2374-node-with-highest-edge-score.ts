38e5a346974518f862c64ef45e3bb99252a90083
Quang Trung <gnurt10@gmail.com>
2024-01-28 22:09:16 +0700

    Create 2374-node-with-highest-edge-score.ts

---
+++ code/medium-3/2374-node-with-highest-edge-score.ts
@@ -0,0 +1,31 @@
+function edgeScore(edges: number[]): number {
+    let maxSum = -Infinity;
+    const outputConfig = {};
+    const edgeConfig = {};
+    for (let index = 0; index < edges.length; index++) {
+        const to = edges[index];
+        if (!edgeConfig[to]) {
+            edgeConfig[to] = 0;
+        }
+        const tempOutput = edgeConfig[to] + index;
+        if (!outputConfig[tempOutput]) {
+            outputConfig[tempOutput] = [];
+        }
+        outputConfig[tempOutput].push(to);
+        if (tempOutput > maxSum) {
+            maxSum = tempOutput;
+        }
+        edgeConfig[to] = tempOutput;
+    }
+    return Math.min(...outputConfig[maxSum]);
+};
+
+function findSmallest(arr) {
+    let small = Infinity;
+    for (const iterator of arr) {
+        if (iterator < small) {
+            small = iterator;
+        }
+    }
+    return small;
+}
