fb1917a0822748722e48c8afff3b2f0ec4e228af
Quang Trung <gnurt10@gmail.com>
2024-01-30 00:28:01 +0700

    Update 2091-removing-minimum-and-maximum-from-array.ts

--- code/medium-3/2091-removing-minimum-and-maximum-from-array.ts
+++ code/medium-3/2091-removing-minimum-and-maximum-from-array.ts
@@ -3,18 +3,19 @@ function minimumDeletions(nums: number[]): number {
     max = -Infinity,
     minIdx = 0,
     maxIdx = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < min) {
-      min = nums[i];
-      minIdx = i;
-    }
-    if (nums[i] > max) {
-      max = nums[i];
-      maxIdx = i;
-    }
-  }
-//reuse those 2 variables :)
-  min = minIdx < maxIdx ? minIdx : maxIdx;
-  max = minIdx < maxIdx ? maxIdx : minIdx;
-  return Math.min(min + (nums.length - max) + 1, max + 1, nums.length - min);
+      for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < min) {
+          min = nums[i];
+          minIdx = i;
+        }
+        if (nums[i] > max) {
+          max = nums[i];
+          maxIdx = i;
+        }
+      }
+      min = minIdx < maxIdx ? minIdx : maxIdx;
+      max = minIdx < maxIdx ? maxIdx : minIdx;
+      return Math.min(min + (nums.length - max) + 1, max + 1, nums.length - min);
 };
+// TC O(n)
+// SC O(1)
