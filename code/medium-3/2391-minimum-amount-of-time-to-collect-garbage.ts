64c766d658f1df8ee114a786a317a8589d4f4f46
Quang Trung <gnurt10@gmail.com>
2024-01-31 02:21:54 +0700

    Create 2391-minimum-amount-of-time-to-collect-garbage.ts

---
+++ code/medium-3/2391-minimum-amount-of-time-to-collect-garbage.ts
@@ -0,0 +1,16 @@
+function garbageCollection(garbage: string[], travel: number[]): number {
+    let travelTime = 0
+    garbage = garbage.reverse()
+    
+    for (const type of ['G', 'P', 'M']) {
+        const lastHouseWithGarbage = garbage.findIndex(house => house.includes(type))
+        
+        if (lastHouseWithGarbage === -1) {
+        continue
+        }
+
+        travelTime += travel.slice(0, garbage.length - lastHouseWithGarbage - 1).reduce((acc, num) => acc + num, 0)
+    }
+
+    return garbage.join('').length + travelTime
+};
