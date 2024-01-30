d75f0b3ed9ea2b85d5fb33a3a071b03d2be721bf
Quang Trung <gnurt10@gmail.com>
2024-01-31 02:25:09 +0700

    Update 2096-step-by-step-directions-from-a-binary-tree-node-to-another.ts

--- code/medium-3/2096-step-by-step-directions-from-a-binary-tree-node-to-another.ts
+++ code/medium-3/2096-step-by-step-directions-from-a-binary-tree-node-to-another.ts
@@ -22,20 +22,18 @@ function getDirections(root: TreeNode | null, startValue: number, destValue: num
             return getPath(node.left, value, acc + 'L') + getPath(node.right, value, acc + 'R')
         }
     }
-    
-	// generate the paths
+
     let startPath = getPath(root, startValue);
     let destPath = getPath(root, destValue);
-    
-    // find the lowest common ancestor
+
     let i = 0;
     for (; i < startPath.length && i < destPath.length && startPath[i] === destPath[i]; i++);
-    
-	// output the final path
+
     let output = '';
     for (let j = i; j < startPath.length; j++) {
         output += 'U';
     }
-    
     return output + destPath.substring(i);
 };
+// TC O(n2)
+// SC O(n)
