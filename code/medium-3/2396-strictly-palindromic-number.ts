d73d3b47acdda9ece55f70906f4cd36de9901d0b
Quang Trung <gnurt10@gmail.com>
2024-01-31 02:22:22 +0700

    Create 2396-strictly-palindromic-number.ts

---
+++ code/medium-3/2396-strictly-palindromic-number.ts
@@ -0,0 +1,9 @@
+function isStrictlyPalindromic(n: number): boolean {
+    let i=2;
+    while(i<n-1){
+        let temp = n.toString(i);
+        if(temp !== temp.split('').reverse().join('')) return false;
+        i++;
+    }
+    return true;
+};
