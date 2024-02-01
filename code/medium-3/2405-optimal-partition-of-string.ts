3ad9d44604d372b2326368c295b0d46f17d81b31
Quang Trung <gnurt10@gmail.com>
2024-02-01 07:46:22 +0700

    Create 2405-optimal-partition-of-string.ts

---
+++ code/medium-3/2405-optimal-partition-of-string.ts
@@ -0,0 +1,10 @@
+function partitionString(s: string, count=1, map = new Map()): number {
+    for(let i=0; i<s.length; i++) {
+        if(map.has(s[i])){
+            count++;
+            map = new Map();
+        }
+        map.set(s[i], 1);
+    }
+    return count;
+};
