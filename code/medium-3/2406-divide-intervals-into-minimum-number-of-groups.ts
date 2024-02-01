a45f2b86ea9f68ee62d581418d7a58af18bb3632
Quang Trung <gnurt10@gmail.com>
2024-02-02 01:39:33 +0700

    Create 2406-divide-intervals-into-minimum-number-of-groups.ts

---
+++ code/medium-3/2406-divide-intervals-into-minimum-number-of-groups.ts
@@ -0,0 +1,14 @@
+function minGroups(intervals: number[][]): number {
+    const starts = intervals.map((el) => el[0]).sort((a, b) => a - b);
+     const ends = intervals.map((el) => el[1]).sort((a, b) => a - b);
+      let room = 0;
+      let endIdx = 0;
+      for (let i = 0; i < starts.length; i++) {
+        if (starts[i] <= ends[endIdx]) {
+          room++;
+        } else {
+          endIdx++;
+      }
+     }
+     return room;
+};
