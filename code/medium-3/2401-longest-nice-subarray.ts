c74ecfe10f1a8c5c4f8c254e890b3decd08c1ece
Quang Trung <gnurt10@gmail.com>
2024-02-01 07:46:02 +0700

    Create 2401-longest-nice-subarray.ts

---
+++ code/medium-3/2401-longest-nice-subarray.ts
@@ -0,0 +1,12 @@
+function longestNiceSubarray(nums: number[]): number {
+    let mask=0,j=0,maxWindow=0;
+    for(let i=0;i<nums.length;i++){
+        while((mask & nums[i])!==0){//Need to reduce the window
+            mask = mask & ~nums[j];
+            j++;
+        }
+        mask = mask|nums[i];
+        maxWindow = Math.max(maxWindow,i-j+1);
+    }
+    return maxWindow;
+};
