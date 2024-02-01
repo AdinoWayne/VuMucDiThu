58bb646b032bde5c99a01ca65ea3c4c8ad379b2b
Quang Trung <gnurt10@gmail.com>
2024-02-02 01:39:07 +0700

    Create 2410-maximum-matching-of-players-with-trainers.ts

---
+++ code/medium-3/2410-maximum-matching-of-players-with-trainers.ts
@@ -0,0 +1,13 @@
+function matchPlayersAndTrainers(players: number[], trainers: number[]): number {
+    players.sort((a, b) => a - b);
+    trainers.sort((a, b) => a - b);
+    
+    let i = 0;
+    for (let j = 0; j < trainers.length; j++) {
+        if (players[i] <= trainers[j]) {
+            i++;
+        }
+    }
+    
+    return i;
+};
