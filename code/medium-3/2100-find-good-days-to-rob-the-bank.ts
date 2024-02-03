ba5a4a2ea80ea92a3d18d63f4ee991163624acf0
Quang Trung <gnurt10@gmail.com>
2024-02-03 12:48:12 +0700

    Update 2100-find-good-days-to-rob-the-bank.ts

--- code/medium-3/2100-find-good-days-to-rob-the-bank.ts
+++ code/medium-3/2100-find-good-days-to-rob-the-bank.ts
@@ -5,45 +5,38 @@ function goodDaysToRobBank(security: number[], time: number): number[] {
     
     const result = []
     for(let i = 0; i < security.length; i++){
-        // The day i is good day to rob if left[i] and right[i] >= time
         if(left[i] >= time && right[i] >= time){
             result.push(i)
         }
     }
-    
     return result
 };
-
-function buildRightValidArray(security){
+function buildRightValidArray(security) {
     const n = security.length
     const right = new Array(n)
-    
     right[n-1] = 0
-    
     for(let i = n-2; i >= 0; i--){
         if(security[i+1] >= security[i]){
             right[i] = right[i+1] +1
-        }
-        else{
+        } else {
             right[i] = 0
         }
     }
-    
     return right
 }
 
-function buildLeftValidArray(security){
+function buildLeftValidArray(security) {
     const n = security.length
     const left = new Array(n)
     left[0] = 0
     for(let i = 1; i < n; i++){
         if(security[i-1] >= security[i]){
             left[i] = left[i-1] + 1
-        }else
-        {
+        } else {
             left[i] = 0
         }
     }
-    
     return left
 }
+// TC O(n)
+// SC O(n)
