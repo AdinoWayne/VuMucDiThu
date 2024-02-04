c3fca8c4b9463f5566cb8fbeb0e8183f78662a38
Quang Trung <gnurt10@gmail.com>
2024-02-04 14:13:44 +0700

    Create 2433-find-the-original-array-of-prefix-xor.ts

---
+++ code/medium-3/2433-find-the-original-array-of-prefix-xor.ts
@@ -0,0 +1,8 @@
+function findArray(pref: number[]): number[] {
+    let xor = pref[0];
+    for(let i=1; i<pref.length; i++){
+        pref[i] ^= xor;
+        xor ^= pref[i];
+    }
+    return pref;
+};
