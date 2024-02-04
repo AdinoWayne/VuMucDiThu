1324cd9f11ec26f88f851ded0f4b159f6ce6b517
Quang Trung <gnurt10@gmail.com>
2024-02-05 00:06:21 +0700

    Create 2439-minimize-maximum-of-array.ts

---
+++ code/medium-3/2439-minimize-maximum-of-array.ts
@@ -0,0 +1,9 @@
+function minimizeArrayValue(nums: number[]): number {
+    let sum = 0;
+    let res = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        res = Math.max(res, Math.ceil(sum / (i+1)));
+    }
+    return res;
+};
