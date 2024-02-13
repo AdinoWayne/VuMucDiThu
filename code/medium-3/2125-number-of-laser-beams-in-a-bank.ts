6e005bb9a524b1eef169ac47106d31d464731df3
Quang Trung <gnurt10@gmail.com>
2024-02-13 11:02:16 +0700

    Update 2125-number-of-laser-beams-in-a-bank.ts

--- code/medium-3/2125-number-of-laser-beams-in-a-bank.ts
+++ code/medium-3/2125-number-of-laser-beams-in-a-bank.ts
@@ -1,15 +1,13 @@
 function numberOfBeams(bank: string[]): number {
     const devices: number[] = bank.map(e => (e.match(/1/g) || []).length).filter(a => a !== 0);
-  
-  if (!devices.length || devices.length === 1) {
-    return 0;
-  }
-
-  let sum = 0;
-
-  for (let i = 1; i < devices.length; i++) {
-    sum += devices[i] * devices[i - 1];
-  }
-  
-  return sum;
+    if (!devices.length || devices.length === 1) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 1; i < devices.length; i++) {
+        sum += devices[i] * devices[i - 1];
+    }
+    return sum;
 };
+// TC O(n2)
+// SC O(n2)
