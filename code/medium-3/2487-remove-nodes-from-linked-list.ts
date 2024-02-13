9ae92fe8c9fb4e284b33e819f7e1e6db9403e8db
Quang Trung <gnurt10@gmail.com>
2024-02-13 11:00:41 +0700

    Create 2487-remove-nodes-from-linked-list.ts

---
+++ code/medium-3/2487-remove-nodes-from-linked-list.ts
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeNodes(head: ListNode | null): ListNode | null {
+    if(!head) return null
+    
+    head.next = removeNodes(head.next)
+    
+    if(head.next && head.val < head.next.val) return head.next
+    
+    return head
+};
