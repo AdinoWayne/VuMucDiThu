6da7a1013f1d0c443e22a157056938e91b83c67a
Quang Trung <gnurt10@gmail.com>
2024-02-15 00:19:03 +0700

    Update 2139-minimum-moves-to-reach-target-score.ts

--- code/medium-3/2139-minimum-moves-to-reach-target-score.ts
+++ code/medium-3/2139-minimum-moves-to-reach-target-score.ts
@@ -1,7 +1,6 @@
 function minMoves(target: number, maxDoubles: number): number {
     if (maxDoubles === 0) return target - 1;
     let count = 0;
-    
     while (target > 1) {
         if (target % 2 === 0 && maxDoubles > 0) {
             target /= 2;
@@ -9,9 +8,9 @@ function minMoves(target: number, maxDoubles: number): number {
         } else {
             target--;
         }
-        
         count++;
     }
-    
     return count;
 };
+// Time complexity: O(log₂(target))
+// Space complexity: O(1)
