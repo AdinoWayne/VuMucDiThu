149c99a0588c2169ed3481c99b2c471d407bdf54
Quang Trung <gnurt10@gmail.com>
2024-02-15 00:15:57 +0700

    Create 2517-maximum-tastiness-of-candy-basket.ts

---
+++ code/medium-3/2517-maximum-tastiness-of-candy-basket.ts
@@ -0,0 +1,35 @@
+function maximumTastiness(price: number[], k: number): number {
+    price.sort((a, b) => a - b);
+
+  const n = price.length;
+  let ans = 0;
+  let low = 0;
+  let high = price[n - 1];
+
+  while (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    if (isOk(mid, price, k)) {
+      ans = mid;
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+
+  return ans;
+};
+
+function isOk(target, price, k) {
+  let lastVal = price[0];
+  let count = 1;
+
+  for (let i = 1; i < price.length; i++) {
+    const diff = price[i] - lastVal;
+    if (diff >= target) {
+      count++;
+      lastVal = price[i];
+    }
+  }
+
+  return count >= k;
+}
