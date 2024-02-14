31b8c010f6319e19ba3a5f59b49a2575fb223ae3
Quang Trung <gnurt10@gmail.com>
2024-02-14 18:39:43 +0700

    Create 2501-longest-square-streak-in-an-array.ts

---
+++ code/medium-3/2501-longest-square-streak-in-an-array.ts
@@ -0,0 +1,26 @@
+function longestSquareStreak(nums: number[]): number {
+    const squares = new Set();
+    for (let i = 2; i < 317; ++i) {
+        squares.add(i * i);
+    } 
+
+    nums = nums.filter(o => o < 317 || squares.has(o));
+    nums.sort((a, b) => a - b);
+    const set = new Set(nums);
+    let result = -1;
+    let count = 0, current;
+
+    for (const num of nums) {
+        current = num;
+        count = 1;
+
+        while (set.has(current * current)) {
+            current *= current;
+            ++count;
+        }
+
+        if (count > result) result = count;
+    }
+
+    return result === 1 ? -1 : result;
+};
