5aa485edf0fa3314355ee8b269631dca24c89fd9
Quang Trung <gnurt10@gmail.com>
2024-02-18 00:20:00 +0700

    Update 2149-rearrange-array-elements-by-sign.ts

--- code/medium-3/2149-rearrange-array-elements-by-sign.ts
+++ code/medium-3/2149-rearrange-array-elements-by-sign.ts
@@ -2,10 +2,10 @@ function rearrangeArray(nums: number[]): number[] {
     let positiveNums = nums.filter(x => x > 0)
     let negativeNums = nums.filter(x => x < 0)
     let result = []
-
     for (let i = 0; i < nums.length / 2; i++) {
         result.push(positiveNums[i], negativeNums[i])
     }
-
     return result
 };
+// TC O(n) 
+// SC O(n)
