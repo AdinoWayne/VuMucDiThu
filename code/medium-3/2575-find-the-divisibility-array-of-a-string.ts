856b09af1d5f756792972fd49ad12b5d837beee9
Quang Trung <gnurt10@gmail.com>
2024-02-19 00:02:31 +0700

    Create 2575-find-the-divisibility-array-of-a-string.ts

---
+++ code/medium-3/2575-find-the-divisibility-array-of-a-string.ts
@@ -0,0 +1,12 @@
+function divisibilityArray(word: string, m: number): number[] {
+    const ans = [];
+    let num = 0, carry = 0;
+    
+    for (let i = 0; i < word.length; i++) {
+        num = Number(carry + word[i]);
+        carry = num % m;
+        ans.push(carry ? 0 : 1);
+    }
+    
+    return ans;
+};
