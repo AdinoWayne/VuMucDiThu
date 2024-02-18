3342cfc0331a78c9048830827b4ab3547cc3675a
Quang Trung <gnurt10@gmail.com>
2024-02-19 00:03:07 +0700

    Create 2576-find-the-maximum-number-of-marked-indices.ts

---
+++ code/medium-3/2576-find-the-maximum-number-of-marked-indices.ts
@@ -0,0 +1,13 @@
+function maxNumOfMarkedIndices(nums: number[]): number {
+  const n = nums.length;
+  nums.sort((a, b) => a - b); // sort the array in non-decreasing order
+  let st = 0;
+  let mid = Math.floor((n + 1) / 2);
+  while (mid < n) {
+    if (nums[st] * 2 <= nums[mid]) {
+      st++;
+    }
+    mid++;
+  }
+  return st * 2; 
+};
