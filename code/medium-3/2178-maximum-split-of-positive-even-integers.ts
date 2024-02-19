d29c49f2012d1e3ddef631a8945b2c49d0debe41
Quang Trung <gnurt10@gmail.com>
2024-02-20 00:06:22 +0700

    Update 2178-maximum-split-of-positive-even-integers.ts

--- code/medium-3/2178-maximum-split-of-positive-even-integers.ts
+++ code/medium-3/2178-maximum-split-of-positive-even-integers.ts
@@ -1,17 +1,14 @@
 function maximumEvenSplit(finalSum: number): any[] {
     if(finalSum % 2) return [];
-
     const set = new Set();
-
     let n = 2, sum = 0;
-
     while(sum < finalSum) {
         sum += n;
         set.add(n);
         n += 2;
     }
-
     set.delete(sum - finalSum);
-
     return [...set];
 };
+// TC O(Logn) 
+// SC O(Logn)
