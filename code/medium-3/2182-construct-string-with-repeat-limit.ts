e44cc7c0c95eddbcab70a22e84896c1dbda03049
Quang Trung <gnurt10@gmail.com>
2024-02-21 06:56:17 +0700

    Update 2182-construct-string-with-repeat-limit.ts

--- code/medium-3/2182-construct-string-with-repeat-limit.ts
+++ code/medium-3/2182-construct-string-with-repeat-limit.ts
@@ -3,23 +3,18 @@ function repeatLimitedString(s: string, repeatLimit: number): string {
     const counts = new Array(26).fill(0);
     let result = "";
     let cnt, takeOne, len, lastLetter;
-    
     for (let i = 0; i < s.length; ++i) {
         ++counts[s.charCodeAt(i) - aCode];
     }
-    
     while (result.length < s.length) {
         takeOne = false;
         len = result.length;
-
         for (let i = 26; i >= 0; --i) {
             if (counts[i] > 0) {
-                
                 if (lastLetter === i) {
                     takeOne = true;
                     continue;
                 }
-                
                 lastLetter = i;
                 cnt = takeOne ? 1 : Math.min(counts[i], repeatLimit);
                 result += String.fromCharCode(i + aCode).repeat(cnt);
@@ -27,9 +22,9 @@ function repeatLimitedString(s: string, repeatLimit: number): string {
                 break;
             }
         }
-        
         if (result.length === len) break;
     }
-    
     return result;
 };
+// TC O(n * k)
+// SC O(k)
