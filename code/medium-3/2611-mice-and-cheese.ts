51a7519ccdf42992d6d3813ca1fa02e796df4b41
Quang Trung <gnurt10@gmail.com>
2024-02-22 00:04:54 +0700

    Create 2611-mice-and-cheese.ts

---
+++ code/medium-3/2611-mice-and-cheese.ts
@@ -0,0 +1,12 @@
+function miceAndCheese(reward1: number[], reward2: number[], k: number): number {
+    let n = reward1.length, rewards = [];
+    for (let i = 0; i < n; i++) {
+        rewards.push([reward1[i], reward2[i], reward1[i] - reward2[i]]);
+    }
+    rewards.sort((a, b) => b[2] - a[2]);
+    let score = 0;
+    for (let i = 0; i < n; i++) {
+        score += i < k ? rewards[i][0] : rewards[i][1];
+    }
+    return score;
+};
