9a3e204771753205f324846fd5d8c0539a2bb7bd
Quang Trung <gnurt10@gmail.com>
2024-02-23 00:01:03 +0700

    Update 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.ts

--- code/medium-3/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.ts
+++ code/medium-3/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.ts
@@ -1,23 +1,23 @@
 function getAncestors(n: number, edges: number[][]): any[][] {
     const inEdges = Array.from({ length: n }, () => new Set());
   const outEdges = Array.from({ length: n }, () => []);
-  // init information
   for (const [from, to] of edges) {
     inEdges[to].add(from);
     outEdges[from].push(to);
   }
   for (let i = 0; i < n; ++i) {
-	// start dfs from each node as the root
     dfs(inEdges, outEdges, i, i, new Uint8Array(n));
   }
   return inEdges.map(set => Array.from(set).sort((a, b) => Number(a) - Number(b)));
 };
 
 const dfs = (inEdges, outEdges, originValue, curValue, finished) => {
-  if (finished[curValue]) return; // avoid repeat calculation
+  if (finished[curValue]) return;
   finished[curValue] = 1;
   for (const out of outEdges[curValue]) {
     inEdges[out].add(originValue);
-    dfs(inEdges, outEdges, originValue, out, finished); // traverse tree
+    dfs(inEdges, outEdges, originValue, out, finished);
   }
 };
+// TC O(n^2)
+// SC O(n)
