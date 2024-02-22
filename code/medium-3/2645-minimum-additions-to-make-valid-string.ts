fc5db9e55276abb985776f44233d61ed9e04f5d8
Quang Trung <gnurt10@gmail.com>
2024-02-23 00:02:58 +0700

    Create 2645-minimum-additions-to-make-valid-string.ts

---
+++ code/medium-3/2645-minimum-additions-to-make-valid-string.ts
@@ -0,0 +1,15 @@
+function addMinimum(word: string): number {
+var i = 0;
+    var result = 0;
+    var abc = 'abc';
+    while (i < word.length) {
+        for(var j = 0; j < abc.length; j++) {
+            if (word[i] !== abc[j]) {
+                result++;
+            } else {
+                i++;
+            }
+        }
+    }
+    return result;
+};
