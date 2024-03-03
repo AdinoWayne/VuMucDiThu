8e2cc78ef5587323e81ba31022eaaa5f9ccceb3d
Quang Trung <gnurt10@gmail.com>
2024-03-03 11:44:11 +0700

    Update 2275-largest-combination-with-bitwise-and-greater-than-zero.ts

--- code/medium-3/2275-largest-combination-with-bitwise-and-greater-than-zero.ts
+++ code/medium-3/2275-largest-combination-with-bitwise-and-greater-than-zero.ts
@@ -12,3 +12,13 @@ function largestCombination(candidates: number[]): number {
 };
 // Time Complexity: O(n)
 // Space Complexity: O(1)
+//hint:
+// index--> 6 5 4 3 2 1 0
+//     16 = 0 0 1 0 0 0 0
+//     17 = 0 0 1 0 0 0 1
+//     71 = 1 0 0 0 1 1 1
+//     62 = 0 1 1 1 1 1 0
+//     12 = 0 0 0 1 1 0 0
+//     24 = 0 0 1 1 0 0 0
+//     14 = 0 0 0 1 1 1 0 
+// Approach: To maximize number of elements whose AND will result a number greater than zero find maximum number of ones that lie in a particular index.
