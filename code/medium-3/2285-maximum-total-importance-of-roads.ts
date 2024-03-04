9a5ccc882934c3ac195968696fd556260ddd6a78
Quang Trung <gnurt10@gmail.com>
2024-03-05 00:43:08 +0700

    Update 2285-maximum-total-importance-of-roads.ts

--- code/medium-3/2285-maximum-total-importance-of-roads.ts
+++ code/medium-3/2285-maximum-total-importance-of-roads.ts
@@ -1,18 +1,15 @@
 function maximumImportance(n: number, roads: number[][]): number {
     const edgesCount = new Array(n).fill(0);
-    
     for (const [a, b] of roads) {
         edgesCount[a] += 1;
         edgesCount[b] += 1;
     }
-    
     edgesCount.sort((a, b) => a - b);
-    
     let totSum = 0;
-    
     for (let i = n - 1; i >= 0; --i) {
         totSum += (edgesCount[i] * (i + 1));
     }
-    
     return totSum;
 };
+// TC O(nlogn)
+// SC O(n)
