553a2f0b3e16eb0f8f361fb9ffed2199286973e2
Quang Trung <gnurt10@gmail.com>
2024-03-08 00:53:25 +0700

    Update 2320-count-number-of-ways-to-place-houses.ts

--- code/medium-3/2320-count-number-of-ways-to-place-houses.ts
+++ code/medium-3/2320-count-number-of-ways-to-place-houses.ts
@@ -3,12 +3,12 @@ function countHousePlacements(n: number): any {
     let house = 1n
     let space = 1n
     let total = house + space
-
     for (let i = 2; i < n + 1; i++) {
         house = space
         space = total
         total = (house + space) % MOD
     }
-
     return (total * total) % MOD
 };
+// Time Complexity: O(n)
+// Space Complexity: O(1)
