d983dbf285c414c25c87a146ffb460fe5374a4ba
Quang Trung <gnurt10@gmail.com>
2024-03-14 07:40:08 +0700

    Update 2337-move-pieces-to-obtain-a-string.ts

--- code/medium-3/2337-move-pieces-to-obtain-a-string.ts
+++ code/medium-3/2337-move-pieces-to-obtain-a-string.ts
@@ -5,22 +5,14 @@ function canChange(start: string, target: string): boolean {
     for (let i = 0; i < start.length; i++) {
         if (start[i] === "R") r++
         if (start[i] === "L") l--
-
-        /**
-         * If we encountered L and there is R that has not reached the target,
-         * then we know that this L is blocking the way
-         */
         if (target[i] === "L" && r > 0) return false
         if (start[i] === "L" && r > 0) return false
 
         if (target[i] === "L") l++
         if (target[i] === "R") r--
-
         if (l < 0 || r < 0) return false
     }
-
-    /**
-     * Final l and r count must be zero
-     */
     return !l && !r
 };
+// TC O(n)
+// SC O(1)
