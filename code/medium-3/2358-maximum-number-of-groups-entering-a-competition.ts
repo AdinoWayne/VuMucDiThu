01471281d97760931746c156b6fa448e47cdf2b6
Quang Trung <gnurt10@gmail.com>
2024-03-19 00:24:14 +0700

    Update 2358-maximum-number-of-groups-entering-a-competition.ts

--- code/medium-3/2358-maximum-number-of-groups-entering-a-competition.ts
+++ code/medium-3/2358-maximum-number-of-groups-entering-a-competition.ts
@@ -1,11 +1,10 @@
 function maximumGroups(grades: number[]): number {
     const n = grades.length;
-    
     let i = 1;
-    
     while((i + 1) * i/2 < n){
         i ++;
     }
-        
     return (i + 1) * i/2 == n? i : i -1;
 };
+// Time complexity: O(log n) on average and worst case, O(1) in the best case
+// Space complexity: O(1)
