286aa188fef47a185a267bd801e5a36249ff91f5
Quang Trung <gnurt10@gmail.com>
2024-03-21 00:45:54 +0700

    Update 2364-count-number-of-bad-pairs.ts

--- code/medium-3/2364-count-number-of-bad-pairs.ts
+++ code/medium-3/2364-count-number-of-bad-pairs.ts
@@ -2,20 +2,17 @@ function countBadPairs(nums: number[]): number {
     let totalPairs = 0
     let goodPairs = 0
     let differencesFreqsMap = {}
-    
     for(let i = 0; i < nums.length; i++) {
         const diff = nums[i] - i
-        
         totalPairs+=i
-        
         if(diff in differencesFreqsMap) {
             goodPairs+= differencesFreqsMap[diff]
             differencesFreqsMap[diff]++
             continue
         }
-        
         differencesFreqsMap[diff] = 1
     }
-    
     return totalPairs - goodPairs
 };
+// The time complexity is typically O(n) but can become O(n^2) in the worst case.
+// The space complexity is O(n) in the worst and average cases.
