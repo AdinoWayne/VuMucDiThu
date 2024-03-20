580d62ec9867f42a0d74d5287b380d15bc93bbbf
Quang Trung <gnurt10@gmail.com>
2024-03-20 23:16:39 +0700

    Update 2358-maximum-number-of-groups-entering-a-competition.ts

--- code/medium-3/2358-maximum-number-of-groups-entering-a-competition.ts
+++ code/medium-3/2358-maximum-number-of-groups-entering-a-competition.ts
@@ -8,3 +8,11 @@ function maximumGroups(grades: number[]): number {
 };
 // Time complexity: O(log n) on average and worst case, O(1) in the best case
 // Space complexity: O(1)
+
+// hint:
+// Therefore, we only care about the size of the grades array, not the grades themselves.
+
+// The number of students in l groups is l * (l + 1) / 2.
+
+// Binary Search
+// We can binary-search for maximum l so that l * (l + 1) / 2 <= n.
