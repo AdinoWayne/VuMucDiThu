be1827f447398baf35c153935639a36c86cb30b0
PeanutTran <85547159+PeanutTran@users.noreply.github.com>
2024-05-04 00:38:48 +0700

    Update 2492-minimum-score-of-a-path-between-two-cities.ts

--- code/medium-3/2492-minimum-score-of-a-path-between-two-cities.ts
+++ code/medium-3/2492-minimum-score-of-a-path-between-two-cities.ts
@@ -1,16 +1,13 @@
 function minScore(n: number, roads: number[][]): number {
     const graph = new Array(n + 1).fill(null).map(() => []);
     const visited = new Set([]);
-
     for (const [v1, v2, distance] of roads) {
         graph[v1].push([v2, distance]);
         graph[v2].push([v1, distance]);
     }
-
     const queue = [1];
     visited.add(1);
     let ans = Infinity;
-
     while (queue.length > 0) {
         const node = queue.shift();
         for (const [next, distance] of graph[node]) {
@@ -21,6 +18,7 @@ function minScore(n: number, roads: number[][]): number {
         queue.push(next);
         }
     }
-
     return ans;
 };
+// The minScore function has a time complexity of O(V + E) in the worst case, making it efficient for various graph sizes.
+// The space complexity is also O(V + E), indicating moderate memory usage for storing the graph representation and visited nodes.
